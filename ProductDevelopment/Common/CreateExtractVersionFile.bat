ce98231d9a91572baa65a3bc8010b3cb66bafdee
William_Parr <William_Parr@extract.local>
2009-06-17 13:47:34 -0500

    ﻿ [git-vault-id] Extract$/Engineering@265/266

---
+++ ProductDevelopment/Common/CreateExtractVersionFile.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+
+@ECHO FlexIndexVersion	%FlexIndexVersion%	 >%1
+@ECHO ReusableComponentsVersion	%ReusableComponentsVersion%	 >>%1
+@ECHO LaserficheVersion	%LaserficheVersion%	 >>%1
+@ECHO IDShieldOfficeVersion	%IDShieldOfficeVersion%	 >>%1
+@ECHO LabDEVersion	%LabDEVersion%	 >>%1
+
