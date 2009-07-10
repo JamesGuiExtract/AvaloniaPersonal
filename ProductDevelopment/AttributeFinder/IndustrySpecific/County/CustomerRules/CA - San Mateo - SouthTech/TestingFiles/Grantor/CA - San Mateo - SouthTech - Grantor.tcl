e2884c1434d117c10ac970eadc45ebd084cac3f0
Jennifer_Hagebock <Jennifer_Hagebock@extract.local>
2009-07-10 12:18:40 -0500

    ﻿ [git-vault-id] Extract$/Engineering@926/927

--- ProductDevelopment/AttributeFinder/IndustrySpecific/County/CustomerRules/CA - San Mateo - SouthTech/TestingFiles/Grantor/CA - San Mateo - SouthTech - Grantor.tcl
+++ ProductDevelopment/AttributeFinder/IndustrySpecific/County/CustomerRules/CA - San Mateo - SouthTech/TestingFiles/Grantor/CA - San Mateo - SouthTech - Grantor.tcl
@@ -1,11 +1,11 @@
-//UCLIDAFCoreTest.AutomatedRuleSetTester.1;;CA - San Mateo - SouthTech - Grantor - ABSTRACT OF JUDGEMENT.dat
-//UCLIDAFCoreTest.AutomatedRuleSetTester.1;;CA - San Mateo - SouthTech - Grantor - AFFIDAVIT OF DEATH.dat
-//UCLIDAFCoreTest.AutomatedRuleSetTester.1;;CA - San Mateo - SouthTech - Grantor - ASSIGNMENT OF DEED OF TRUST.dat
-//UCLIDAFCoreTest.AutomatedRuleSetTester.1;;CA - San Mateo - SouthTech - Grantor - DEED OF TRUST.dat
-//UCLIDAFCoreTest.AutomatedRuleSetTester.1;;CA - San Mateo - SouthTech - Grantor - DEED.dat
-//UCLIDAFCoreTest.AutomatedRuleSetTester.1;;CA - San Mateo - SouthTech - Grantor - MODIFICATION TO DEED OF TRUST.dat
-//UCLIDAFCoreTest.AutomatedRuleSetTester.1;;CA - San Mateo - SouthTech - Grantor - NOTICE OF STATE TAX LIEN.dat
-//UCLIDAFCoreTest.AutomatedRuleSetTester.1;;CA - San Mateo - SouthTech - Grantor - RECONVEYANCE.dat
-//UCLIDAFCoreTest.AutomatedRuleSetTester.1;;CA - San Mateo - SouthTech - Grantor - RELEASE OF LIEN BY STATE_COUNTY.dat
-//UCLIDAFCoreTest.AutomatedRuleSetTester.1;;CA - San Mateo - SouthTech - Grantor - SUBSTITUTION OF TRUSTEE.dat
-UCLIDAFCoreTest.AutomatedRuleSetTester.1;;CA - San Mateo - SouthTech - Grantor - Set009.dat
+UCLIDAFCoreTest.AutomatedRuleSetTester.1;;CA - San Mateo - SouthTech - Grantor - ABSTRACT OF JUDGEMENT.dat
+UCLIDAFCoreTest.AutomatedRuleSetTester.1;;CA - San Mateo - SouthTech - Grantor - AFFIDAVIT OF DEATH.dat
+UCLIDAFCoreTest.AutomatedRuleSetTester.1;;CA - San Mateo - SouthTech - Grantor - ASSIGNMENT OF DEED OF TRUST.dat
+UCLIDAFCoreTest.AutomatedRuleSetTester.1;;CA - San Mateo - SouthTech - Grantor - DEED OF TRUST.dat
+UCLIDAFCoreTest.AutomatedRuleSetTester.1;;CA - San Mateo - SouthTech - Grantor - DEED.dat
+UCLIDAFCoreTest.AutomatedRuleSetTester.1;;CA - San Mateo - SouthTech - Grantor - MODIFICATION TO DEED OF TRUST.dat
+UCLIDAFCoreTest.AutomatedRuleSetTester.1;;CA - San Mateo - SouthTech - Grantor - NOTICE OF STATE TAX LIEN.dat
+UCLIDAFCoreTest.AutomatedRuleSetTester.1;;CA - San Mateo - SouthTech - Grantor - RECONVEYANCE.dat
+UCLIDAFCoreTest.AutomatedRuleSetTester.1;;CA - San Mateo - SouthTech - Grantor - RELEASE OF LIEN BY STATE_COUNTY.dat
+UCLIDAFCoreTest.AutomatedRuleSetTester.1;;CA - San Mateo - SouthTech - Grantor - SUBSTITUTION OF TRUSTEE.dat
+//UCLIDAFCoreTest.AutomatedRuleSetTester.1;;CA - San Mateo - SouthTech - Grantor - Set009.dat
