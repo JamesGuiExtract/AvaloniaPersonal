c149ab1331a348c80fdc2b75875aee6012ed789b
Nathan_Figueroa <Nathan_Figueroa@extract.local>
2009-08-05 17:36:37 -0500

    ﻿Created page summary grid that displays visited pages in green. [FIDSC #3616] Reviewed by William and Wayne. [git-vault-id] Extract$/Engineering@1600/1601

---
+++ RC.Net/Imaging/Forms/Core/Code/PageSummaryView.Designer.cs
@@ -0,0 +1,184 @@
+namespace Extract.Imaging.Forms
+{
+    partial class PageSummaryView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <overloads>Releases resources used by the <see cref="PageSummaryView"/>.</overloads>
+        /// <summary>
+        /// Releases all unmanaged resources used by the <see cref="PageSummaryView"/>.
+        /// </summary>
+        /// <param name="disposing"><see langword="true"/> to release both managed and unmanaged 
+        /// resources; <see langword="false"/> to release only unmanaged resources.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // Release managed resources
+                if (components != null)
+                {
+                    components.Dispose();
+                }
+            }
+
+            // Release unmanaged resources
+
+            // Call base dispose method
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this._dataGridView = new System.Windows.Forms.DataGridView();
+            this.First = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Second = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Third = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Fourth = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Fifth = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sixth = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Seventh = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Eighth = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Ninth = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tenth = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this._dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // _dataGridView
+            // 
+            this._dataGridView.AllowUserToAddRows = false;
+            this._dataGridView.AllowUserToDeleteRows = false;
+            this._dataGridView.AllowUserToResizeColumns = false;
+            this._dataGridView.AllowUserToResizeRows = false;
+            this._dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this._dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this._dataGridView.ColumnHeadersVisible = false;
+            this._dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.First,
+            this.Second,
+            this.Third,
+            this.Fourth,
+            this.Fifth,
+            this.Sixth,
+            this.Seventh,
+            this.Eighth,
+            this.Ninth,
+            this.Tenth});
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this._dataGridView.DefaultCellStyle = dataGridViewCellStyle1;
+            this._dataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._dataGridView.Enabled = false;
+            this._dataGridView.Location = new System.Drawing.Point(0, 0);
+            this._dataGridView.Margin = new System.Windows.Forms.Padding(0);
+            this._dataGridView.MultiSelect = false;
+            this._dataGridView.Name = "_dataGridView";
+            this._dataGridView.ReadOnly = true;
+            this._dataGridView.RowHeadersVisible = false;
+            this._dataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this._dataGridView.Size = new System.Drawing.Size(100, 100);
+            this._dataGridView.StandardTab = true;
+            this._dataGridView.TabIndex = 1;
+            this._dataGridView.CellValidating += new System.Windows.Forms.DataGridViewCellValidatingEventHandler(this.HandleDataGridViewCellValidating);
+            this._dataGridView.CurrentCellChanged += new System.EventHandler(this.HandleDataGridViewCurrentCellChanged);
+            // 
+            // First
+            // 
+            this.First.HeaderText = "First";
+            this.First.Name = "First";
+            this.First.ReadOnly = true;
+            // 
+            // Second
+            // 
+            this.Second.HeaderText = "Second";
+            this.Second.Name = "Second";
+            this.Second.ReadOnly = true;
+            // 
+            // Third
+            // 
+            this.Third.HeaderText = "Third";
+            this.Third.Name = "Third";
+            this.Third.ReadOnly = true;
+            // 
+            // Fourth
+            // 
+            this.Fourth.HeaderText = "Fourth";
+            this.Fourth.Name = "Fourth";
+            this.Fourth.ReadOnly = true;
+            // 
+            // Fifth
+            // 
+            this.Fifth.HeaderText = "Fifth";
+            this.Fifth.Name = "Fifth";
+            this.Fifth.ReadOnly = true;
+            // 
+            // Sixth
+            // 
+            this.Sixth.HeaderText = "Sixth";
+            this.Sixth.Name = "Sixth";
+            this.Sixth.ReadOnly = true;
+            // 
+            // Seventh
+            // 
+            this.Seventh.HeaderText = "Seventh";
+            this.Seventh.Name = "Seventh";
+            this.Seventh.ReadOnly = true;
+            // 
+            // Eighth
+            // 
+            this.Eighth.HeaderText = "Eighth";
+            this.Eighth.Name = "Eighth";
+            this.Eighth.ReadOnly = true;
+            // 
+            // Ninth
+            // 
+            this.Ninth.HeaderText = "Ninth";
+            this.Ninth.Name = "Ninth";
+            this.Ninth.ReadOnly = true;
+            // 
+            // Tenth
+            // 
+            this.Tenth.HeaderText = "Tenth";
+            this.Tenth.Name = "Tenth";
+            this.Tenth.ReadOnly = true;
+            // 
+            // PageSummaryView
+            // 
+            this.Controls.Add(this._dataGridView);
+            this.MinimumSize = new System.Drawing.Size(100, 100);
+            this.Name = "PageSummaryView";
+            this.Size = new System.Drawing.Size(100, 100);
+            ((System.ComponentModel.ISupportInitialize)(this._dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView _dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn First;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Second;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Third;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Fourth;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Fifth;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sixth;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Seventh;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Eighth;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Ninth;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tenth;
+    }
+}
---
+++ RC.Net/Imaging/Forms/Core/Code/PageSummaryView.cs
@@ -0,0 +1,430 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+using System.Globalization;
+using System.Security.Permissions;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Extract.Imaging.Forms
+{
+    /// <summary>
+    /// Represents a control that displays a grid of visited pages.
+    /// </summary>
+    public partial class PageSummaryView : UserControl, IImageViewerControl
+    {
+        #region PageSummaryView Constants
+
+        /// <summary>
+        /// The number of cells in each row of the <see cref="PageSummaryView"/> grid.
+        /// </summary>
+        const int CELLS_PER_ROW = 10;
+
+        /// <summary>
+        /// Windows message that is posted when a nonsystem key is pressed.
+        /// </summary>
+        const int WM_KEYDOWN = 0x100;
+
+        /// <summary>
+        /// Windows message that is posted when a system key is pressed.
+        /// </summary>
+        const int WM_SYSKEYDOWN = 0x104;
+
+        #endregion PageSummaryView Constants
+
+        #region PageSummaryView Fields
+
+        /// <summary>
+        /// The image viewer associated with the <see cref="PageSummaryView"/>.
+        /// </summary>
+        ImageViewer _imageViewer;
+
+        /// <summary>
+        /// The one-based page number which was most recently active.
+        /// </summary>
+        int _lastPage;
+
+        /// <summary>
+        /// The cell style associated with pages that have been visited.
+        /// </summary>
+        DataGridViewCellStyle _visitedPageStyle;
+
+        #endregion PageSummaryView Fields
+
+        #region PageSummaryView Constructors
+
+        /// <summary>
+        /// Initializes a new <see cref="PageSummaryView"/> class.
+        /// </summary>
+        // Don't fight with auto-generated code.
+        //[SuppressMessage("Microsoft.Performance", "CA1805:DoNotInitializeUnnecessarily")]
+        public PageSummaryView()
+        {
+            InitializeComponent();
+        }
+
+        #endregion PageSummaryView Constructors
+
+        #region PageSummaryView Properties
+
+        /// <summary>
+        /// Gets the style for visited page cells.
+        /// </summary>
+        /// <returns>The style for visited page cells.</returns>
+        DataGridViewCellStyle VisitedPageStyle
+        {
+            get
+            {
+                if (_visitedPageStyle == null)
+                {
+                    DataGridViewCellStyle style = _dataGridView.DefaultCellStyle.Clone();
+                    style.BackColor = Color.Green;
+                    _visitedPageStyle = style;
+                }
+
+                return _visitedPageStyle;
+            }
+        }
+
+        #endregion PageSummaryView Properties
+
+        #region PageSummaryView Methods
+
+        /// <summary>
+        /// Determines whether it is valid to move forward the specified number of pages.
+        /// </summary>
+        /// <param name="pages">The number of pages to move forward. May be negative.</param>
+        /// <returns><see langword="true"/> if moving forward by <paramref name="pages"/> would 
+        /// result in a valid page number; <see langword="false"/> if moving forward would be an 
+        /// invalid page number.</returns>
+        bool IsValidToMoveBy(int pages)
+        {
+            int newPage = _imageViewer.PageNumber + pages;
+            return newPage > 0 && newPage <= _imageViewer.PageCount;
+        }
+
+        /// <summary>
+        /// Resets the cells of the <see cref="PageSummaryView"/> based on the status of the 
+        /// <see cref="_imageViewer"/>.
+        /// </summary>
+        void Reset()
+        {
+            // Check if the image viewer's image is available
+            bool imageAvailable = _imageViewer != null && _imageViewer.IsImageAvailable;
+
+            // Reset the list view
+            _dataGridView.Rows.Clear();
+            _dataGridView.Enabled = imageAvailable;
+            _lastPage = 0;
+
+            if (imageAvailable)
+            {
+                // Compute the number of full rows and remaining cells needed to represent the pages
+                int fullRows = _imageViewer.PageCount / CELLS_PER_ROW;
+                int remainingCells = _imageViewer.PageCount % CELLS_PER_ROW;
+
+                // Create the full rows
+                for (int i = 0; i < fullRows; i++)
+                {
+                    string[] row = CreateRow(i);
+                    _dataGridView.Rows.Add(row);
+                }
+
+                // Create the last row of remaining cells if necessary
+                if (remainingCells > 0)
+                {
+                    string[] row = CreateRow(fullRows, remainingCells);
+                    _dataGridView.Rows.Add(row);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Creates the specified row fully populated with page numbers.
+        /// </summary>
+        /// <param name="rowNumber">The zero-based row to create.</param>
+        /// <returns>The row corresponding to <paramref name="rowNumber"/>.</returns>
+        static string[] CreateRow(int rowNumber)
+        {
+            return CreateRow(rowNumber, CELLS_PER_ROW);
+        }
+
+        /// <summary>
+        /// Creates the specified row partially populated with page numbers.
+        /// </summary>
+        /// <param name="rowNumber">The zero-based row to create.</param>
+        /// <param name="cellsInRow">The number of cells in the row to contain page numbers.</param>
+        /// <returns>The row corresponding to <paramref name="rowNumber"/> with populated with 
+        /// <paramref name="cellsInRow"/> number of page number cells.</returns>
+        static string[] CreateRow(int rowNumber, int cellsInRow)
+        {
+            // Prepare the cells of the row
+            string[] row = new string[CELLS_PER_ROW];
+
+            // Set the text for each cell
+            int lower = rowNumber * CELLS_PER_ROW + 1;
+            int upper = lower + cellsInRow;
+            for (int i = lower; i < upper; i++)
+            {
+                row[i - lower] = i.ToString(CultureInfo.CurrentCulture);
+            }
+
+            // Any remaining cells should be blank
+            for (int i = cellsInRow; i < CELLS_PER_ROW; i++)
+            {
+                row[i] = "";
+            }
+
+            return row;
+        }
+
+        /// <summary>
+        /// Retrieves the cell that corresponds to the specified page number.
+        /// </summary>
+        /// <param name="pageNumber">The one-based page number corresponding to the cell to 
+        /// retrieve.</param>
+        /// <returns>The cell that corresponds to the specified <paramref name="pageNumber"/>.
+        /// </returns>
+        DataGridViewCell GetCellByPageNumber(int pageNumber)
+        {
+            int row = (pageNumber - 1) / CELLS_PER_ROW;
+            int column = (pageNumber - 1) % CELLS_PER_ROW;
+
+            return _dataGridView.Rows[row].Cells[column];
+        }
+
+        /// <summary>
+        /// Gets the page number to which the specified indices correspond.
+        /// </summary>
+        /// <param name="row">The zero-based row.</param>
+        /// <param name="column">The zero-based column.</param>
+        /// <returns>The page number corresponding to the cell at <paramref name="row"/> and 
+        /// <paramref name="column"/>.</returns>
+        static int GetPageNumberByIndices(int row, int column)
+        {
+            return row * CELLS_PER_ROW + column + 1;
+        }
+
+        #endregion PageSummaryView Methods
+
+        #region PageSummaryView Overrides
+
+        /// <summary>
+        /// Processes a command key.
+        /// </summary>
+        /// <param name="msg">The window message to process.</param>
+        /// <param name="keyData">The key to process.</param>
+        /// <returns><see langword="true"/> if the character was processed by the control; 
+        /// otherwise, <see langword="false"/>.</returns>
+        [SecurityPermission(SecurityAction.LinkDemand, Flags=SecurityPermissionFlag.UnmanagedCode)]
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (msg.Msg == WM_KEYDOWN || msg.Msg == WM_SYSKEYDOWN)
+            {
+                switch (keyData)
+                {
+                    case Keys.Down:
+
+                        // If moving would move to an invalid page cell, don't process the key.
+                        if (!IsValidToMoveBy(CELLS_PER_ROW))
+                        {
+                            return true;
+                        }
+                        break;
+
+                    case Keys.Right:
+
+                        // If moving would move to an invalid page cell, don't process the key.
+                        if (!IsValidToMoveBy(1))
+                        {
+                            return true;
+                        }
+                        break;
+                }
+            }
+
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        #endregion PageSummaryView Overrides
+
+        #region PageSummaryView Event Handlers
+
+        /// <summary>
+        /// Handles the <see cref="Extract.Imaging.Forms.ImageViewer.ImageFileChanged"/> event.
+        /// </summary>
+        /// <param name="sender">The object that sent the 
+        /// <see cref="Extract.Imaging.Forms.ImageViewer.ImageFileChanged"/> event.</param>
+        /// <param name="e">The event data associated with the 
+        /// <see cref="Extract.Imaging.Forms.ImageViewer.ImageFileChanged"/> event.</param>
+        void HandleImageFileChanged(object sender, ImageFileChangedEventArgs e)
+        {
+            try
+            {
+                Reset();
+            }
+            catch (Exception ex)
+            {
+                ExtractException ee = ExtractException.AsExtractException("ELI26984", ex);
+                ee.AddDebugData("Event data", e, false);
+                ee.Display();
+            }
+        }
+
+        /// <summary>
+        /// Handles the <see cref="Extract.Imaging.Forms.ImageViewer.PageChanged"/> event.
+        /// </summary>
+        /// <param name="sender">The object that sent the 
+        /// <see cref="Extract.Imaging.Forms.ImageViewer.PageChanged"/> event.</param>
+        /// <param name="e">The event data associated with the 
+        /// <see cref="Extract.Imaging.Forms.ImageViewer.PageChanged"/> event.</param>
+        void HandlePageChanged(object sender, PageChangedEventArgs e)
+        {
+            try
+            {
+                // Select the appropriate cell if it is not already selected
+                DataGridViewCell currentCell = GetCellByPageNumber(e.PageNumber);
+                if (!currentCell.Selected)
+                {
+                    currentCell.Selected = true;
+                }
+
+                // Mark the previously visited page
+                if (_lastPage > 0)
+	            {
+                    DataGridViewCell previousCell = GetCellByPageNumber(_lastPage);
+                    previousCell.Style = VisitedPageStyle;
+	            }
+
+                // Store the current page as the last visited page
+                _lastPage = e.PageNumber;
+            }
+            catch (Exception ex)
+            {
+                ExtractException ee = ExtractException.AsExtractException("ELI27003", ex);
+                ee.AddDebugData("Event data", e, false);
+                ee.Display();
+            }
+        }
+
+        /// <summary>
+        /// Handles the <see cref="DataGridView.CurrentCellChanged"/> event.
+        /// </summary>
+        /// <param name="sender">The object that sent the 
+        /// <see cref="DataGridView.CurrentCellChanged"/> event.</param>
+        /// <param name="e">The event data associated with the 
+        /// <see cref="DataGridView.CurrentCellChanged"/> event.</param>
+        void HandleDataGridViewCurrentCellChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                DataGridViewCell cell = _dataGridView.CurrentCell;
+                if (cell != null)
+                {
+                    // Set the image viewer's page to correspond to the current cell
+                    int page = GetPageNumberByIndices(cell.RowIndex, cell.ColumnIndex);
+                    if (_imageViewer.PageNumber != page)
+                    {
+                        _imageViewer.PageNumber = page;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ExtractException ee = ExtractException.AsExtractException("ELI27004", ex);
+                ee.AddDebugData("Event data", e, false);
+                ee.Display();
+            }
+        }
+
+        /// <summary>
+        /// Handles the <see cref="DataGridView.CellValidating"/> event.
+        /// </summary>
+        /// <param name="sender">The object that sent the 
+        /// <see cref="DataGridView.CellValidating"/> event.</param>
+        /// <param name="e">The event data associated with the 
+        /// <see cref="DataGridView.CellValidating"/> event.</param>
+        void HandleDataGridViewCellValidating(object sender, DataGridViewCellValidatingEventArgs e)
+        {
+            try
+            {
+                // Check if a mouse event is about to occur
+                if (Control.MouseButtons != MouseButtons.None)
+                {
+                    // Is a cell being clicked?
+                    Point mouse = _dataGridView.PointToClient(Control.MousePosition);
+                    DataGridView.HitTestInfo info = _dataGridView.HitTest(mouse.X, mouse.Y);
+                    if (info.Type == DataGridViewHitTestType.Cell)
+                    {
+                        // If the cell being clicked is invalid, disallow the new selection
+                        int page = GetPageNumberByIndices(info.RowIndex, info.ColumnIndex);
+                        if (page > _imageViewer.PageCount)
+                        {
+                            e.Cancel = true;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ExtractException ee = ExtractException.AsExtractException("ELI27043", ex);
+                ee.AddDebugData("Event data", e, false);
+                ee.Display();
+            }
+        }
+        
+        #endregion PageSummaryView Event Handlers
+
+        #region IImageViewerControl Members
+
+        /// <summary>
+        /// Gets or sets the image viewer to which the <see cref="PageSummaryView"/> 
+        /// is connected.
+        /// </summary>
+        /// <value>The image viewer to which the <see cref="PageSummaryView"/> is 
+        /// connected. <see langword="null"/> if connection should be disconnected.</value>
+        /// <returns>The image viewer to which the <see cref="PageSummaryView"/> is 
+        /// connected. <see langword="null"/> if no connections are established.</returns>
+        [Browsable(false)]
+        public ImageViewer ImageViewer
+        {
+            get
+            {
+                return _imageViewer;
+            }
+            set
+            {
+                try
+                {
+                    // Unregister from previously subscribed-to events
+                    if (_imageViewer != null)
+                    {
+                        _imageViewer.ImageFileChanged -= HandleImageFileChanged;
+                        _imageViewer.PageChanged -= HandlePageChanged;
+                    }
+
+                    // Store the new image viewer
+                    _imageViewer = value;
+                    Reset();
+
+                    // Register for events
+                    if (_imageViewer != null)
+                    {
+                        _imageViewer.ImageFileChanged += HandleImageFileChanged;
+                        _imageViewer.PageChanged += HandlePageChanged;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new ExtractException("ELI26983",
+                        "Unable to establish connection to image viewer.", ex);
+                }
+            }
+        }
+
+        #endregion IImageViewerControl Members
+    }
+}
