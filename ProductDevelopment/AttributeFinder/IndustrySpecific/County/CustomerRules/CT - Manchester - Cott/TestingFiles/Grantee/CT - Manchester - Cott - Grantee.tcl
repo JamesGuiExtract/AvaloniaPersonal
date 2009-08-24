eed2a0c3f28ba97f1871fb5ee1c84533c9956fc8
Art_Jones <Art_Jones@extract.local>
2009-08-24 13:40:54 -0500

    ﻿ [git-vault-id] Extract$/Engineering@1996/2053

---
+++ ProductDevelopment/AttributeFinder/IndustrySpecific/County/CustomerRules/CT - Manchester - Cott/TestingFiles/Grantee/CT - Manchester - Cott - Grantee.tcl
@@ -0,0 +1,7 @@
+UCLIDAFCoreTest.AutomatedRuleSetTester.1;;CT - Manchester - Cott - assignment.dat
+UCLIDAFCoreTest.AutomatedRuleSetTester.1;;CT - Manchester - Cott - lien.dat
+UCLIDAFCoreTest.AutomatedRuleSetTester.1;;CT - Manchester - Cott - lispendens.dat
+UCLIDAFCoreTest.AutomatedRuleSetTester.1;;CT - Manchester - Cott - mortgage.dat
+UCLIDAFCoreTest.AutomatedRuleSetTester.1;;CT - Manchester - Cott - quitclaim.dat
+UCLIDAFCoreTest.AutomatedRuleSetTester.1;;CT - Manchester - Cott - release.dat
+UCLIDAFCoreTest.AutomatedRuleSetTester.1;;CT - Manchester - Cott - warranty.dat
