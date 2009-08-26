7ee4cb4684a0872df22b51a44d815b3d31ee3e7c
Jennifer_Hagebock <Jennifer_Hagebock@extract.local>
2009-08-26 09:53:02 -0500

    ﻿ [git-vault-id] Extract$/Engineering@2031/2092

---
+++ ProductDevelopment/AttributeFinder/IndustrySpecific/County/CustomerRules/CT - Manchester - Cott/TestingFiles/Grantor/Grantor.tcl
@@ -0,0 +1,6 @@
+UCLIDAFCoreTest.AutomatedRuleSetTester.1;;Grantor - Assignment.dat
+UCLIDAFCoreTest.AutomatedRuleSetTester.1;;Grantor - Lien.dat
+UCLIDAFCoreTest.AutomatedRuleSetTester.1;;Grantor - Lis Pendens.dat
+UCLIDAFCoreTest.AutomatedRuleSetTester.1;;Grantor - Quit Claim.dat
+UCLIDAFCoreTest.AutomatedRuleSetTester.1;;Grantor - Release.dat
+UCLIDAFCoreTest.AutomatedRuleSetTester.1;;Grantor - Warranty.dat
