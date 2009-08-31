de0120732b5fb34c77e84213d0b10d8b642bf9ac
Douglas_Beachy <Douglas_Beachy@extract.local>
2009-08-31 11:55:56 -0500

    ﻿ [git-vault-id] Extract$/Engineering@2183/2258

---
+++ ProductDevelopment/AttributeFinder/IndustrySpecific/Redaction/CustomerRules/CA - Alameda - FNTI/TestingFiles/CA - Alameda - FNTI - Set003 to Set008.tcl
@@ -0,0 +1,7 @@
+EXTRACTRedactionTester.IDShieldTester.1;;CA - Alameda - FNTI - Set003 to Set008.dat
+//EXTRACTRedactionTester.IDShieldTester.1;;CA - Alameda - FNTI - Set003.dat
+//EXTRACTRedactionTester.IDShieldTester.1;;CA - Alameda - FNTI - Set004.dat
+//EXTRACTRedactionTester.IDShieldTester.1;;CA - Alameda - FNTI - Set005.dat
+//EXTRACTRedactionTester.IDShieldTester.1;;CA - Alameda - FNTI - Set006.dat
+//EXTRACTRedactionTester.IDShieldTester.1;;CA - Alameda - FNTI - Set007.dat
+//EXTRACTRedactionTester.IDShieldTester.1;;CA - Alameda - FNTI - Set008.dat
---
+++ ProductDevelopment/AttributeFinder/IndustrySpecific/Redaction/CustomerRules/CA - Alameda - FNTI/TestingFiles/CA - Alameda - FNTI - Set006.tcl
@@ -0,0 +1,1 @@
+EXTRACTRedactionTester.IDShieldTester.1;;CA - Alameda - FNTI - Set006.dat
