ccd87a9ec9a5b1c8f84fe3eace071b055f543bdd
Jennifer_Hagebock <Jennifer_Hagebock@extract.local>
2009-09-03 10:48:24 -0500

    ﻿ [git-vault-id] Extract$/Engineering@2308/2431

--- ProductDevelopment/AttributeFinder/IndustrySpecific/County/CustomerRules/IN - Vigo - CSI/TestingFiles/LegalDescription/IN - Vigo - CSI - LegalDescription_Random200.tcl
+++
@@ -1,1 +0,0 @@
-UCLIDAFCoreTest.AutomatedRuleSetTester.1;;IN - Vigo - CSI - LegalDescription_Random200.dat
--- ProductDevelopment/AttributeFinder/IndustrySpecific/County/CustomerRules/IN - Vigo - CSI/TestingFiles/LegalDescription/IN - Vigo - CSI - LegalDescription_Random400.tcl
+++
@@ -1,4 +0,0 @@
-//Doc types not in top 80 percent - uncomment to test.
-//UCLIDAFCoreTest.AutomatedRuleSetTester.1;;IN - Vigo - CSI - LegalDescription - Unsupported.dat
-//All doc types- delete above lines and uncomment to test.
-UCLIDAFCoreTest.AutomatedRuleSetTester.1;;IN - Vigo - CSI - LegalDescription_Random400.dat
