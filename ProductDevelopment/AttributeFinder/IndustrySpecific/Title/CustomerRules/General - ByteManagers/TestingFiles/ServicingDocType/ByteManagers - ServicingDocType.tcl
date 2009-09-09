8cb258ee1c4bdd9c8bed0ac66c81c225aba9b2a4
Wayne_Lenius <Wayne_Lenius@extract.local>
2009-09-09 12:45:50 -0500

    ﻿ [git-vault-id] Extract$/Engineering@2525/2661

--- ProductDevelopment/AttributeFinder/IndustrySpecific/Title/CustomerRules/General - ByteManagers/TestingFiles/ServicingDocType/ByteManagers - ServicingDocType.tcl
+++
@@ -1,5 +0,0 @@
-UCLIDAFCoreTest.AutomatedRuleSetTester.1;;ByteManagers - ServicingDocType - CEA.dat
-UCLIDAFCoreTest.AutomatedRuleSetTester.1;;ByteManagers - ServicingDocType - DOT.dat
-UCLIDAFCoreTest.AutomatedRuleSetTester.1;;ByteManagers - ServicingDocType - FED.dat
-
-UCLIDAFCoreTest.AutomatedRuleSetTester.1;;ByteManagers - ServicingDocType.dat
--- ProductDevelopment/AttributeFinder/IndustrySpecific/Title/CustomerRules/General - ByteManagers/TestingFiles/UnderwritingDocType/ByteManagers - UnderwritingDocType.tcl
+++
@@ -1,5 +0,0 @@
-UCLIDAFCoreTest.AutomatedRuleSetTester.1;;ByteManagers - UnderwritingDocType - Borrower-Loan Information.dat
-UCLIDAFCoreTest.AutomatedRuleSetTester.1;;ByteManagers - UnderwritingDocType - Income-Employment.dat
-UCLIDAFCoreTest.AutomatedRuleSetTester.1;;ByteManagers - UnderwritingDocType - Lender Disclosures.dat
-
-UCLIDAFCoreTest.AutomatedRuleSetTester.1;;ByteManagers - UnderwritingDocType.dat
