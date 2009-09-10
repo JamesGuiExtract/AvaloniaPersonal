ceebcbead4246e70a370c317d385ba5ddd7b81f6
Wayne_Lenius <Wayne_Lenius@extract.local>
2009-09-10 08:44:16 -0500

    ﻿ [git-vault-id] Extract$/Engineering@2604/2741

--- ProductDevelopment/AttributeFinder/IndustrySpecific/Redaction/CustomerRules/DSSG/TestingFiles/DSSG.tcl
+++
@@ -1,5 +0,0 @@
-EXTRACTRedactionTester.IDShieldTester.1;;Set001.dat
-EXTRACTRedactionTester.IDShieldTester.1;;Residential.dat
-EXTRACTRedactionTester.IDShieldTester.1;;ServiceTickets.dat
-EXTRACTRedactionTester.IDShieldTester.1;;SLPFiles.dat
-EXTRACTRedactionTester.IDShieldTester.1;;SubFiles.dat
--- ProductDevelopment/AttributeFinder/IndustrySpecific/Redaction/CustomerRules/DSSG/TestingFiles/Set003.tcl
+++
@@ -1,6 +0,0 @@
-EXTRACTRedactionTester.IDShieldTester.1;;Set003.dat
-EXTRACTRedactionTester.IDShieldTester.1;;Set003 - SSN.dat
-EXTRACTRedactionTester.IDShieldTester.1;;Set003 - CCN.dat
-EXTRACTRedactionTester.IDShieldTester.1;;Set003 - DOB.dat
-EXTRACTRedactionTester.IDShieldTester.1;;Set003 - Bank.dat
-EXTRACTRedactionTester.IDShieldTester.1;;Set003 - TaxID.dat
