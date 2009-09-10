3a0f6a9118605a7f78bd36d79037fc51973677aa
Wayne_Lenius <Wayne_Lenius@extract.local>
2009-09-10 08:55:24 -0500

    ﻿ [git-vault-id] Extract$/Engineering@2620/2757

--- ProductDevelopment/AttributeFinder/IndustrySpecific/Redaction/CustomerRules/IN - District Rules - ACS/TestingFiles/IN - District Rules - ACS - ALL.tcl
+++
@@ -1,6 +0,0 @@
-EXTRACTRedactionTester.IDShieldTester.1;;IN - District Rules - ACS - EastCentral.dat
-EXTRACTRedactionTester.IDShieldTester.1;;IN - District Rules - ACS - NorthEast.dat
-EXTRACTRedactionTester.IDShieldTester.1;;IN - District Rules - ACS - NorthWest.dat
-EXTRACTRedactionTester.IDShieldTester.1;;IN - District Rules - ACS - SouthEast.dat
-EXTRACTRedactionTester.IDShieldTester.1;;IN - District Rules - ACS - SouthWest.dat
-EXTRACTRedactionTester.IDShieldTester.1;;IN - District Rules - ACS - WestCentral.dat
--- ProductDevelopment/AttributeFinder/IndustrySpecific/Redaction/CustomerRules/IN - District Rules - ACS/TestingFiles/IN - District Rules - ACS - NorthWest.tcl
+++
@@ -1,1 +0,0 @@
-EXTRACTRedactionTester.IDShieldTester.1;;IN - District Rules - ACS - NorthWest.dat
