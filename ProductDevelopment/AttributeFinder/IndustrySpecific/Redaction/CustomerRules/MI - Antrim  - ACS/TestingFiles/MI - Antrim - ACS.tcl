be01884ddf1a6389e6c2345137a98a1f5a703f93
Wayne_Lenius <Wayne_Lenius@extract.local>
2009-09-10 09:01:48 -0500

    ﻿ [git-vault-id] Extract$/Engineering@2642/2779

--- ProductDevelopment/AttributeFinder/IndustrySpecific/Redaction/CustomerRules/MI - Antrim  - ACS/TestingFiles/MI - Antrim - ACS.tcl
+++
@@ -1,6 +0,0 @@
-EXTRACTRedactionTester.IDShieldTester.1;;MI - Antrim - ACS.dat
-
-//Doc types not in top 80 percent - uncomment to test.
-//EXTRACTRedactionTester.IDShieldTester.1;;MI - Antrim - ACS - Unsupported.dat
-//All doc types- delete above lines and uncomment to test.
-//EXTRACTRedactionTester.IDShieldTester.1;;MI - Antrim - ACS.dat
