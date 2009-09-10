70065e4cdaf38910adf7a1ff69ad2994224110bc
Wayne_Lenius <Wayne_Lenius@extract.local>
2009-09-10 08:45:09 -0500

    ﻿ [git-vault-id] Extract$/Engineering@2608/2745

--- ProductDevelopment/AttributeFinder/IndustrySpecific/Redaction/CustomerRules/IA - Dubuque - Cott/TestingFiles/IA - Dubuque - Cott.tcl
+++
@@ -1,6 +0,0 @@
-EXTRACTRedactionTester.IDShieldTester.1;;IA - Dubuque - Cott.dat
-
-//Doc types not in top 80 percent - uncomment to test.
-//EXTRACTRedactionTester.IDShieldTester.1;;IA - Dubuque - Cott - Unsupported.dat
-//All doc types- delete above lines and uncomment to test.
-//EXTRACTRedactionTester.IDShieldTester.1;;IA - Dubuque - Cott.dat
