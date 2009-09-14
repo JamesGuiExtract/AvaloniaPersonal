461a438b5c7baebc9a6d0c7afe4f5f49ac2e19e6
Nathan_Figueroa <Nathan_Figueroa@extract.local>
2009-09-14 13:30:58 -0500

    ﻿Redactions in the RedactionGridView are sorted spatially. [FIDSC #3666] Reviewed by Jeff and Steve. [git-vault-id] Extract$/Engineering@2904/3051

---
+++ RC.Net/Redaction/Verification/Core/Code/RedactionGridViewRowComparer.cs
@@ -0,0 +1,86 @@
+using Extract.AttributeFinder;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extract.Redaction.Verification
+{
+    /// <summary>
+    /// Represents a comparison operation between two <see cref="RedactionGridViewRow"/>s.
+    /// </summary>
+    public class RedactionGridViewRowComparer : IComparer, IComparer<RedactionGridViewRow>
+    {
+        #region RedactionGridViewComparer Fields
+
+        /// <summary>
+        /// Spatially compares COM attributes.
+        /// </summary>
+        readonly AttributeComparer _attributeComparer = new AttributeComparer();
+
+        #endregion RedactionGridViewComparer Fields
+
+        #region RedactionGridViewComparer Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RedactionGridViewRowComparer"/> class.
+        /// </summary>
+        public RedactionGridViewRowComparer()
+        {
+        }
+
+        #endregion RedactionGridViewComparer Constructors
+
+        #region IComparer Members
+
+        /// <summary>
+        /// Compares two objects.
+        /// </summary>
+        /// <param name="x">The first object to compare.</param>
+        /// <param name="y">The second object to compare.</param>
+        /// <returns>Less than zero if <paramref name="x"/> is less than <paramref name="y"/>;
+        /// zero if <paramref name="x"/> equals <paramref name="y"/>; greater than zero if 
+        /// <paramref name="x"/> is greater than <paramref name="y"/>.</returns>
+        public int Compare(object x, object y)
+        {
+            return Compare(x as RedactionGridViewRow, y as RedactionGridViewRow);
+        }
+
+        #endregion IComparer Members
+
+        #region IComparer<RedactionGridViewRow> Members
+
+        /// <summary>
+        /// Compares two objects and returns a value indicating whether one is less than, equal to, 
+        /// or greater than the other.
+        /// </summary>
+        /// <param name="x">The first object to compare.</param>
+        /// <param name="y">The second object to compare.</param>
+        /// <returns>Less than zero if <paramref name="x"/> is less than <paramref name="y"/>;
+        /// zero if <paramref name="x"/> equals <paramref name="y"/>; greater than zero if 
+        /// <paramref name="x"/> is greater than <paramref name="y"/>.</returns>
+        public int Compare(RedactionGridViewRow x, RedactionGridViewRow y)
+        {
+            // If these are the same object, they are equal
+            if (RedactionGridViewRow.ReferenceEquals(x, y))
+            {
+                return 0;
+            }
+
+            // null comes before non-null
+            if (x == null)
+            {
+                return -1;
+            }
+            else if (y == null)
+            {
+                return 1;
+            }
+
+            // Compare the attributes spatially
+            return _attributeComparer.Compare(x.ComAttribute, y.ComAttribute);
+        }
+
+        #endregion IComparer<RedactionGridViewRow> Members
+    }
+}
