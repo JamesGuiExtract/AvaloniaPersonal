b812ab98425e1f02b6e3db889e9f6d475ece30ed
Jeff_Shergalis <Jeff_Shergalis@extract.local>
2009-09-25 13:42:36 -0500

    ﻿Checked in framework for new Apply Bates Number task
    Reviewed by:
    Nathan/1/0
    William/0/0
    [git-vault-id] Extract$/Engineering@3403/3566

---
+++ RC.Net/FileActionManager/FileProcessors/Core/Code/ApplyBatesNumberTask.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Extract.FileActionManager.FileProcessors
+{
+    /// <summary>
+    /// Represents a file processing task that applies Bates numbers to documents.
+    /// </summary>
+    [ComVisible(true)]
+    [Guid("8DD63918-D299-48DB-BA54-FD1CFAAAF0E2")]
+    [ProgId("Extract.FileActionManager.FileProcessors.ApplyBatesNumberTask")]
+    public class ApplyBatesNumberTask
+    {
+    }
+}
