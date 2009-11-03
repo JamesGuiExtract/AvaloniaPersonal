ef00e425ebf98fd1ba254da3d83b6731f5ee8c56
Art_Jones <Art_Jones@extract.local>
2009-11-03 10:23:40 -0600

    ﻿ [git-vault-id] Extract$/Engineering@4645/4836

--- ProductDevelopment/AttributeFinder/AFConditions/AutomatedTest/DocTypeCondition/ExecuteTest_1.bat
+++ ProductDevelopment/AttributeFinder/AFConditions/AutomatedTest/DocTypeCondition/ExecuteTest_1.bat
@@ -1,11 +1,11 @@
 REM Clean Source folder
-call Clean.bat
+call Clean.bat Test_1
 
 REM Supply Numbered Files for 4 hours
-START CopyNumberedFiles "..\..\..\AFCore\AutomatedTest\Images\Image1.tif.uss" ".\Source" 3s -h4
+START I:\Common\Engineering\Tools\Utils\CopyNumberedFiles\CopyNumberedFiles.exe "..\..\..\AFCore\AutomatedTest\Images\Image1.tif.uss" ".\Source" 3s -h4
 
 REM Execute command-line for desired test
 START ProcessFiles.exe MemoryLeak_1.fps /s
 
 REM Start Logging Statistics to numbered subfolder
-LogProcessStats ProcessFiles 5s .\Stats\Test_1 /el
+LogProcessStats.exe ProcessFiles 5s .\Stats\Test_1 /el
