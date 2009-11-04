536a65c83734f150eb0f851769eb37762a9c27af
Art_Jones <Art_Jones@extract.local>
2009-11-04 10:39:14 -0600

    ﻿ [git-vault-id] Extract$/Engineering@4707/4898

--- ProductDevelopment/AttributeFinder/AFOutputHandlers/AutomatedTest/RemoveAttributes/ExecuteTest_1.bat
+++ ProductDevelopment/AttributeFinder/AFOutputHandlers/AutomatedTest/RemoveAttributes/ExecuteTest_1.bat
@@ -2,7 +2,8 @@ REM Clean Source folder
 call Clean.bat Test_1
 
 REM Supply Numbered Files for 4 hours
-START CopyNumberedFiles "..\..\..\AFCore\AutomatedTest\Images\Image1.tif.uss" ".\Source" 10s -h4
+START CopyNumberedFiles "..\..\..\AFCore\AutomatedTest\Images\Image1.tif" ".\Source" 10s -h4
+::START CopyNumberedFiles "..\..\..\AFCore\AutomatedTest\Images\Image1.tif.uss" ".\Source" 10s -h4
 
 REM Execute command-line for desired test
 START ProcessFiles.exe MemoryLeak_1.fps /s
