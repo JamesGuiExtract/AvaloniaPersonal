d660534e77e81f5f4868b46e94babc2e04478b77
Nathan_Figueroa <Nathan_Figueroa@extract.local>
2009-11-30 14:57:20 -0600

    ﻿Imported libraries in project build order. Reviewed by Steve and William. [git-vault-id] Extract$/Engineering@5377/5571

--- ProductDevelopment/AttributeFinder/IndustrySpecific/Redaction/RedactionTester/Code/stdafx.h
+++ ProductDevelopment/AttributeFinder/IndustrySpecific/Redaction/RedactionTester/Code/stdafx.h
@@ -48,18 +48,18 @@
 
 using namespace ATL;
 
+#import "..\..\..\..\..\..\ReusableComponents\COMComponents\UCLIDComponentsLM\COMLM\Code\COMLM.tlb" named_guids 
+using namespace UCLID_COMLMLib;
+
 #import "..\..\..\..\..\..\ReusableComponents\COMComponents\UCLIDCOMUtils\Core\Code\UCLIDCOMUtils.tlb" named_guids
 using namespace UCLID_COMUTILSLib;
 
-#import "..\..\..\..\..\..\ReusableComponents\COMComponents\UCLIDComponentsLM\COMLM\Code\COMLM.tlb" named_guids 
-using namespace UCLID_COMLMLib;
+#import "..\..\..\..\..\..\ReusableComponents\COMComponents\UCLIDTestingFramework\Interfaces\Code\UCLIDTestingFramework.tlb" named_guids 
+using namespace UCLID_TESTINGFRAMEWORKINTERFACESLib;
 
 #import "..\..\..\..\..\..\ReusableComponents\COMComponents\UCLIDRasterAndOCRMgmt\Core\Code\UCLIDRasterAndOCRMgmt.tlb" named_guids \
 	rename("LoadImage", "LoadRasterImage")
 using namespace UCLID_RASTERANDOCRMGMTLib;
-
-#import "..\..\..\..\..\..\ReusableComponents\COMComponents\UCLIDTestingFramework\Interfaces\Code\UCLIDTestingFramework.tlb" named_guids 
-using namespace UCLID_TESTINGFRAMEWORKINTERFACESLib;
 
 #import "..\..\..\..\..\..\ReusableComponents\InputFunnel\IFCore\Code\IFCore.tlb" named_guids
 using namespace UCLID_INPUTFUNNELLib;
