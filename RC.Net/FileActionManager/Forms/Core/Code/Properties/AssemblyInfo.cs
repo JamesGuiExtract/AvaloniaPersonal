0c9712d4d22612e8b8e998cf1db9e964c76fef3f
Jeff_Shergalis <Jeff_Shergalis@extract.local>
2009-12-03 10:33:08 -0600

    ﻿Modified as per DNRCAU #336  - Created new project for the FileTagsButton Extract.FileActionManager.Forms
    Reviewed by:
    Nathan/0/0
    Steve/0/0
    [git-vault-id] Extract$/Engineering@5459/5653

---
+++ RC.Net/FileActionManager/Forms/Core/Code/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Extract.FileActionManager.Forms")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Extract Systems")]
+[assembly: AssemblyProduct("Extract.FileActionManager.Forms")]
+[assembly: AssemblyCopyright("Copyright © Extract Systems 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Mark assembly as CLS compliant.
+[assembly: CLSCompliant(true)]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e347470d-aebb-4ad8-bd97-bb591e607e98")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
