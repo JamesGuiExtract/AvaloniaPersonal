c3b3c2d24cc95bc951e7022adb6fa82eb6f2bdb2
Nathan_Figueroa <Nathan_Figueroa@extract.local>
2010-01-11 11:45:34 -0600

    ﻿Updated RegisterAll.bat to provide more user feedback. [FIDSC #3917] Added /s flag to run batch file without prompts or message statements. Reviewed by William and Jeff. [git-vault-id] Extract$/Engineering@6330/6672

--- ProductDevelopment/Common/RegisterAll.bat
+++ ProductDevelopment/Common/RegisterAll.bat
@@ -1,18 +1,39 @@
 @echo off
-@REM This batch file will step through all subdirectories of the parent folder and register all files lsited
-@REM in files with the extension rl
-@REM Set path so CommonComponents is on the path ( assumes this files is in commoncomponents directory)
-@SET PATH=%PATH%;%~p0;%~p0..\FlexIndexComponents\Bin;%~p0..\InputFunnelComponents\Bin;%~p0..\IcoMap for ArcGIS\Bin
-@for /R ..\ %%r in (*.rl,*.nl) do (
-	@for /F "tokens=1 delims=," %%i in (%%~sr) do (
-		@if "%%~xr" == ".rl" (
-			@if "%%~xi" == ".exe" (
+::This batch file will step through all subdirectories of the parent folder and register all C++ files 
+::listed in files with the extension rl & all .Net assemblies listed in files with the extension nl
+
+::Set path so CommonComponents is on the path (assumes these files are in commoncomponents directory)
+set PATH=%PATH%;%~p0;%~p0..\FlexIndexComponents\Bin;%~p0..\InputFunnelComponents\Bin;%~p0..\IcoMap for ArcGIS\Bin
+
+if not [%1]==[/s] (
+	echo Press any key to start file registration process...
+	pause > nul
+	echo.
+)
+
+for /R ..\ %%r in (*.rl,*.nl) do (
+	for /F "tokens=1 delims=," %%i in (%%~sr) do (
+		if "%%~xr" == ".rl" (
+			if not [%1]==[/s] (
+				echo Registering %%~dpr%%i...
+			)
+			if "%%~xi" == ".exe" (
 				"%%~dpr%%i" /RegServer
 			) else if not "%%~xi" == ".rl" (
 				regsvr32 "%%~dpr%%i" /s
 			)
 		) else if "%%~xr" == ".nl" (
+			if not [%1]==[/s] (
+				echo Registering %%~dpr%%i...
+			)
 			"%WINDIR%\Microsoft.NET\Framework\v2.0.50727\regasm" "%%~dpr%%i" /silent
 		)
 	)
+)
+
+if not [%1]==[/s] (
+	echo.
+	echo File registration process complete. Press any key to continue...
+	pause > nul
+
 )
