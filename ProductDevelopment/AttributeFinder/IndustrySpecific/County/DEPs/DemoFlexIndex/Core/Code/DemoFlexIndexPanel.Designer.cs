5d80e467e2db33c4fe0f41d27911062decab01d5
Steve_Kurth <Steve_Kurth@extract.local>
2010-01-14 11:52:52 -0600

    ﻿ [git-vault-id] Extract$/Engineering@6409/6763

---
+++ ProductDevelopment/AttributeFinder/IndustrySpecific/County/DEPs/DemoFlexIndex/Core/Code/DemoFlexIndexPanel.Designer.cs
@@ -0,0 +1,53 @@
+namespace Extract.DataEntry.DEPs.DemoFlexIndex
+{
+    partial class DemoFlexIndexPanel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Extract.DataEntry.HighlightColor highlightColor1 = new Extract.DataEntry.HighlightColor();
+            Extract.DataEntry.HighlightColor highlightColor2 = new Extract.DataEntry.HighlightColor();
+            this.SuspendLayout();
+            // 
+            // DemoFlexIndexPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            highlightColor1.Color = System.Drawing.Color.LightSalmon;
+            highlightColor1.MaxOcrConfidence = 89;
+            highlightColor2.Color = System.Drawing.Color.LightGreen;
+            highlightColor2.MaxOcrConfidence = 100;
+            this.HighlightColors = new Extract.DataEntry.HighlightColor[] {
+        highlightColor1,
+        highlightColor2};
+            this.Name = "DemoFlexIndexPanel";
+            this.Size = new System.Drawing.Size(430, 540);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ ProductDevelopment/AttributeFinder/IndustrySpecific/County/DEPs/DemoFlexIndex/Core/Code/DemoFlexIndexPanel.cs
@@ -0,0 +1,52 @@
+using Extract.DataEntry;
+using Extract.Licensing;
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Extract.DataEntry.DEPs.DemoFlexIndex
+{
+    /// <summary>
+    /// A sample <see cref="DataEntryControlHost"/> intended to demonstrate data entry functionality
+    /// in the county document industry.
+    /// </summary>
+    public partial class DemoFlexIndexPanel : DataEntryControlHost
+    {
+        #region Constants
+
+        /// <summary>
+        /// The name of the object to be used in the validate license calls.
+        /// </summary>
+        static readonly string _OBJECT_NAME = typeof(DemoFlexIndexPanel).ToString();
+
+        #endregion Constants
+
+        /// <summary>
+        /// Initializes a new <see cref="DemoFlexIndexPanel"/> instance.
+        /// </summary>
+        public DemoFlexIndexPanel()
+            : base()
+        {
+            try
+            {
+                if (LicenseManager.UsageMode == LicenseUsageMode.Designtime)
+                {
+                    // Load the license files from folder
+                    LicenseUtilities.LoadLicenseFilesFromFolder(0, new MapLabel());
+                }
+
+                // Validate the license
+                LicenseUtilities.ValidateLicense(
+                    LicenseIdName.LabDEVerificationUIObject, "ELI29164", _OBJECT_NAME);
+
+                InitializeComponent();
+            }
+            catch (Exception ex)
+            {
+                throw ExtractException.AsExtractException("ELI29165", ex);
+            }
+        }
+    }
+}
