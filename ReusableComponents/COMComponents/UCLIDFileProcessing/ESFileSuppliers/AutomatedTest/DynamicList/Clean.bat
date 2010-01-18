aac804cdfa5b33e5643aacadeee7c5357d4f7853
Art_Jones <Art_Jones@extract.local>
2010-01-18 09:52:43 -0600

    ﻿ [git-vault-id] Extract$/Engineering@6480/6834

--- ReusableComponents/COMComponents/UCLIDFileProcessing/ESFileSuppliers/AutomatedTest/DynamicList/Clean.bat
+++ ReusableComponents/COMComponents/UCLIDFileProcessing/ESFileSuppliers/AutomatedTest/DynamicList/Clean.bat
@@ -1,7 +1,8 @@
 echo off
-del /q .\Source\One\*.*
-del /q .\Source\Two\*.*
-del /q .\Source\Three\*.*
-del /q .\Source\Four\*.*
-del /q .\Source\Five\*.*
-del /q .\Source\Six\*.*
+del /S /F /q .\Source\One\*.*
+del /S /F /q .\Source\Two\*.*
+del /S /F /q .\Source\Three\*.*
+del /S /F /q .\Source\Four\*.*
+del /S /F /q .\Source\Five\*.*
+del /S /F /q .\Source\Six\*.*
+del /S /F /q .\Stats\%1\*.*
