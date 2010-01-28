866f34512a6e394ce12c04b81b130b8f58feeeb0
Nathaniel_Heyer <Nathaniel_Heyer@extract.local>
2010-01-28 17:17:05 -0600

    ﻿ [git-vault-id] Extract$/Engineering@7054/7431

--- ProductDevelopment/AttributeFinder/AFValueFinders/AutomatedTest/AddressFinder/ExecuteTest.bat
+++ ProductDevelopment/AttributeFinder/AFValueFinders/AutomatedTest/AddressFinder/ExecuteTest.bat
@@ -1,3 +1,8 @@
+::Set up PATH
+if defined programfiles(x86) set programfiles=%programfiles(x86)%
+
+set path=path;%programfiles%\extract systems\commoncomponents
+
 REM Clean Source folder
 call Clean.bat
 
