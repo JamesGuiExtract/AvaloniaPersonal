85ce86c3cca62b54a365c9a04b3d4a8ac03632db
Art_Jones <Art_Jones@extract.local>
2010-02-02 09:59:19 -0600

    ﻿ [git-vault-id] Extract$/Engineering@7211/7590

--- ProductDevelopment/AttributeFinder/AFValueFinders/AutomatedTest/AddressFinder/ExecuteTest.bat
+++ ProductDevelopment/AttributeFinder/AFValueFinders/AutomatedTest/AddressFinder/ExecuteTest.bat
@@ -4,7 +4,7 @@ if defined programfiles(x86) set programfiles=%programfiles(x86)%
 set path=path;%programfiles%\extract systems\commoncomponents
 
 REM Clean Source folder
-call Clean.bat
+call Clean.bat Test_1
 
 REM Supply Numbered Files for 4 hours
 START CopyNumberedFiles "..\..\..\AFCore\AutomatedTest\Images\Image1.tif.uss" ".\Source" 400ms -h4
