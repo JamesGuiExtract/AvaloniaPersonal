a707d16913621ac4023dcadee7482deb5b4473d7
Joanna_Lee <Joanna_Lee@extract.local>
2010-02-04 14:26:34 -0600

    ﻿ [git-vault-id] Extract$/Engineering@7332/7712

---
+++ ProductDevelopment/AttributeFinder/AFConditions/AutomatedTest/CharacterConfidenceCondition/Clean.bat
@@ -0,0 +1,3 @@
+@echo off
+del /q .\Source\*.*
+del /S /F /q .\Stats\%1\*.*
---
+++ ProductDevelopment/AttributeFinder/AFConditions/AutomatedTest/CharacterConfidenceCondition/ExecuteTest.bat
@@ -0,0 +1,11 @@
+REM Clean Source folder
+call Clean.bat Test_1
+
+REM Supply Numbered Files for 4 hours
+START I:\Common\Engineering\Tools\Utils\CopyNumberedFiles\CopyNumberedFiles.exe "..\..\..\AFCore\AutomatedTest\Images\Image3.tif.uss" ".\Source" 300ms -h4
+
+REM Execute command-line for desired test
+START ProcessFiles.exe MemoryLeak.fps /s
+
+REM Start Logging Statistics to numbered subfolder
+LogProcessStats.exe ProcessFiles 5s .\Stats\Test_1 /el
