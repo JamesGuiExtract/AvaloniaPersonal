a2da169f73b22b7d9f5aec2754a9742dc8fe14d8
Art_Jones <Art_Jones@extract.local>
2010-02-04 16:24:05 -0600

    ﻿ [git-vault-id] Extract$/Engineering@7357/7737

--- ProductDevelopment/AttributeFinder/AFValueFinders/AutomatedTest/CheckFinder/ExecuteMemoryLeakTest.bat
+++ ProductDevelopment/AttributeFinder/AFValueFinders/AutomatedTest/CheckFinder/ExecuteMemoryLeakTest.bat
@@ -5,8 +5,8 @@ if not exist "%~dp0\TestArea" mkdir "%~dp0\TestArea\"
 REM Clean Source folder
 call Clean.bat Test_1
 
-REM Supply numbered file sets every 6 minutes for 8 hours
-START CopyNumberedSets Images TestArea 360 8
+REM Supply numbered file sets every 8 seconds for 4 hours
+START CopyNumberedSets Images TestArea 8 4
 
 REM Execute command-line for desired test
 START ProcessFiles.exe MemoryLeak.fps /s
