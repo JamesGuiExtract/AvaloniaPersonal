d0646cc635b682b13fcf436f5b8602308d9dbd74
Jeff_Shergalis <Jeff_Shergalis@extract.local>
2010-02-10 11:49:18 -0600

    ﻿Added memory leak test for ModifyPdfFile Fixed issue in the ApplyBatesNumber memory leak test file [git-vault-id] Extract$/Engineering@7470/7856

--- RC.Net/FileActionManager/FileProcessors/Core/Testing/Manual/MemoryLeak/ModifyPdfFile/ExecuteTest_1.bat
+++ RC.Net/FileActionManager/FileProcessors/Core/Testing/Manual/MemoryLeak/ModifyPdfFile/ExecuteTest_1.bat
@@ -8,10 +8,10 @@ if defined programfiles(x86) set programfiles=%programfiles(x86)%
 set CommonComponentsDir=%programfiles%\Extract Systems\CommonComponents
 
 :: Supply Numbered Files for 4 hours
-START I:\Common\Engineering\Tools\Utils\CopyNumberedFiles\CopyNumberedFiles.exe ".\4PageTif.tif" ".\Source" 500ms -h4
+START I:\Common\Engineering\Tools\Utils\CopyNumberedFiles\CopyNumberedFiles.exe ".\TestImage001.annotated.pdf" ".\Source" 500ms -h4
 
 :: Execute command-line for desired test
-START "%CommonComponentsDir%\ProcessFiles.exe" 1_ApplyBatesNewNumberEachDocumentTif.fps /s
+START "%CommonComponentsDir%\ProcessFiles.exe" 1_ModifyPdfFile.fps /s
 
 :: Start Logging Statistics every minute to a numbered subfolder
 "%CommonComponentsDir%\LogProcessStats" ProcessFiles 1m .\Stats\Test_1 /el
