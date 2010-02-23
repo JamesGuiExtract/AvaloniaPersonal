e2842ea2a5df0a8f015e069713a2cee9d8ff9468
Steve_Kurth <Steve_Kurth@extract.local>
2010-02-23 14:49:04 -0600

    ﻿[FlexIDSCore:3643] All instances where getRegExpFromFile is used have been converted to use CachedObjectFromFile with RegExLoader instead so that changes files where RegEx's are loaded from are automatically updated as rules are running. I also incorporated into CachedObjectFromFile the ability to auto-encrypt since most locations that are using CachedObjectFromFile were performing auto-encryption just prior to the loadObjectFromFile called.
    [FlexIDSCore:4048]
    Added defaults for IDS Shield Statistics Reporter query for automated redaction and verification condition settings.
    
    Reviewed by:
    Nathan (1/0)
    [git-vault-id] Extract$/Engineering@7793/8185

--- ProductDevelopment/AttributeFinder/AFCore/Code/RegExLoader.cpp
+++ ProductDevelopment/AttributeFinder/AFCore/Code/RegExLoader.cpp
@@ -1,15 +1,172 @@
 #include "stdafx.h"
 #include "RegExLoader.h"
-#include "Common.h"
 
-#include <Misc.h>
+#include "CommentedTextFileReader.h"
+#include "Misc.h"
+#include "UCLIDException.h"
+#include "EncryptedFileManager.h"
+#include "StringTokenizer.h"
 
+//-------------------------------------------------------------------------------------------------
+// Consts
+//-------------------------------------------------------------------------------------------------
+const static string gstrIMPORT = "#import";
+
+//--------------------------------------------------------------------------------------------------
+// Local Methods
+//--------------------------------------------------------------------------------------------------
+string getRegExpFromFile(const string& strFilename);
+void convertFileToLines(const string& strInput, bool bIsFilename, vector<string>& rvecLines)
+{
+	if (bIsFilename)
+	{
+		// first make sure the file exists
+		if (!isValidFile(strInput))
+		{
+			UCLIDException ue("ELI07158", "Input file doesn't exist.");
+			ue.addDebugInfo("Input File", strInput);
+			ue.addWin32ErrorInfo();
+			throw ue;
+		}
+
+		// If input file is an etf file
+		if (getExtensionFromFullPath(strInput, true) == ".etf")
+		{
+			// Open an input file, which is encrypted
+			EncryptedFileManager efm;
+			// decrypt the file
+			rvecLines = efm.decryptTextFile(strInput);
+		}
+		else
+		{
+			// treat the input file as ASCII text file
+			ifstream ifs(strInput.c_str());
+			
+			while (!ifs.eof())
+			{
+				string strLine("");
+
+				getline(ifs, strLine);
+
+				if (strLine.empty())
+				{
+					continue;
+				}
+
+				// save the line in the vector
+				rvecLines.push_back(strLine);
+			}
+		}
+	}
+	else	// if input is a block of text
+	{
+		// delimiter is line feed
+		StringTokenizer::sGetTokens(strInput, "\r\n", rvecLines);
+	}
+}
+//-------------------------------------------------------------------------------------------------
+vector<string> convertFileToLines(const string& strFilename)
+{
+	vector<string> vecLines;
+	convertFileToLines( strFilename, true, vecLines );
+	return vecLines;
+}
+//--------------------------------------------------------------------------------------------------
+string getRegExpFromLines(CommentedTextFileReader& ctfr, const string& strRootFile,
+						  bool bAutoEncrypt, const string& strAutoEncryptKey)
+{
+	string strRegExp = "";
+	vector<string> vecLineNoComments;
+	while ( !ctfr.reachedEndOfStream())
+	{
+		string strCurrent = ctfr.getLineText();
+		// check for #import statements
+		unsigned long ulStartImport = strCurrent.find(gstrIMPORT);
+		if (ulStartImport != string::npos)
+		{
+			unsigned long ulStartFileName = strCurrent.find_first_not_of(" \t", 
+				ulStartImport + gstrIMPORT.size());
+			if (ulStartFileName == string::npos)
+			{
+				UCLIDException ue("ELI09752", "Invalid #import statememt.");
+				ue.addDebugInfo("string", strCurrent);
+				throw ue;
+			}
+
+			// if no root folder is specified then #import statements are invalid
+			// and we throw an exception
+			if (strRootFile == "\\")
+			{
+				UCLIDException ue("ELI10018", "#import specified with no root folder.");
+				throw ue;
+			}
+
+			string strRelName = strCurrent.substr(ulStartFileName);
+			string strFullFileName = getAbsoluteFileName(strRootFile, strRelName, false);
+			// AutoEncrypt if necessary
+			if (bAutoEncrypt)
+			{
+				autoEncryptFile(strFullFileName, strAutoEncryptKey);
+			}
+			string strExp = getRegExpFromFile(strFullFileName);
+			// include anything on the line before the #import
+			string strLineBegin = strCurrent.substr(0, ulStartImport);
+			vecLineNoComments.push_back(strLineBegin + strExp);
+		}
+		else if ( !strCurrent.empty())
+		{
+			vecLineNoComments.push_back( strCurrent );
+		}
+	}
+	// Build the regular expression string, removing any whitespace( \f\n\r\t\v)
+	strRegExp = asString(vecLineNoComments, true );
+
+	return strRegExp;
+}
+//--------------------------------------------------------------------------------------------------
+string getRegExpFromFile(const string& strFilename)
+{
+	// String to contain the return value
+	string strRegExp = "";
+
+	vector<string> vecLines;
+	convertFileToLines(strFilename, true, vecLines);
+	
+	// Extract comments from the loaded vector of lines
+	CommentedTextFileReader ctfr( vecLines, "//");
+	strRegExp = getRegExpFromLines(ctfr, strFilename, false, "");
+
+	return strRegExp;
+}
+
+//-------------------------------------------------------------------------------------------------
+// Public Exported Methods
+//-------------------------------------------------------------------------------------------------
+string getRegExpFromText(const string& strText, const string& strRootFolder,
+						 bool bAutoEncrypt/* = false*/, const string& strAutoEncryptKey)
+{
+	// String to contain the return value
+	string strRegExp = "";
+
+	vector<string> vecLines;
+	convertFileToLines(strText, false, vecLines);
+	
+	// Extract comments from the loaded vector of lines
+	CommentedTextFileReader ctfr( vecLines, "//");
+	strRegExp = getRegExpFromLines(ctfr, strRootFolder + "\\", bAutoEncrypt, strAutoEncryptKey);
+
+	return strRegExp;
+}
+
+//--------------------------------------------------------------------------------------------------
+// RegExLoader Class
+//--------------------------------------------------------------------------------------------------
 RegExLoader::RegExLoader()
 {
 }
 //--------------------------------------------------------------------------------------------------
 void RegExLoader::loadObjectFromFile(string& strRegEx, const string& strFileName)
 {
-	strRegEx = getRegExpFromFile(strFileName, true, gstrAF_AUTO_ENCRYPT_KEY_PATH);
+	strRegEx = getRegExpFromFile(strFileName);
 }
 //--------------------------------------------------------------------------------------------------
--- ProductDevelopment/AttributeFinder/AFCore/Code/RegExLoader.h
+++ ProductDevelopment/AttributeFinder/AFCore/Code/RegExLoader.h
@@ -1,10 +1,16 @@
 #pragma once
 
+#include "BaseUtils.h"
+
 #include <string>
+#include <vector>
 
 using namespace std;
 
-class RegExLoader
+//-------------------------------------------------------------------------------------------------
+// RegExLoader Class
+//-------------------------------------------------------------------------------------------------
+class EXPORT_BaseUtils RegExLoader
 {
 public:
 	RegExLoader(); // Constructor
@@ -13,3 +19,16 @@ public:
 	// The input file is auto-encrypted if out of date and if specified by the RDT settings.
 	void loadObjectFromFile(string& strRegEx, const string& strFileName);
 };
+
+//-------------------------------------------------------------------------------------------------
+// Public Exported Methods
+//-------------------------------------------------------------------------------------------------
+// PURPOSE: Load a regular expression from a string of text that is in the format of a regular
+//			Expression file.
+// REQUIRE: - strText must be valid regular expression file text as defined above.
+//			- strRootFolder must exist if any #import statements are used in strText
+//			- filename must either be absolute or relative to strRootFolder
+//			
+EXPORT_BaseUtils string getRegExpFromText(const string& strText, const string& strRootFolder,
+								bool bAutoEncrypt = false, const string& strAutoEncryptKey = "");
+//-------------------------------------------------------------------------------------------------
