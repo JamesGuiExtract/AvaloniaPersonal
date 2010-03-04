5811613459a20786bacb6115fd129cd755b517aa
Joanna_Lee <Joanna_Lee@extract.local>
2010-03-04 09:28:02 -0600

    ﻿ [git-vault-id] Extract$/Engineering@8061/8489

---
+++ ReusableComponents/COMComponents/UCLIDFileProcessing/AutomatedTest/MemoryLeak/ConvertToSearchablePDF/ExecuteTest_5.bat
@@ -0,0 +1,11 @@
+REM Clean Source folder
+call Clean.bat
+
+REM Supply Numbered Files for 6 hours
+START CopyNumberedFiles ".\0001.tif" ".\Source" 5 -h6
+
+REM Execute command-line for desired test
+START ProcessFiles.exe 5.fps /s
+
+REM Start Logging Statistics every minute to a numbered subfolder
+LogProcessStats ProcessFiles 1m .\Stats\Test_5 /el
