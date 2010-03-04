0d8ec99e16c7391e621bbe35ca91905e4fa1fc28
Joanna_Lee <Joanna_Lee@extract.local>
2010-03-04 09:28:36 -0600

    ﻿ [git-vault-id] Extract$/Engineering@8062/8490

---
+++ ProductDevelopment/AttributeFinder/IndustrySpecific/Redaction/AutomatedTest/MemoryLeak/CreateRedactedImage/ExecuteTest6.bat
@@ -0,0 +1,12 @@
+REM Clean Source folder
+call Clean.bat Test_6
+
+REM Supply Numbered Files for 6 hours - first VOA files, then image files
+START CopyNumberedFiles.exe "Image1.pdf.voa" ".\Source" 2s -h6
+START CopyNumberedFiles.exe "Image1.pdf" ".\Source" 3s -h6
+
+REM Execute command-line for desired test
+START ProcessFiles.exe CreateRedactedImage6.fps /s
+
+REM Start Logging Statistics to numbered subfolder
+LogProcessStats ProcessFiles 5s .\Stats\Test_6 /el
