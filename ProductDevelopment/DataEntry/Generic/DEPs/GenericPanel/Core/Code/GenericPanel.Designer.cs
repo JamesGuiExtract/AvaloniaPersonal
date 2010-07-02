e98cf525ea714432e2320a46f9558c315758d57f
Steve_Kurth <Steve_Kurth@extract.local>
2010-07-02 09:57:52 -0500

    ﻿Modified the tab indexes to workaround a tab order issue that exists with more than 10 sibling fields (DataEntry:1004).
    Reviewed by:
    Jeff (0/0)
    [git-vault-id] Extract$/Engineering@10423/11109

--- ProductDevelopment/DataEntry/Generic/DEPs/GenericPanel/Core/Code/GenericPanel.Designer.cs
+++ ProductDevelopment/DataEntry/Generic/DEPs/GenericPanel/Core/Code/GenericPanel.Designer.cs
@@ -78,7 +78,6 @@ namespace Extract.DataEntry.DEP.Generic
             this._label1.Location = new System.Drawing.Point(4, 4);
             this._label1.Name = "_label1";
             this._label1.Size = new System.Drawing.Size(41, 13);
-            this._label1.TabIndex = 0;
             this._label1.Text = "_label1";
             // 
             // _field1
@@ -90,7 +89,7 @@ namespace Extract.DataEntry.DEP.Generic
             this._field1.Name = "_field1";
             this._field1.PersistAttribute = false;
             this._field1.Size = new System.Drawing.Size(448, 20);
-            this._field1.TabIndex = 0;
+            this._field1.TabIndex = 10;
             this._field1.ValidationErrorMessage = "Invalid value";
             // 
             // _field2
@@ -102,7 +101,7 @@ namespace Extract.DataEntry.DEP.Generic
             this._field2.Name = "_field2";
             this._field2.PersistAttribute = false;
             this._field2.Size = new System.Drawing.Size(448, 20);
-            this._field2.TabIndex = 1;
+            this._field2.TabIndex = 11;
             this._field2.ValidationErrorMessage = "Invalid value";
             // 
             // _label2
@@ -111,7 +110,6 @@ namespace Extract.DataEntry.DEP.Generic
             this._label2.Location = new System.Drawing.Point(4, 44);
             this._label2.Name = "_label2";
             this._label2.Size = new System.Drawing.Size(41, 13);
-            this._label2.TabIndex = 2;
             this._label2.Text = "_label2";
             // 
             // _field3
@@ -123,7 +121,7 @@ namespace Extract.DataEntry.DEP.Generic
             this._field3.Name = "_field3";
             this._field3.PersistAttribute = false;
             this._field3.Size = new System.Drawing.Size(448, 20);
-            this._field3.TabIndex = 2;
+            this._field3.TabIndex = 12;
             this._field3.ValidationErrorMessage = "Invalid value";
             // 
             // _label3
@@ -132,7 +130,6 @@ namespace Extract.DataEntry.DEP.Generic
             this._label3.Location = new System.Drawing.Point(4, 84);
             this._label3.Name = "_label3";
             this._label3.Size = new System.Drawing.Size(41, 13);
-            this._label3.TabIndex = 4;
             this._label3.Text = "_label3";
             // 
             // _field4
@@ -144,7 +141,7 @@ namespace Extract.DataEntry.DEP.Generic
             this._field4.Name = "_field4";
             this._field4.PersistAttribute = false;
             this._field4.Size = new System.Drawing.Size(448, 20);
-            this._field4.TabIndex = 3;
+            this._field4.TabIndex = 13;
             this._field4.ValidationErrorMessage = "Invalid value";
             // 
             // _label4
@@ -153,7 +150,6 @@ namespace Extract.DataEntry.DEP.Generic
             this._label4.Location = new System.Drawing.Point(4, 124);
             this._label4.Name = "_label4";
             this._label4.Size = new System.Drawing.Size(41, 13);
-            this._label4.TabIndex = 6;
             this._label4.Text = "_label4";
             // 
             // _field5
@@ -165,7 +161,7 @@ namespace Extract.DataEntry.DEP.Generic
             this._field5.Name = "_field5";
             this._field5.PersistAttribute = false;
             this._field5.Size = new System.Drawing.Size(448, 20);
-            this._field5.TabIndex = 4;
+            this._field5.TabIndex = 14;
             this._field5.ValidationErrorMessage = "Invalid value";
             // 
             // _label5
@@ -174,7 +170,6 @@ namespace Extract.DataEntry.DEP.Generic
             this._label5.Location = new System.Drawing.Point(4, 164);
             this._label5.Name = "_label5";
             this._label5.Size = new System.Drawing.Size(41, 13);
-            this._label5.TabIndex = 8;
             this._label5.Text = "_label5";
             // 
             // _field6
@@ -186,7 +181,7 @@ namespace Extract.DataEntry.DEP.Generic
             this._field6.Name = "_field6";
             this._field6.PersistAttribute = false;
             this._field6.Size = new System.Drawing.Size(448, 20);
-            this._field6.TabIndex = 5;
+            this._field6.TabIndex = 15;
             this._field6.ValidationErrorMessage = "Invalid value";
             // 
             // _label6
@@ -195,7 +190,6 @@ namespace Extract.DataEntry.DEP.Generic
             this._label6.Location = new System.Drawing.Point(4, 204);
             this._label6.Name = "_label6";
             this._label6.Size = new System.Drawing.Size(41, 13);
-            this._label6.TabIndex = 10;
             this._label6.Text = "_label6";
             // 
             // _field7
@@ -207,7 +201,7 @@ namespace Extract.DataEntry.DEP.Generic
             this._field7.Name = "_field7";
             this._field7.PersistAttribute = false;
             this._field7.Size = new System.Drawing.Size(448, 20);
-            this._field7.TabIndex = 6;
+            this._field7.TabIndex = 16;
             this._field7.ValidationErrorMessage = "Invalid value";
             // 
             // _label7
@@ -216,7 +210,6 @@ namespace Extract.DataEntry.DEP.Generic
             this._label7.Location = new System.Drawing.Point(4, 244);
             this._label7.Name = "_label7";
             this._label7.Size = new System.Drawing.Size(41, 13);
-            this._label7.TabIndex = 12;
             this._label7.Text = "_label7";
             // 
             // _field8
@@ -228,7 +221,7 @@ namespace Extract.DataEntry.DEP.Generic
             this._field8.Name = "_field8";
             this._field8.PersistAttribute = false;
             this._field8.Size = new System.Drawing.Size(448, 20);
-            this._field8.TabIndex = 7;
+            this._field8.TabIndex = 17;
             this._field8.ValidationErrorMessage = "Invalid value";
             // 
             // _label8
@@ -237,7 +230,6 @@ namespace Extract.DataEntry.DEP.Generic
             this._label8.Location = new System.Drawing.Point(4, 284);
             this._label8.Name = "_label8";
             this._label8.Size = new System.Drawing.Size(41, 13);
-            this._label8.TabIndex = 14;
             this._label8.Text = "_label8";
             // 
             // _field9
@@ -249,7 +241,7 @@ namespace Extract.DataEntry.DEP.Generic
             this._field9.Name = "_field9";
             this._field9.PersistAttribute = false;
             this._field9.Size = new System.Drawing.Size(448, 20);
-            this._field9.TabIndex = 8;
+            this._field9.TabIndex = 18;
             this._field9.ValidationErrorMessage = "Invalid value";
             // 
             // _label9
@@ -258,7 +250,6 @@ namespace Extract.DataEntry.DEP.Generic
             this._label9.Location = new System.Drawing.Point(4, 324);
             this._label9.Name = "_label9";
             this._label9.Size = new System.Drawing.Size(41, 13);
-            this._label9.TabIndex = 16;
             this._label9.Text = "_label9";
             // 
             // _field10
@@ -270,7 +261,7 @@ namespace Extract.DataEntry.DEP.Generic
             this._field10.Name = "_field10";
             this._field10.PersistAttribute = false;
             this._field10.Size = new System.Drawing.Size(448, 20);
-            this._field10.TabIndex = 9;
+            this._field10.TabIndex = 19;
             this._field10.ValidationErrorMessage = "Invalid value";
             // 
             // _label10
@@ -279,7 +270,6 @@ namespace Extract.DataEntry.DEP.Generic
             this._label10.Location = new System.Drawing.Point(4, 364);
             this._label10.Name = "_label10";
             this._label10.Size = new System.Drawing.Size(47, 13);
-            this._label10.TabIndex = 18;
             this._label10.Text = "_label10";
             // 
             // _field20
@@ -291,7 +281,7 @@ namespace Extract.DataEntry.DEP.Generic
             this._field20.Name = "_field20";
             this._field20.PersistAttribute = false;
             this._field20.Size = new System.Drawing.Size(448, 20);
-            this._field20.TabIndex = 19;
+            this._field20.TabIndex = 29;
             this._field20.ValidationErrorMessage = "Invalid value";
             // 
             // _label20
@@ -300,7 +290,6 @@ namespace Extract.DataEntry.DEP.Generic
             this._label20.Location = new System.Drawing.Point(4, 764);
             this._label20.Name = "_label20";
             this._label20.Size = new System.Drawing.Size(47, 13);
-            this._label20.TabIndex = 38;
             this._label20.Text = "_label20";
             // 
             // _field19
@@ -312,7 +301,7 @@ namespace Extract.DataEntry.DEP.Generic
             this._field19.Name = "_field19";
             this._field19.PersistAttribute = false;
             this._field19.Size = new System.Drawing.Size(448, 20);
-            this._field19.TabIndex = 18;
+            this._field19.TabIndex = 28;
             this._field19.ValidationErrorMessage = "Invalid value";
             // 
             // _label19
@@ -321,7 +310,6 @@ namespace Extract.DataEntry.DEP.Generic
             this._label19.Location = new System.Drawing.Point(4, 724);
             this._label19.Name = "_label19";
             this._label19.Size = new System.Drawing.Size(47, 13);
-            this._label19.TabIndex = 36;
             this._label19.Text = "_label19";
             // 
             // _field18
@@ -333,7 +321,7 @@ namespace Extract.DataEntry.DEP.Generic
             this._field18.Name = "_field18";
             this._field18.PersistAttribute = false;
             this._field18.Size = new System.Drawing.Size(448, 20);
-            this._field18.TabIndex = 17;
+            this._field18.TabIndex = 27;
             this._field18.ValidationErrorMessage = "Invalid value";
             // 
             // _label18
@@ -342,7 +330,6 @@ namespace Extract.DataEntry.DEP.Generic
             this._label18.Location = new System.Drawing.Point(4, 684);
             this._label18.Name = "_label18";
             this._label18.Size = new System.Drawing.Size(47, 13);
-            this._label18.TabIndex = 34;
             this._label18.Text = "_label18";
             // 
             // _field17
@@ -354,7 +341,7 @@ namespace Extract.DataEntry.DEP.Generic
             this._field17.Name = "_field17";
             this._field17.PersistAttribute = false;
             this._field17.Size = new System.Drawing.Size(448, 20);
-            this._field17.TabIndex = 16;
+            this._field17.TabIndex = 26;
             this._field17.ValidationErrorMessage = "Invalid value";
             // 
             // _label17
@@ -363,7 +350,6 @@ namespace Extract.DataEntry.DEP.Generic
             this._label17.Location = new System.Drawing.Point(4, 644);
             this._label17.Name = "_label17";
             this._label17.Size = new System.Drawing.Size(47, 13);
-            this._label17.TabIndex = 32;
             this._label17.Text = "_label17";
             // 
             // _field16
@@ -375,7 +361,7 @@ namespace Extract.DataEntry.DEP.Generic
             this._field16.Name = "_field16";
             this._field16.PersistAttribute = false;
             this._field16.Size = new System.Drawing.Size(448, 20);
-            this._field16.TabIndex = 15;
+            this._field16.TabIndex = 25;
             this._field16.ValidationErrorMessage = "Invalid value";
             // 
             // _label16
@@ -384,7 +370,6 @@ namespace Extract.DataEntry.DEP.Generic
             this._label16.Location = new System.Drawing.Point(4, 604);
             this._label16.Name = "_label16";
             this._label16.Size = new System.Drawing.Size(47, 13);
-            this._label16.TabIndex = 30;
             this._label16.Text = "_label16";
             // 
             // _field15
@@ -396,7 +381,7 @@ namespace Extract.DataEntry.DEP.Generic
             this._field15.Name = "_field15";
             this._field15.PersistAttribute = false;
             this._field15.Size = new System.Drawing.Size(448, 20);
-            this._field15.TabIndex = 14;
+            this._field15.TabIndex = 24;
             this._field15.ValidationErrorMessage = "Invalid value";
             // 
             // _label15
@@ -405,7 +390,6 @@ namespace Extract.DataEntry.DEP.Generic
             this._label15.Location = new System.Drawing.Point(4, 564);
             this._label15.Name = "_label15";
             this._label15.Size = new System.Drawing.Size(47, 13);
-            this._label15.TabIndex = 28;
             this._label15.Text = "_label15";
             // 
             // _field14
@@ -417,7 +401,7 @@ namespace Extract.DataEntry.DEP.Generic
             this._field14.Name = "_field14";
             this._field14.PersistAttribute = false;
             this._field14.Size = new System.Drawing.Size(448, 20);
-            this._field14.TabIndex = 13;
+            this._field14.TabIndex = 23;
             this._field14.ValidationErrorMessage = "Invalid value";
             // 
             // _label14
@@ -426,7 +410,6 @@ namespace Extract.DataEntry.DEP.Generic
             this._label14.Location = new System.Drawing.Point(4, 524);
             this._label14.Name = "_label14";
             this._label14.Size = new System.Drawing.Size(47, 13);
-            this._label14.TabIndex = 26;
             this._label14.Text = "_label14";
             // 
             // _field13
@@ -438,7 +421,7 @@ namespace Extract.DataEntry.DEP.Generic
             this._field13.Name = "_field13";
             this._field13.PersistAttribute = false;
             this._field13.Size = new System.Drawing.Size(448, 20);
-            this._field13.TabIndex = 12;
+            this._field13.TabIndex = 22;
             this._field13.ValidationErrorMessage = "Invalid value";
             // 
             // _label13
@@ -447,7 +430,6 @@ namespace Extract.DataEntry.DEP.Generic
             this._label13.Location = new System.Drawing.Point(4, 484);
             this._label13.Name = "_label13";
             this._label13.Size = new System.Drawing.Size(47, 13);
-            this._label13.TabIndex = 24;
             this._label13.Text = "_label13";
             // 
             // _field12
@@ -459,7 +441,7 @@ namespace Extract.DataEntry.DEP.Generic
             this._field12.Name = "_field12";
             this._field12.PersistAttribute = false;
             this._field12.Size = new System.Drawing.Size(448, 20);
-            this._field12.TabIndex = 11;
+            this._field12.TabIndex = 21;
             this._field12.ValidationErrorMessage = "Invalid value";
             // 
             // _label12
@@ -468,7 +450,6 @@ namespace Extract.DataEntry.DEP.Generic
             this._label12.Location = new System.Drawing.Point(4, 444);
             this._label12.Name = "_label12";
             this._label12.Size = new System.Drawing.Size(47, 13);
-            this._label12.TabIndex = 22;
             this._label12.Text = "_label12";
             // 
             // _field11
@@ -480,7 +461,7 @@ namespace Extract.DataEntry.DEP.Generic
             this._field11.Name = "_field11";
             this._field11.PersistAttribute = false;
             this._field11.Size = new System.Drawing.Size(448, 20);
-            this._field11.TabIndex = 10;
+            this._field11.TabIndex = 20;
             this._field11.ValidationErrorMessage = "Invalid value";
             // 
             // _label11
@@ -489,7 +470,6 @@ namespace Extract.DataEntry.DEP.Generic
             this._label11.Location = new System.Drawing.Point(4, 404);
             this._label11.Name = "_label11";
             this._label11.Size = new System.Drawing.Size(47, 13);
-            this._label11.TabIndex = 20;
             this._label11.Text = "_label11";
             // 
             // GenericPanel
