ffd0e1170d93a07e95c578a75ff2f9c060f49eef
Jeff_Shergalis <Jeff_Shergalis@extract.local>
2010-09-08 10:12:34 -0500

    ﻿Modified as per FlexIDSConfiguration #187  - Moved the ID Shield configuration to the central admin applications page
    Reviewed by:
    Steve/0/0
    [git-vault-id] Extract$/Engineering@11424/12121

--- ProductDevelopment/AFIntegrations/SharePoint/Extract.SharePoint/Core/Code/ADMIN/Extract.SharePoint.Redaction/ConfigureIdShieldSettings.aspx
+++ ProductDevelopment/AFIntegrations/SharePoint/Extract.SharePoint/Core/Code/ADMIN/Extract.SharePoint.Redaction/ConfigureIdShieldSettings.aspx
@@ -1,23 +1,14 @@
 ﻿<%@ Assembly Name="$SharePoint.Project.AssemblyFullName$" %>
+<%@ Assembly Name="Microsoft.SharePoint.ApplicationPages.Administration, Version=14.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c"%>
 <%@ Import Namespace="Microsoft.SharePoint.ApplicationPages" %>
 <%@ Register Tagprefix="SharePoint" Namespace="Microsoft.SharePoint.WebControls" Assembly="Microsoft.SharePoint, Version=14.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
 <%@ Register Tagprefix="Utilities" Namespace="Microsoft.SharePoint.Utilities" Assembly="Microsoft.SharePoint, Version=14.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
 <%@ Register Tagprefix="asp" Namespace="System.Web.UI" Assembly="System.Web.Extensions, Version=3.5.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" %>
 <%@ Register Tagprefix="SharePoint" Namespace="Microsoft.SharePoint.WebControls" Assembly="Microsoft.SharePoint, Version=14.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
-<%@ Import Namespace="Microsoft.SharePoint" %>
 <%@ Assembly Name="Microsoft.Web.CommandUI, Version=14.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
-<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ConfigureIdShieldSettings.aspx.cs" Inherits="Extract.SharePoint.Redaction.Layouts.ConfigureIdShieldSettings" DynamicMasterPageFile="~masterurl/default.master" %>
+<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ConfigureIdShieldSettings.aspx.cs" Inherits="Extract.SharePoint.Redaction.Layouts.ConfigureIdShieldSettings" MasterPageFile="~/_admin/admin.master" %>
 
 <asp:Content ID="PageHead" ContentPlaceHolderID="PlaceHolderAdditionalPageHead" runat="server">
-
-<script type="text/javascript">
-    function HandleCancelClicked()
-    {
-        SP.UI.ModalDialog.commonModalDialogClose(SP.UI.DialogResult.cancel,
-        'Cancel clicked');
-    }
-</script>
-
 </asp:Content>
 
 <asp:Content ID="Main" ContentPlaceHolderID="PlaceHolderMain" runat="server">
@@ -63,7 +54,7 @@
 <br />
 <asp:Panel ID="panelButtons" runat="server" HorizontalAlign="Right">
     <asp:Button ID="buttonOk" runat="server" Text="OK" OnClick="HandleOkButtonClick" />
-    <asp:Button ID="buttonCancel" runat="server" Text="Cancel" OnClientClick="HandleCancelClicked()" />
+    <asp:Button ID="buttonCancel" runat="server" Text="Cancel" OnClick="HandleCancelButtonClick" />
 </asp:Panel>
 </asp:Content>
 
--- ProductDevelopment/AFIntegrations/SharePoint/Extract.SharePoint/Core/Code/ADMIN/Extract.SharePoint.Redaction/ConfigureIdShieldSettings.aspx.cs
+++ ProductDevelopment/AFIntegrations/SharePoint/Extract.SharePoint/Core/Code/ADMIN/Extract.SharePoint.Redaction/ConfigureIdShieldSettings.aspx.cs
@@ -1,14 +1,16 @@
 ﻿using Microsoft.SharePoint;
+using Microsoft.SharePoint.Administration;
+using Microsoft.SharePoint.ApplicationPages;
 using Microsoft.SharePoint.Utilities;
-using Microsoft.SharePoint.WebControls;
 using System;
+using System.Globalization;
 
 namespace Extract.SharePoint.Redaction.Layouts
 {
     /// <summary>
     /// Code behind file for the ID Shield configuration page
     /// </summary>
-    public partial class ConfigureIdShieldSettings : LayoutsPageBase
+    public partial class ConfigureIdShieldSettings : DialogAdminPageBase
     {
         /// <summary>
         /// Called when the page loads.
@@ -54,34 +56,44 @@ namespace Extract.SharePoint.Redaction.Layouts
 
             try
             {
-                SPUtility.ValidateFormDigest();
-                SPSecurity.RunWithElevatedPrivileges(delegate()
+                // Remove trailing '\'
+                string folder = textFolder.Text.Trim();
+                if (folder.EndsWith("\\", StringComparison.Ordinal))
                 {
-                    // Remove trailing '\'
-                    string folder = textFolder.Text.Trim();
-                    if (folder.EndsWith("\\", StringComparison.Ordinal))
-                    {
-                        folder = folder.Substring(0, folder.Length - 1);
-                    }
+                    folder = folder.Substring(0, folder.Length - 1);
+                }
 
-                    IdShieldSettings settings = IdShieldSettings.GetIdShieldSettings(true);
-                    settings.LocalWorkingFolder = folder;
-                    settings.ExceptionServiceIPAddress = textExceptionIpAddress.Text.Trim();
-                    settings.Update();
-                });
+                IdShieldSettings settings = IdShieldSettings.GetIdShieldSettings(true);
+                settings.LocalWorkingFolder = folder;
+                settings.ExceptionServiceIPAddress = textExceptionIpAddress.Text.Trim();
+                settings.Update();
 
-                string response = "<script type=\"text/javascript\">"
-                    + " window.frameElement.commitPopup(); "
-                        + "</script>";
-                // Send the response to close the dialog
-                Context.Response.Write(response);
-                Context.Response.Flush();
-                Context.Response.End();
+                // Redirect back to the application management page
+                SPUtility.Redirect("/applications.aspx", SPRedirectFlags.Default, this.Context);
             }
             catch (Exception ex)
             {
                 IdShieldHelper.LogException(ex, ErrorCategoryId.IdShieldSettingsConfiguration,
                     "ELI30553");
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Handles the cancel button click from the configuration page
+        /// </summary>
+        /// <param name="sender">The object which sent the event.</param>
+        /// <param name="e">The data associated with the event.</param>
+        protected void HandleCancelButtonClick(object sender, EventArgs e)
+        {
+            try
+            {
+                SPUtility.Redirect("/applications.aspx", SPRedirectFlags.Default, this.Context);
+            }
+            catch (Exception ex)
+            {
+                IdShieldHelper.LogException(ex, ErrorCategoryId.IdShieldSettingsConfiguration,
+                    "ELI30605");
                 throw;
             }
         }
