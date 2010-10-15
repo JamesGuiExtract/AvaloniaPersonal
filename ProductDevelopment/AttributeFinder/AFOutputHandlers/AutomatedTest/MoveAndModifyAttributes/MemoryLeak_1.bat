661ada660645caf301c8dee5ab6f2714562e55da
Joanna_Lee <Joanna_Lee@extract.local>
2010-10-15 16:34:48 -0500

    ﻿ [git-vault-id] Extract$/Engineering@12029/12754

---
+++ ProductDevelopment/AttributeFinder/AFOutputHandlers/AutomatedTest/MoveAndModifyAttributes/MemoryLeak_1.bat
@@ -0,0 +1,12 @@
+:: Clean Source folder
+if exist "%testpath%\Source\" (
+del /q "%testpath%\Source\*.*"
+) else (
+md "%testpath%\Source"
+)
+
+:: Wait a few seconds
+"%ccdir%\sleep" 10s
+
+:: Supply Numbered Files for hours specified in runtest.bat
+start "CopyNumberedFiles" "%ccdir%\CopyNumberedFiles.exe" "%ccdir%\..\TestingFiles\ProductDevelopment\AttributeFinder\AFCore\AutomatedTest\Images\Image1.tif.uss" "%testpath%\Source" 6s -h%processingtime%
