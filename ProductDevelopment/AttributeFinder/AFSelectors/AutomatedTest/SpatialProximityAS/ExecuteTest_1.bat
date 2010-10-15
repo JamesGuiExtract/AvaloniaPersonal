661ecaa3cf1f706b7d02709c954115ae82d6cdbc
Joanna_Lee <Joanna_Lee@extract.local>
2010-10-15 17:14:22 -0500

    ﻿ [git-vault-id] Extract$/Engineering@12098/12823

--- ProductDevelopment/AttributeFinder/AFSelectors/AutomatedTest/SpatialProximityAS/ExecuteTest_1.bat
+++ ProductDevelopment/AttributeFinder/AFSelectors/AutomatedTest/SpatialProximityAS/ExecuteTest_1.bat
@@ -1,11 +1,11 @@
 REM Clean Source folder
 call Clean.bat Test_1
 
-REM Supply numbered file every 2 seconds for 8 hours
-START CopyNumberedSets Images MemoryTestArea 3 8
+REM Supply numbered file every 3 seconds for 8 hours
+START CopyNumberedSets Images Source 3 8
 
 REM Execute command-line for desired test
-START ProcessFiles.exe MemoryLeak.fps /s
+START ProcessFiles.exe MemoryLeak_3.fps /s
 
 REM Start Logging Statistics to numbered subfolder
 LogProcessStats.exe ProcessFiles.exe,SSOCR2.exe,XOCR32b.exe,AdjustImageResolution.exe,CleanupImage.exe,ESConvertToPDF.exe,ESConvertUSSToTxt.exe,ImageFormatConverter.exe,RedactFromXml.exe,RunRules.exe,SQLServerInfo.exe 5s .\Stats\Test_1 /el
