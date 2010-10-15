1c65d262d8055b01e90c68a6f627f440c09fc7d9
Joanna_Lee <Joanna_Lee@extract.local>
2010-10-15 16:31:29 -0500

    ﻿ [git-vault-id] Extract$/Engineering@12023/12748

--- ProductDevelopment/AttributeFinder/AFOutputHandlers/AutomatedTest/MergeAttributes/ExecuteTest_1.bat
+++ ProductDevelopment/AttributeFinder/AFOutputHandlers/AutomatedTest/MergeAttributes/ExecuteTest_1.bat
@@ -2,10 +2,10 @@ REM Clean Source folder
 call Clean.bat Test_1
 
 REM Supply numbered file sets every 6 seconds for 10 hours
-START CopyNumberedSets Images MemoryTestArea 6 10
+START CopyNumberedSets Images Source 6 10
 
 REM Execute command-line for desired test
-START ProcessFiles.exe MemoryLeak.fps /s
+START ProcessFiles.exe MemoryLeak_1.fps /s
 
 REM Start Logging Statistics to numbered subfolder
 LogProcessStats ProcessFiles.exe,SSOCR2.exe,XOCR32b.exe,AdjustImageResolution.exe,CleanupImage.exe,ESConvertToPDF.exe,ESConvertUSSToTxt.exe,ImageFormatConverter.exe,RedactFromXml.exe,RunRules.exe,SQLServerInfo.exe 5s .\Stats\Test_1 /el
