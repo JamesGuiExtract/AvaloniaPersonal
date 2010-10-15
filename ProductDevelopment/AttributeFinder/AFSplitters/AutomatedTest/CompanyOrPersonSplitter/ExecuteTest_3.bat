efbf1356ad4fa13635ed532cd9f357f99c054586
Joanna_Lee <Joanna_Lee@extract.local>
2010-10-15 17:16:06 -0500

    ﻿ [git-vault-id] Extract$/Engineering@12101/12826

--- ProductDevelopment/AttributeFinder/AFSplitters/AutomatedTest/CompanyOrPersonSplitter/ExecuteTest_3.bat
+++ ProductDevelopment/AttributeFinder/AFSplitters/AutomatedTest/CompanyOrPersonSplitter/ExecuteTest_3.bat
@@ -5,7 +5,7 @@ REM Supply Numbered Files for 4 hours
 START CopyNumberedFiles "PersonNameTests.txt" ".\Source" 10s -h4
 
 REM Execute command-line for desired test
-START ProcessFiles.exe MemoryLeak.fps /s
+START ProcessFiles.exe MemoryLeak_3.fps /s
 
 REM Start Logging Statistics to numbered subfolder
 LogProcessStats ProcessFiles 5s .\Stats\Test_3 /el
