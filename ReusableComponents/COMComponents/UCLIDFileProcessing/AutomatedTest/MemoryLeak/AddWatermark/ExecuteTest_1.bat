5edcb5709e176111e1b724b3ba5c0e0780f0b642
Joanna_Lee <Joanna_Lee@extract.local>
2010-10-18 10:50:41 -0500

    ﻿ [git-vault-id] Extract$/Engineering@12280/13005

--- ReusableComponents/COMComponents/UCLIDFileProcessing/AutomatedTest/MemoryLeak/AddWatermark/ExecuteTest_1.bat
+++ ReusableComponents/COMComponents/UCLIDFileProcessing/AutomatedTest/MemoryLeak/AddWatermark/ExecuteTest_1.bat
@@ -5,7 +5,7 @@ REM Supply Numbered Files for 6 hours
 START CopyNumberedFiles ".\MultiPage.tif" ".\Source" 1 -h6
 
 REM Execute command-line for desired test
-START ProcessFiles.exe 1_1BitStampsTif.fps /s
+START ProcessFiles.exe MemoryLeak_1.fps /s
 
 REM Start Logging Statistics every minute to a numbered subfolder
 LogProcessStats ProcessFiles 1m .\Stats\Test_1 /el
--- ReusableComponents/COMComponents/UCLIDFileProcessing/AutomatedTest/MemoryLeak/AddWatermark/ExecuteTest_4.bat
+++ ReusableComponents/COMComponents/UCLIDFileProcessing/AutomatedTest/MemoryLeak/AddWatermark/ExecuteTest_4.bat
@@ -5,7 +5,7 @@ REM Supply Numbered Files for 6 hours
 START CopyNumberedFiles ".\MultiPage.pdf" ".\Source" 15 -h6
 
 REM Execute command-line for desired test
-START ProcessFiles.exe 4_24BitStampsPdf.fps /s
+START ProcessFiles.exe MemoryLeak_4.fps /s
 
 REM Start Logging Statistics every minute to a numbered subfolder
 LogProcessStats ProcessFiles 1m .\Stats\Test_4 /el
