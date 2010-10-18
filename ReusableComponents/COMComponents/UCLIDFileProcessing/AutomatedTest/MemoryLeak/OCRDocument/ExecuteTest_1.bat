ffaa2b1a6187d2d98bfc50dc740b1398336eb4e9
Joanna_Lee <Joanna_Lee@extract.local>
2010-10-18 11:31:45 -0500

    ﻿ [git-vault-id] Extract$/Engineering@12349/13074

--- ReusableComponents/COMComponents/UCLIDFileProcessing/AutomatedTest/MemoryLeak/OCRDocument/ExecuteTest_1.bat
+++ ReusableComponents/COMComponents/UCLIDFileProcessing/AutomatedTest/MemoryLeak/OCRDocument/ExecuteTest_1.bat
@@ -5,7 +5,7 @@ REM Supply Numbered Files for 6 hours
 START CopyNumberedFiles "C:\Program Files\Extract Systems\TestingFiles\ProductDevelopment\AttributeFinder\AFCore\AutomatedTest\Images\Image2.tif" ".\Source" 1 -h6
 
 REM Execute command-line for desired test
-START ProcessFiles.exe 1_OCR_All_Pages.fps /s
+START ProcessFiles.exe MemoryLeak_1.fps /s
 
 REM Start Logging Statistics every minute to a numbered subfolder
 LogProcessStats ProcessFiles 1m .\Stats\Test_1 /el
--- ReusableComponents/COMComponents/UCLIDFileProcessing/AutomatedTest/MemoryLeak/OCRDocument/ExecuteTest_3.bat
+++ ReusableComponents/COMComponents/UCLIDFileProcessing/AutomatedTest/MemoryLeak/OCRDocument/ExecuteTest_3.bat
@@ -5,7 +5,7 @@ REM Supply Numbered Files for 6 hours
 START CopyNumberedFiles "C:\Program Files\Extract Systems\TestingFiles\ProductDevelopment\AttributeFinder\AFCore\AutomatedTest\Images\Image2.tif" ".\Source" 400ms -h6
 
 REM Execute command-line for desired test
-START ProcessFiles.exe 3_OCR_Last_Page.fps /s
+START ProcessFiles.exe MemoryLeak_3.fps /s
 
 REM Start Logging Statistics every minute to a numbered subfolder
 LogProcessStats ProcessFiles 1m .\Stats\Test_3 /el
