6be8da76f2cc0c4b05986a28cf1a79d7ad1ab227
Joanna_Lee <Joanna_Lee@extract.local>
2010-10-18 11:04:42 -0500

    ﻿ [git-vault-id] Extract$/Engineering@12302/13027

--- ReusableComponents/COMComponents/UCLIDFileProcessing/AutomatedTest/MemoryLeak/ConditionalTask/ExecuteTest_1.bat
+++ ReusableComponents/COMComponents/UCLIDFileProcessing/AutomatedTest/MemoryLeak/ConditionalTask/ExecuteTest_1.bat
@@ -2,13 +2,13 @@ REM Clean Source folder
 call Clean.bat Test_1
 
 REM create TestArea
-mkdir TestArea
+mkdir Source
 
 REM Supply numbered file sets every 10 seconds for 6 hours
-START CopyNumberedSets SourceImages TestArea 10 6
+START CopyNumberedSets SourceImages Source 10 6
 
 REM Execute command-line for desired test
-START ProcessFiles.exe MemoryLeak.fps /s
+START ProcessFiles.exe MemoryLeak_1.fps /s
 
 REM Start Logging Statistics to numbered subfolder
 LogProcessStats ProcessFiles,SSOCR2,XOCR32b,AdjustImageResolution,CleanupImage,ESConvertToPDF,ESConvertUSSToTxt,ImageFormatConverter,RedactFromXml,RunRules,SQLServerInfo 5s .\Stats\Test_1 /el
