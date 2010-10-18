4bbfc8f1a7185ac57fd88f5cf552861c707c73fe
Joanna_Lee <Joanna_Lee@extract.local>
2010-10-18 09:22:49 -0500

    ﻿ [git-vault-id] Extract$/Engineering@12149/12874

--- ProductDevelopment/AttributeFinder/AFValueFinders/AutomatedTest/ImageRegionWithLines/ExecuteTest_1.bat
+++ ProductDevelopment/AttributeFinder/AFValueFinders/AutomatedTest/ImageRegionWithLines/ExecuteTest_1.bat
@@ -2,10 +2,10 @@ REM Clean Source folder
 call Clean.bat
 
 REM Supply numbered file sets every 5 minutes for 8 hours
-START CopyNumberedSets SourceImages TestArea 300 8
+START CopyNumberedSets SourceImages Source 300 8
 
 REM Execute command-line for desired test
-START ProcessFiles.exe MemoryLeak.fps /s
+START ProcessFiles.exe MemoryLeak_1.fps /s
 
 REM Start Logging Statistics to numbered subfolder
 LogProcessStats ProcessFiles.exe,SSOCR2.exe,XOCR32b.exe,AdjustImageResolution.exe,CleanupImage.exe,ESConvertToPDF.exe,ESConvertUSSToTxt.exe,ImageFormatConverter.exe,RedactFromXml.exe,RunRules.exe,SQLServerInfo.exe 5s .\Stats\Test_1 /el
