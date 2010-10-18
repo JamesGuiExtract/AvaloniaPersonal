fe9b8713af9f749a0bcef853a764b15426392a46
Joanna_Lee <Joanna_Lee@extract.local>
2010-10-18 09:29:15 -0500

    ﻿ [git-vault-id] Extract$/Engineering@12158/12883

--- ProductDevelopment/AttributeFinder/AFValueFinders/AutomatedTest/LoopFinder/ExecuteTest_2.bat
+++ ProductDevelopment/AttributeFinder/AFValueFinders/AutomatedTest/LoopFinder/ExecuteTest_2.bat
@@ -5,7 +5,7 @@ REM Supply Numbered Files for 4 hours
 START CopyNumberedFiles "..\..\..\AFCore\AutomatedTest\Images\Image2.tif.uss" ".\Source" 3s -h4
 
 REM Execute command-line for desired test
-START ProcessFiles.exe MemoryLeak2.fps /s
+START ProcessFiles.exe MemoryLeak_2.fps /s
 
 REM Start Logging Statistics to numbered subfolder
 LogProcessStats ProcessFiles.exe,SSOCR2.exe,XOCR32b.exe,AdjustImageResolution.exe,CleanupImage.exe,ESConvertToPDF.exe,ESConvertUSSToTxt.exe,ImageFormatConverter.exe,RedactFromXml.exe,RunRules.exe,SQLServerInfo.exe 5s .\Stats\Test_2 /el
