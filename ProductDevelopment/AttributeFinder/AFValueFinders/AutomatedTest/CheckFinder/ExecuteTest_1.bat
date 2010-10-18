0331c5a2d23844ae6bdfd011a123c9c6884ba2e7
Joanna_Lee <Joanna_Lee@extract.local>
2010-10-18 09:11:02 -0500

    ﻿ [git-vault-id] Extract$/Engineering@12131/12856

--- ProductDevelopment/AttributeFinder/AFValueFinders/AutomatedTest/CheckFinder/ExecuteTest_1.bat
+++ ProductDevelopment/AttributeFinder/AFValueFinders/AutomatedTest/CheckFinder/ExecuteTest_1.bat
@@ -1,15 +1,15 @@
 @echo off
 REM Ensure the Source folder exists
-if not exist "%~dp0\TestArea" mkdir "%~dp0\TestArea\"
+if not exist "%~dp0\Source" mkdir "%~dp0\Source\"
 
 REM Clean Source folder
 call Clean.bat Test_1
 
 REM Supply numbered file sets every 8 seconds for 4 hours
-START CopyNumberedSets Images TestArea 8 4
+START CopyNumberedSets Images Source 8 4
 
 REM Execute command-line for desired test
-START ProcessFiles.exe MemoryLeak.fps /s
+START ProcessFiles.exe MemoryLeak_1.fps /s
 
 REM Start Logging Statistics to numbered subfolder
 LogProcessStats ProcessFiles.exe 5s .\Stats\Test_1 /el
