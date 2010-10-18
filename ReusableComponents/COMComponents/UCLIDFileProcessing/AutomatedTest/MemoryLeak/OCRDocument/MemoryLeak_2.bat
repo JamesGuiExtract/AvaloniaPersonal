17554596f7314bac4f66aae79020bc98c5000e68
Joanna_Lee <Joanna_Lee@extract.local>
2010-10-18 11:32:21 -0500

    ﻿ [git-vault-id] Extract$/Engineering@12350/13075

---
+++ ReusableComponents/COMComponents/UCLIDFileProcessing/AutomatedTest/MemoryLeak/OCRDocument/MemoryLeak_2.bat
@@ -0,0 +1,12 @@
+:: Clean Source folder
+if exist "%testpath%\Source\" (
+del /q "%testpath%\Source\*.*"
+) else (
+md "%testpath%\Source"
+)
+
+:: Wait a few seconds
+"%ccdir%\sleep" 10s
+
+:: Supply Numbered Files for hours specified in runtest.bat
+start "CopyNumberedFiles" "%ccdir%\CopyNumberedFiles.exe" "%ccdir%\..\TestingFiles\ProductDevelopment\AttributeFinder\AFCore\AutomatedTest\Images\Image2.tif" "%testpath%\Source" 400ms -h%processingtime%
