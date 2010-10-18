b98807170e8ed9d628dbd15194c0692c70db3894
Joanna_Lee <Joanna_Lee@extract.local>
2010-10-18 11:11:18 -0500

    ﻿ [git-vault-id] Extract$/Engineering@12311/13036

---
+++ ReusableComponents/COMComponents/UCLIDFileProcessing/AutomatedTest/MemoryLeak/ConvertToSearchablePDF/MemoryLeak_5.bat
@@ -0,0 +1,12 @@
+:: Clean Source folder
+if exist "%testpath%\Source\" (
+del /q "%testpath%\Source\*.*"
+) else (
+md "%testpath%\Source"
+)
+
+:: Wait a few seconds
+"%ccdir%\sleep" 10s
+
+:: Supply Numbered Files for hours specified in runtest.bat
+start "CopyNumberedFiles" "%ccdir%\CopyNumberedFiles.exe" "%testpath%\0001.tif" "%testpath%\Source" 5 -h%processingtime%
