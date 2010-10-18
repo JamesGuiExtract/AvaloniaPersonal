793157864524d4f632b53fe12797820f9898d523
Joanna_Lee <Joanna_Lee@extract.local>
2010-10-18 11:36:55 -0500

    ﻿ [git-vault-id] Extract$/Engineering@12357/13082

--- ReusableComponents/COMComponents/UCLIDFileProcessing/ESFileSuppliers/AutomatedTest/DynamicList/MemoryLeak_1.bat
+++ ReusableComponents/COMComponents/UCLIDFileProcessing/ESFileSuppliers/AutomatedTest/DynamicList/MemoryLeak_1.bat
@@ -1,13 +1,14 @@
-REM Clean Source folder
-CALL Clean.bat Test_1
-
-REM Supply 60000 numbered files
-FOR %%i in (One Two Three Four Five Six) DO (
-  START /B CopyNumberedFiles "dummy.tif" ".\Source\%%i" 0ms -n10000
+:: Clean Source folder
+if exist "%testpath%\Source\" (
+del /q "%testpath%\Source\*.*"
+) else (
+md "%testpath%\Source"
 )
 
-REM Execute command-line for desired test
-START ProcessFiles.exe MemoryLeak_1.fps /s
+:: Wait a few seconds
+"%ccdir%\sleep" 10s
 
-REM Start Logging Statistics to numbered subfolder
-LogProcessStats ProcessFiles 5s .\Stats\Test_1 /el
+:: Supply Numbered Files for hours specified in runtest.bat
+for %%i in (One Two Three Four Five Six) DO (
+start  /B "CopyNumberedFiles" "%ccdir%\CopyNumberedFiles.exe" "%testpath%\dummy.tif" "%testpath%\Source\%%i" 0ms -n10000
+)
