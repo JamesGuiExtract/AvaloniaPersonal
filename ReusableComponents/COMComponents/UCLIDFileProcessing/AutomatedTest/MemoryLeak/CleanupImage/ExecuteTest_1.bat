baa543c27afb9be1772a7edf808e3c667c805555
Joanna_Lee <Joanna_Lee@extract.local>
2010-10-18 11:02:11 -0500

    ﻿ [git-vault-id] Extract$/Engineering@12298/13023

--- ReusableComponents/COMComponents/UCLIDFileProcessing/AutomatedTest/MemoryLeak/CleanupImage/ExecuteTest_1.bat
+++ ReusableComponents/COMComponents/UCLIDFileProcessing/AutomatedTest/MemoryLeak/CleanupImage/ExecuteTest_1.bat
@@ -5,7 +5,7 @@ REM Supply Numbered Files for 6 hours
 START CopyNumberedFiles ".\striped.tif" ".\Source" 1 -h6
 
 REM Execute command-line for desired test
-START ProcessFiles.exe 1_FillStripes.fps /s
+START ProcessFiles.exe MemoryLeak_1.fps /s
 
 REM Start Logging Statistics every minute to a numbered subfolder
 LogProcessStats ProcessFiles,SSOCR2,XOCR32b,AdjustImageResolution,CleanupImage,ESConvertToPDF,ESConvertUSSToTxt,ImageFormatConverter,RedactFromXml,RunRules,SQLServerInfo  1m .\Stats\Test_1 /el
--- ReusableComponents/COMComponents/UCLIDFileProcessing/AutomatedTest/MemoryLeak/CleanupImage/ExecuteTest_2.bat
+++ ReusableComponents/COMComponents/UCLIDFileProcessing/AutomatedTest/MemoryLeak/CleanupImage/ExecuteTest_2.bat
@@ -5,7 +5,7 @@ REM Supply Numbered Files for 6 hours
 START CopyNumberedFiles ".\halftones.tif" ".\Source" 1 -h6
 
 REM Execute command-line for desired test
-START ProcessFiles.exe 2_RemoveHalftones.fps /s
+START ProcessFiles.exe MemoryLeak_2.fps /s
 
 REM Start Logging Statistics every minute to a numbered subfolder
 LogProcessStats ProcessFiles,SSOCR2,XOCR32b,AdjustImageResolution,CleanupImage,ESConvertToPDF,ESConvertUSSToTxt,ImageFormatConverter,RedactFromXml,RunRules,SQLServerInfo  1m .\Stats\Test_2 /el
