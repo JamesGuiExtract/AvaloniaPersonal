9a14d045e12f25e85ed87182fd6cbe4c1063b609
Joanna_Lee <Joanna_Lee@extract.local>
2010-10-18 11:45:16 -0500

    ﻿ [git-vault-id] Extract$/Engineering@12365/13090

---
+++ ReusableComponents/COMComponents/UCLIDFileProcessing/ESFileSuppliers/AutomatedTest/StaticList/MemoryLeak_2.bat
@@ -0,0 +1,14 @@
+:: Clean Source folder
+if exist "%testpath%\Source\" (
+del /q "%testpath%\Source\*.*"
+) else (
+md "%testpath%\Source"
+)
+
+:: Wait a few seconds
+"%ccdir%\sleep" 10s
+
+:: Supply Numbered Files for hours specified in runtest.bat
+for %%i in (One Two Three Four Five Six) DO (
+start  /B "CopyNumberedFiles" "%ccdir%\CopyNumberedFiles.exe" "%testpath%\dummy.pdf" "%testpath%\Source\%%i" 0ms -n10000
+)
