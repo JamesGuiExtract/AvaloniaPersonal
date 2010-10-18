12bfd9507b678147ed8082b3e1d3f5d0cb6b4163
Joanna_Lee <Joanna_Lee@extract.local>
2010-10-18 09:57:52 -0500

    ﻿ [git-vault-id] Extract$/Engineering@12205/12930

--- ProductDevelopment/AttributeFinder/AFValueModifiers/AutomatedTest/MERSModifier/ExecuteTest_1.bat
+++ ProductDevelopment/AttributeFinder/AFValueModifiers/AutomatedTest/MERSModifier/ExecuteTest_1.bat
@@ -5,7 +5,7 @@ REM Supply Numbered Files for 4 hours
 START CopyNumberedFiles "001584.tif.uss" ".\Source" 400ms -h4
 
 REM Execute command-line for desired test
-START ProcessFiles.exe MemoryLeak.fps /s
+START ProcessFiles.exe MemoryLeak_1.fps /s
 
 REM Start Logging Statistics to numbered subfolder
 LogProcessStats ProcessFiles 5s .\Stats\Test_1 /el
