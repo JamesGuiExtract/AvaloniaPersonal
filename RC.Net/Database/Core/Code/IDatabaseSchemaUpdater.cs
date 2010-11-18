26a90f2cbb9c9360a354a0d0157ce59a6fc4472d
Jeff_Shergalis <Jeff_Shergalis@extract.local>
2010-11-18 15:06:38 -0600

    ﻿Modified as per DNRCAU #584  - Created the IDatabaseSchemaUpdater interface definition
    Reviewed by:
    William/0/0
    [git-vault-id] Extract$/Engineering@12730/13469

---
+++ RC.Net/Database/Core/Code/IDatabaseSchemaUpdater.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using UCLID_COMUTILSLib;
+
+namespace Extract.Database
+{
+    /// <summary>
+    /// Interface definition for implementing a DatabaseSchemaUpdater.
+    /// </summary>
+    [CLSCompliant(false)]
+    public interface IDatabaseSchemaUpdater
+    {
+        /// <summary>
+        /// Gets a value indicating whether a database schema update is required.
+        /// </summary>
+        /// <value>
+        /// <see langword="true"/> if an update is required; otherwise, <see langword="false"/>.
+        /// </value>
+        bool IsRequired { get; }
+
+        /// <summary>
+        /// Begins the update to latest schema.
+        /// </summary>
+        /// <param name="progressStatus">The progress status object to update, if
+        /// <see langword="null"/> then no progress status will be given. Otherwise it
+        /// will be reinitialized to the appropriate number of steps and updated by the
+        /// update task as it runs.</param>
+        /// <returns>
+        /// A handle to the task that is updating the schema.
+        /// </returns>
+        Task BeginUpdateToLatestSchema(IProgressStatus progressStatus);
+    }
+}
