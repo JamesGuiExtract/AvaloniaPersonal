4081e59fdb4c375ee77e33b505bbbad3ef21e23b
Jeff_Shergalis <Jeff_Shergalis@extract.local>
2010-12-06 09:30:26 -0600

    ﻿Moved SharePoint dev folder to root [git-vault-id] Extract$/Engineering@12826/13565

--- ProductDevelopment/AFIntegrations/SharePoint/E.SP/Installation/PowershellScripts/UninstallIdShieldForSharePoint.ps1
+++
@@ -1,7 +0,0 @@
-$Invocation = (Get-Variable MyInvocation -Scope 0).Value
-$ScriptPath = Split-Path $Invocation.MyCommand.Path
-
-$HelperFile = Join-Path $ScriptPath "SharePointDeploymentHelper.ps1"
-. $HelperFile
-
-Uninstall-Solutions ".\IDShieldForSharePointInstall.xml"
