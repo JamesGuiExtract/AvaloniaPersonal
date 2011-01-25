8c006fb0fcb87b24c83895bc315d85e5ab9f8e8f
Jeff_Shergalis <Jeff_Shergalis@extract.local>
2011-01-25 11:19:45 -0600

    ﻿Modified as per FIDSI #265-#267 & #269  - Terminology is no consistent between id shield client fps file config page and error message  - Folder browser now properly handles folders on secondary sites  - Folder SaveAs dialog now opens  - Folder browser/save dialog now shows up on top of other windows  - When client app throws an exception, it now attempts to log it to Extract log via the exception helper app  - Added extension method to log exception via helper application  - Refactored code to use exception helper extension methods  - Fixed issue that was causing redact now files to show up on wrong machine
    Reviewed by:
    Steve/1/0
    [git-vault-id] Extract$/Engineering@13403/14175

--- RC.Net/Core/ExtensionMethods/Core/Code/ExtensionMethods.cs
+++ RC.Net/Core/ExtensionMethods/Core/Code/ExtensionMethods.cs
@@ -6,6 +6,9 @@ using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Diagnostics;
+using System.Reflection;
+using System.Windows.Forms;
 
 namespace Extract
 {
@@ -178,5 +181,135 @@ namespace Extract
         }
 
         #endregion Stream Methods
+
+        #region Exception Methods
+
+        /// <summary>
+        /// Path to the Extract systems folder in the program files directory.
+        /// </summary>
+        static readonly string _EXTRACT_FOLDER = Path.Combine(
+            Environment.GetEnvironmentVariable("ProgramFiles(x86)") ??
+            Environment.GetEnvironmentVariable("ProgramFiles"), "Extract Systems");
+
+        /// <summary>
+        /// Path to the Extract common components folder.
+        /// </summary>
+        static readonly string _COMMON_COMPONENTS =
+            Path.Combine(_EXTRACT_FOLDER, "CommonComponents");
+
+        /// <summary>
+        /// Path to the exception helper application.
+        /// </summary>
+        static readonly string _EXCEPTION_HELPER_APP = Path.Combine(_COMMON_COMPONENTS,
+            "ExceptionHelper.exe");
+
+        /// <summary>
+        /// Displays the exception in a message box.
+        /// </summary>
+        /// <param name="ex">The exception.</param>
+        public static void DisplayInMessageBox(this Exception ex)
+        {
+            MessageBox.Show(ex.Message, ex.GetType().ToString(), MessageBoxButtons.OK,
+                MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, 0);
+        }
+
+        /// <summary>
+        /// Logs the exception with helper app.
+        /// </summary>
+        /// <param name="ex">The ex.</param>
+        /// <param name="eliCode">The eli code.</param>
+        /// <returns><see langword="null"/> if all went well, or an <see cref="Exception"/>
+        /// if there was an exception thrown while trying to log the exception.</returns>
+        public static Exception LogExceptionWithHelperApp(this Exception ex, string eliCode)
+        {
+            string tempFile = null;
+            try
+            {
+                if (!File.Exists(_EXCEPTION_HELPER_APP))
+                {
+                    throw new FileNotFoundException("Exception helper application was not found.",
+                        _EXCEPTION_HELPER_APP);
+                }
+
+                // Check if there is an eli code, if so add it as debug data
+                if (!string.IsNullOrEmpty(eliCode))
+                {
+                    AddEliDebugData(ex, eliCode);
+                }
+
+                // Serialize the exception as a hex string and write to temp file
+                string hexException = ex.ToSerializedHexString();
+                tempFile = Path.GetTempFileName();
+                File.WriteAllText(tempFile, hexException);
+
+                // Build the arguments for the exception helper app
+                StringBuilder arguments = new StringBuilder();
+                arguments.Append('"');
+                arguments.Append(tempFile);
+                arguments.Append('"');
+
+                // Launch the helper app
+                using (Process process = new Process())
+                {
+                    process.StartInfo.FileName = _EXCEPTION_HELPER_APP;
+                    process.StartInfo.Arguments = arguments.ToString();
+                    process.Start();
+                    process.WaitForExit();
+                }
+
+                return null;
+            }
+            catch (Exception exception)
+            {
+                try
+                {
+                    // If there was a failure logging the exception, try to add an entry to the
+                    // application log.
+                    var assembly = Assembly.GetCallingAssembly().GetName();
+                    EventLog.WriteEntry(assembly.Name,
+                        eliCode + " " + exception.Message, EventLogEntryType.Error);
+                }
+                catch { }
+
+                return exception;
+            }
+            finally
+            {
+                if (!string.IsNullOrEmpty(tempFile) && File.Exists(tempFile))
+                {
+                    File.Delete(tempFile);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Adds the specified ELI code as debug data to the exception.
+        /// </summary>
+        /// <param name="ex">The exception to add the ELI code to.</param>
+        /// <param name="eliCode">The ELI code to add.</param>
+        // FxCop is catching the "ELICode" key value as an invalid eli code, safe to suppress
+        // this warning.
+        [SuppressMessage("ExtractRules", "ES0002:MethodsShouldContainValidEliCodes")]
+        static void AddEliDebugData(Exception ex, string eliCode)
+        {
+            try
+            {
+                string rootKey = "ELICode";
+                string key = rootKey;
+                int i = 1;
+                while (ex.Data.Contains(key))
+                {
+                    key = rootKey + i.ToString(CultureInfo.InvariantCulture);
+                    i++;
+                }
+
+                ex.Data.Add(key, eliCode);
+            }
+            catch
+            {
+                // Just eat exceptions in this method
+            }
+        }
+        #endregion Exception Methods
     }
 }
--- RC.Net/ExceptionService/Core/Code/ExtractExceptionLogger.cs
+++ RC.Net/ExceptionService/Core/Code/ExtractExceptionLogger.cs
@@ -11,69 +11,6 @@ namespace Extract.ExceptionService
 {
     class ExtractExceptionLogger : IExtractExceptionLogger
     {
-        #region Fields
-
-#if DEBUG
-        /// <summary>
-        /// Path to the exception helper application.
-        /// </summary>
-        static readonly string _EXCEPTION_HELPER_APP = @"D:\Engineering\Binaries\Debug\ExceptionHelper.exe";
-#else
-        /// <summary>
-        /// Path to the Extract systems folder in the program files directory.
-        /// </summary>
-        static readonly string _EXTRACT_FOLDER = Path.Combine(
-            Environment.GetEnvironmentVariable("ProgramFiles(x86)") ??
-            Environment.GetEnvironmentVariable("ProgramFiles"), "Extract Systems");
-
-        /// <summary>
-        /// Path to the Extract common components folder.
-        /// </summary>
-        static readonly string _COMMON_COMPONENTS =
-            Path.Combine(_EXTRACT_FOLDER, "CommonComponents");
-
-        /// <summary>
-        /// Path to the exception helper application.
-        /// </summary>
-        static readonly string _EXCEPTION_HELPER_APP = Path.Combine(_COMMON_COMPONENTS,
-            "ExceptionHelper.exe");
-#endif
-
-        #endregion
-
-        #region Methods
-
-        /// <summary>
-        /// Adds the specified ELI code as debug data to the exception.
-        /// </summary>
-        /// <param name="ex">The exception to add the ELI code to.</param>
-        /// <param name="eliCode">The ELI code to add.</param>
-        // FxCop is catching the "ELICode" key value as an invalid eli code, safe to suppress
-        // this warning.
-        [SuppressMessage("ExtractRules", "ES0002:MethodsShouldContainValidEliCodes")]
-        static void AddEliDebugData(Exception ex, string eliCode)
-        {
-            try
-            {
-                string rootKey = "ELICode";
-                string key = rootKey;
-                int i = 1;
-                while (ex.Data.Contains(key))
-                {
-                    key = rootKey + i.ToString(CultureInfo.InvariantCulture);
-                    i++;
-                }
-
-                ex.Data.Add(key, eliCode);
-            }
-            catch
-            {
-                // Just eat exceptions in this method
-            }
-        }
-
-        #endregion Methods
-
         #region IExtractExceptionLogger Members
 
         /// <summary>
@@ -82,48 +19,18 @@ namespace Extract.ExceptionService
         /// <param name="exceptionData">The data to be logged.</param>
         public void LogException(ExceptionLoggerData exceptionData)
         {
-            string tempFile = null;
             try
             {
                 string eliCode = exceptionData.EliCode;
-                Exception ee = exceptionData.ExceptionData;
-
-                // Check if there is an eli code, if so add it as debug data
-                if (!string.IsNullOrEmpty(eliCode))
+                var ee = exceptionData.ExceptionData.LogExceptionWithHelperApp(eliCode);
+                if (ee != null)
                 {
-                    AddEliDebugData(ee, eliCode);
-                }
-
-                // Serialize the exception as a hex string and write to temp file
-                string hexException = ee.ToSerializedHexString();
-                tempFile = Path.GetTempFileName();
-                File.WriteAllText(tempFile, hexException);
-
-                // Build the arguments for the exception helper app
-                StringBuilder arguments = new StringBuilder();
-                arguments.Append('"');
-                arguments.Append(tempFile);
-                arguments.Append('"');
-
-                // Launch the helper app
-                using (Process process = new Process())
-                {
-                    process.StartInfo.FileName = _EXCEPTION_HELPER_APP;
-                    process.StartInfo.Arguments = arguments.ToString();
-                    process.Start();
-                    process.WaitForExit();
+                    throw ee;
                 }
             }
             catch (Exception exception)
             {
                 throw new FaultException("Unable to log exception. " + exception.Message);
-            }
-            finally
-            {
-                if (!string.IsNullOrEmpty(tempFile) && File.Exists(tempFile))
-                {
-                    File.Delete(tempFile);
-                }
             }
         }
 
