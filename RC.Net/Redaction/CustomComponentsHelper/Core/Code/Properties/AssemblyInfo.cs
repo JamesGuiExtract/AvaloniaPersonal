d848a0b9593cc5091f667d5f0dd05d3b4bbe901b
Jeff_Shergalis <Jeff_Shergalis@extract.local>
2011-02-18 16:03:36 -0600

    ﻿Adding folder that vault missed on earlier checkin [git-vault-id] Extract$/Engineering@13748/14554

---
+++ RC.Net/Redaction/CustomComponentsHelper/Core/Code/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Extract.Redaction.CustomComponentsHelper")]
+[assembly: AssemblyDescription("A collection of helper interfaces and classes for use in RedactionCustomComponents")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Extract Systems, LLC")]
+[assembly: AssemblyProduct("Extract.Redaction.CustomComponentsHelper")]
+[assembly: AssemblyCopyright("Copyright © Extract Systems, LLC 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+[assembly: CLSCompliant(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("62c35fca-8a9b-49c6-859a-ae9490d7ec72")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
