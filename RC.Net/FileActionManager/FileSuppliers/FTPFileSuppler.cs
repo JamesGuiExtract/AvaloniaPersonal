ee39987c1fc7f486fad8b593287a8836b25e6e64
William_Parr <William_Parr@extract.local>
2011-03-08 11:10:27 -0600

    ﻿$/Engineering/RC.Net/FileActionManager/FileSuppliers $/Engineering/RC.Net/FileActionManager/FileSuppliers\Properties
    DotNetRCAndUtils #614 - not complete
    Created new assembly Extract.FileActionManager.FileSuppliers and
    Created FTPFileSupplier - this is just a shell it does not do anything yet
    
    Reviewed By
    Jeff(0/1)
    
    William
    
    [git-vault-id] Extract$/Engineering@13944/14750

---
+++ RC.Net/FileActionManager/FileSuppliers/FTPFileSuppler.cs
@@ -0,0 +1,401 @@
+﻿using Extract.Interop;
+using Extract.Licensing;
+using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using UCLID_COMLMLib;
+using UCLID_COMUTILSLib;
+using UCLID_FILEPROCESSINGLib;
+
+namespace Extract.FileActionManager.FileSuppliers
+{
+    /// <summary>
+    /// A File supplier that will get files from a SFTP/FTP site
+    /// </summary>
+    [ComVisible(true)]
+    [Guid("2D201AC7-8EE8-47D0-96B3-708F4E34435C")]
+    [ProgId("Extract.FileActionManager.FileSuppliers.FTPFileSupplier")]
+    public class FTPFileSupplier : ICategorizedComponent, IConfigurableObject,
+        IMustBeConfiguredObject, ICopyableObject, IFileSupplier, ILicensedComponent,
+        IPersistStream, IDisposable
+    {
+
+        #region Constants
+
+        /// <summary>
+        /// The description of this file supplier
+        /// </summary>
+        const string _COMPONENT_DESCRIPTION = "Files from FTP site";
+
+        /// <summary>
+        /// Current file supplier version.
+        /// </summary>
+        const int _CURRENT_VERSION = 1;
+
+        /// <summary>
+        /// The license id to validate in licensing calls
+        /// </summary>
+        static readonly LicenseIdName _licenseId = LicenseIdName.FileActionManagerObjects;
+
+        #endregion
+
+        #region Fields
+
+        /// <summary>
+        /// Whether the object is dirty or not.
+        /// </summary>
+        bool _dirty;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FTPFileSupplier"/>.
+        /// </summary>
+        public FTPFileSupplier()
+        {
+        }
+
+        #endregion
+
+        #region ICategorizedComponent Members
+
+        /// <summary>
+        /// Gets the name of the COM object.
+        /// </summary>
+        /// <returns>The name of the COM object.</returns>
+        public string GetComponentDescription()
+        {
+            return _COMPONENT_DESCRIPTION;
+        }
+
+        #endregion
+
+        #region IConfigurableObject Members
+
+        /// <summary>
+        /// Performs configuration needed to create a valid <see cref="FTPFileSupplier"/>.
+        /// </summary>
+        /// <returns><see langword="true"/> if the configuration was successfully updated or
+        /// <see langword="false"/> if configuration was unsuccessful.</returns>
+        public bool RunConfiguration()
+        {
+            try
+            {
+                // Validate the license
+                LicenseUtilities.ValidateLicense(_licenseId,
+                    "ELI31989", _COMPONENT_DESCRIPTION);
+
+                MessageBox.Show("Configure FTP File Supplier");
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex.CreateComVisible("ELI31990",
+                    "Error running configuration.");
+            }
+        }
+
+        #endregion
+
+        #region IMustBeConfiguredObject Members
+
+        /// <summary>
+        /// Checks if the object has been configured properly.
+        /// </summary>
+        /// <returns>
+        /// <see langword="true"/> if the object has been configured and
+        /// <see langword="false"/> otherwise.
+        /// </returns>
+        public bool IsConfigured()
+        {
+            try
+            {
+                // This class is configured if the settings are valid
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex.CreateComVisible("ELI31991",
+                    "Failed checking configuration.");
+            }
+        }
+
+        #endregion
+
+        #region ICopyableObject Members
+
+        /// <summary>
+        /// Creates a copy of the <see cref="FTPFileSupplier"/> instance.
+        /// </summary>
+        /// <returns>A copy of the <see cref="FTPFileSupplier"/> instance.</returns>
+        public object Clone()
+        {
+            try
+            {
+                FTPFileSupplier supplier = new FTPFileSupplier();
+                return supplier;
+            }
+            catch (Exception ex)
+            {
+                throw ex.CreateComVisible("ELI31992", "Unable to clone object.");
+            }
+        }
+
+        /// <summary>
+        /// Copies the specified <see cref="FTPFileSupplier"/> instance into this one.
+        /// </summary>
+        /// <param name="pObject">The object from which to copy.</param>
+        public void CopyFrom(object pObject)
+        {
+            try
+            {
+                FTPFileSupplier supplier = (FTPFileSupplier)pObject;
+                CopyFrom(supplier);
+            }
+            catch (Exception ex)
+            {
+                throw ex.CreateComVisible("ELI31993", "Unable to copy object.");
+            }
+        }
+
+        #endregion
+
+        #region IFileSupplier Members
+
+        /// <summary>
+        /// Pauses file supply
+        /// </summary>
+        public void Pause()
+        {
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                throw ex.CreateComVisible("ELI31998", "Unable to pause supplying object.");
+            }
+        }
+
+        /// <summary>
+        /// Resumes file supplying after a pause
+        /// </summary>
+        public void Resume()
+        {
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                throw ex.CreateComVisible("ELI31999", "Unable to resume supplying object.");
+            } throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Starts file supplying
+        /// </summary>
+        /// <param name="pTarget">The IFileSupplerTarget that receives the files</param>
+        /// <param name="pFAMTM">The <see cref="FAMTagManager"/> to use if needed.</param>
+        [CLSCompliant(false)]
+        public void Start(IFileSupplierTarget pTarget, FAMTagManager pFAMTM)
+        {
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                throw ex.CreateComVisible("ELI32000", "Unable to start supplying object.");
+            }
+        }
+
+        /// <summary>
+        /// Stops file supplying
+        /// </summary>
+        public void Stop()
+        {
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                throw ex.CreateComVisible("ELI32001", "Unable to stop supplying object.");
+            }
+        }
+
+        #endregion
+
+        #region IAccessRequired Members
+
+        /// <summary>
+        /// Returns bool value indicating if the task requires admin access
+        /// </summary>
+        /// <returns><see langword="true"/> if the task requires admin access
+        /// <see langword="false"/> if task does not require admin access</returns>
+        public bool RequiresAdminAccess()
+        {
+            return false;
+        }
+
+        #endregion IAccessRequired Members
+
+        #region ILicensedComponent Members
+
+        /// <summary>
+        /// Gets whether this component is licensed.
+        /// </summary>
+        /// <returns><see langword="true"/> if the component is licensed; <see langword="false"/> 
+        /// if the component is not licensed.</returns>
+        public bool IsLicensed()
+        {
+            try
+            {
+                return LicenseUtilities.IsLicensed(_licenseId);
+            }
+            catch (Exception ex)
+            {
+                throw ex.CreateComVisible("ELI31994",
+                    "Unable to determine license status.");
+            }
+        }
+
+        #endregion
+
+        #region IPersistStream Members
+
+        /// <summary>
+        /// Returns the class identifier (CLSID) <see cref="Guid"/> for the component object.
+        /// </summary>
+        /// <param name="classID">Pointer to the location of the CLSID <see cref="Guid"/> on 
+        /// return.</param>
+        public void GetClassID(out Guid classID)
+        {
+            classID = GetType().GUID;
+        }
+
+        /// <summary>
+        /// Checks the object for changes since it was last saved.
+        /// </summary>
+        /// <returns><see cref="HResult.Ok"/> if changes have been made; 
+        /// <see cref="HResult.False"/> if changes have not been made.</returns>
+        public int IsDirty()
+        {
+            return HResult.FromBoolean(_dirty);
+        }
+
+        /// <summary>
+        /// Initializes an object from the <see cref="IStream"/> where it was previously saved.
+        /// </summary>
+        /// <param name="stream"><see cref="IStream"/> from which the object should be loaded.
+        /// </param>
+        public void Load(System.Runtime.InteropServices.ComTypes.IStream stream)
+        {
+            try
+            {
+
+                // Freshly loaded object is no longer dirty
+                _dirty = false;
+            }
+            catch (Exception ex)
+            {
+                throw ex.CreateComVisible("ELI31995",
+                    "Unable to load FTP file supplier.");
+            }
+        }
+
+        /// <summary>
+        /// Saves an object into the specified <see cref="IStream"/> and indicates whether the 
+        /// object should reset its dirty flag.
+        /// </summary>
+        /// <param name="stream"><see cref="IStream"/> into which the object should be saved.
+        /// </param>
+        /// <param name="clearDirty">Value that indicates whether to clear the dirty flag after the
+        /// save is complete. If <see langword="true"/>, the flag should be cleared. If 
+        /// <see langword="false"/>, the flag should be left unchanged.</param>
+        public void Save(System.Runtime.InteropServices.ComTypes.IStream stream, bool clearDirty)
+        {
+            try
+            {
+                if (clearDirty)
+                {
+                    _dirty = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex.CreateComVisible("ELI31996",
+                    "Unable to save FTP file supplier.");
+            }
+        }
+
+        /// <summary>
+        /// Returns the size in bytes of the stream needed to save the object.
+        /// </summary>
+        /// <param name="size">Pointer to a 64-bit unsigned integer value indicating the size, in 
+        /// bytes, of the stream needed to save this object.</param>
+        public void GetSizeMax(out long size)
+        {
+            size = HResult.NotImplemented;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Code to be executed upon registration in order to add this class to the
+        /// "UCLID File Suppliers" COM category.
+        /// </summary>
+        /// <param name="type">The <paramref name="type"/> being registered.</param>
+        [ComRegisterFunction]
+        [ComVisible(false)]
+        static void RegisterFunction(Type type)
+        {
+            ComMethods.RegisterTypeInCategory(type, ExtractGuids.FileSuppliers);
+        }
+
+        /// <summary>
+        /// Code to be executed upon unregistration in order to remove this class from the
+        /// "UCLID File Suppliers" COM category.
+        /// </summary>
+        /// <param name="type">The <paramref name="type"/> being unregistered.</param>
+        [ComUnregisterFunction]
+        [ComVisible(false)]
+        static void UnregisterFunction(Type type)
+        {
+            ComMethods.UnregisterTypeInCategory(type, ExtractGuids.FileSuppliers);
+        }
+
+        #endregion
+
+        #region IDisposable Members
+
+        /// <summary>
+        /// Releases all resources used by the <see cref="FTPFileSupplier"/>.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <overloads>Releases resources used by the <see cref="FTPFileSupplier"/>.</overloads>
+        /// <summary>
+        /// Releases all unmanaged resources used by the <see cref="FTPFileSupplier"/>.
+        /// </summary>
+        /// <param name="disposing"><see langword="true"/> to release both managed and unmanaged 
+        /// resources; <see langword="false"/> to release only unmanaged resources.</param>		
+        void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+
+            }
+
+            // Dispose of unmanaged resources
+        }
+
+        #endregion IDisposable
+    }
+}
