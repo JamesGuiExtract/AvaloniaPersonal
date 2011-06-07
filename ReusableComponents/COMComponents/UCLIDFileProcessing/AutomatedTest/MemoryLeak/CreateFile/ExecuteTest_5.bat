78769d05bafac7985d71ca8cf615e309685b575b
Joanna_Lee <Joanna_Lee@extract.local>
2011-06-07 09:24:51 -0500

    ﻿ [git-vault-id] Extract$/Engineering@15345/16381

---
+++ ReusableComponents/COMComponents/UCLIDFileProcessing/AutomatedTest/MemoryLeak/CreateFile/ExecuteTest_5.bat
@@ -0,0 +1,12 @@
+REM Clean Source and Destination folders
+call Clean.bat
+
+REM Supply Numbered Files for 6 hours
+START CopyNumberedFiles "..\..\..\..\..\..\ProductDevelopment\AttributeFinder\AFCore\AutomatedTest\Images\Image2.tif" ".\Source" 1 -h6
+START CopyNumberedFiles "..\..\..\..\..\..\ProductDevelopment\AttributeFinder\AFCore\AutomatedTest\Images\Image2.tif.uss" ".\Source" 1 -h6
+
+REM Execute command-line for desired test
+START ProcessFiles.exe MemoryLeak_5.fps /s
+
+REM Start Logging Statistics every minute to a numbered subfolder
+LogProcessStats ProcessFiles 1m .\Stats\Test_5 /el
---
+++ ReusableComponents/COMComponents/UCLIDFileProcessing/AutomatedTest/MemoryLeak/DeleteEmptyFolder/ExecuteTest_2.bat
@@ -0,0 +1,11 @@
+REM Clean Source and Destination folders
+call Clean.bat
+
+REM Supply Numbered Files for 6 hours
+START CopyNumberedFiles "..\..\..\..\..\..\ProductDevelopment\AttributeFinder\AFCore\AutomatedTest\Images\Image2.tif" ".\Source" 1 -h6
+
+REM Execute command-line for desired test
+START ProcessFiles.exe MemoryLeak_2.fps /s
+
+REM Start Logging Statistics every minute to a numbered subfolder
+LogProcessStats ProcessFiles 1m .\Stats\Test_2 /el
