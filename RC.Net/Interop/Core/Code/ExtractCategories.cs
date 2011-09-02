d08c6abdfa9389033fd62e7c1157ccb1a01a3fd1
Steve_Kurth <Steve_Kurth@extract.local>
2011-09-02 12:48:45 -0500

    ﻿ [git-vault-id] Extract$/Engineering@16311/17455

---
+++ RC.Net/Interop/Core/Code/ExtractCategories.cs
@@ -0,0 +1,142 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extract.Interop
+{
+    /// <summary>
+    /// Represents a collection of globally unique identifiers of Extract Systems COM categories.
+    /// </summary>
+    public static class ExtractCategories
+    {
+        /// <summary>
+        /// GUID for attribute finder conditions
+        /// </summary>
+        public static readonly string AttributeFinderConditionsGuid = "{B12635F5-5BB7-4743-99D8-A379E6CCE596}";
+
+        /// <summary>
+        /// Name for attribute finder conditions
+        /// </summary>
+        public static readonly string AttributeFinderConditionsName = "UCLID AF-API Conditions";
+
+        /// <summary>
+        /// GUID for attribute selectors
+        /// </summary>
+        public static readonly string AttributeSelectorsGuid = "{E791B4DA-3FAC-4BA7-A589-5BA75E491B29}";
+
+        /// <summary>
+        /// Name for attribute selectors
+        /// </summary>
+        public static readonly string AttributeSelectorsName = "UCLID AF-API Selectors";
+
+        /// <summary>
+        /// GUID for attribute splitters
+        /// </summary>
+        public static readonly string AttributeSplittersGuid = "{4775B20B-FBD9-4307-BCC6-F8B281220D82}";
+
+        /// <summary>
+        /// Name for attribute splitters
+        /// </summary>
+        public static readonly string AttributeSplittersName = "UCLID AF-API Attribute Splitters";
+
+        /// <summary>
+        /// GUID for database managers
+        /// </summary>
+        public static readonly string DatabaseManagersGuid = "{1ED1F6BF-00DE-488a-AF8F-596403BE01D2}";
+
+        /// <summary>
+        /// Name for database managers
+        /// </summary>
+        public static readonly string DatabaseManagersName = "Extract FAM Product Specific DB Managers";
+
+        /// <summary>
+        /// GUID for data scorers
+        /// </summary>
+        public static readonly string DataScorersGuid = "{54FF3F6D-BCE2-49D5-9D53-420CE7A00E59}";
+
+        /// <summary>
+        /// Name for data scorers
+        /// </summary>
+        public static readonly string DataScorersName = "UCLID AF-API Data Scorers";
+
+        /// <summary>
+        /// GUID for document preprocessors
+        /// </summary>
+        public static readonly string DocumentPreprocessorsGuid = "{6C1D4B16-B3A2-4cf0-A97A-BF1F178C0A10}";
+
+        /// <summary>
+        /// Name for document preprocessors
+        /// </summary>
+        public static readonly string DocumentPreprocessorsName = "UCLID AF-API Document Preprocessors";
+
+        /// <summary>
+        /// GUID for File Action Manager conditions
+        /// </summary>
+        public static readonly string FileActionManagerConditionsGuid = "{BE9FBE5A-892A-4621-9D05-ADF323AE83F8}";
+
+        /// <summary>
+        /// Name for File Action Manager conditions
+        /// </summary>
+        public static readonly string FileActionManagerConditionsName = "Extract FAM Conditions";
+
+        /// <summary>
+        /// GUID for File Action Manager reports
+        /// </summary>
+        public static readonly string FileActionManagerReportsGuid = "{06B9A398-70FC-4965-A42C-E5B60D7D5288}";
+
+        /// <summary>
+        /// Name for File Action Manager reports
+        /// </summary>
+        public static readonly string FileActionManagerReportsName = "Extract FAM Reports";
+
+        /// <summary>
+        /// GUID for file processors
+        /// </summary>
+        public static readonly string FileProcessorsGuid = "{6A3E6046-2BA3-458f-A74F-DE219860DEBE}";
+
+        /// <summary>
+        /// Name for file processors
+        /// </summary>
+        public static readonly string FileProcessorsName = "UCLID File Processors";
+
+        /// <summary>
+        /// GUID for file suppliers
+        /// </summary>
+        public static readonly string FileSuppliersGuid = "{49D36CE8-61E5-49b4-9397-728845C0F27F}";
+
+        /// <summary>
+        /// Name for file suppliers
+        /// </summary>
+        public static readonly string FileSuppliersName = "Extract File Suppliers";
+
+        /// <summary>
+        /// GUID for output handlers
+        /// </summary>
+        public static readonly string OutputHandlersGuid = "{1B84DB33-2B7E-49d2-BB16-A4A2283D5D9F}";
+
+        /// <summary>
+        /// Name for output handlers
+        /// </summary>
+        public static readonly string OutputHandlersName = "UCLID AF-API Output Handlers";
+
+        /// <summary>
+        /// GUID for value finders
+        /// </summary>
+        public static readonly string ValueFindersGuid = "{723C425A-795B-45EC-AA49-4A478EEDBCAB}";
+
+        /// <summary>
+        /// Name for value finders
+        /// </summary>
+        public static readonly string ValueFindersName = "UCLID AF-API Value Finders";
+
+        /// <summary>
+        /// GUID for value modifiers
+        /// </summary>
+        public static readonly string ValueModifiersGuid = "{723C426B-795B-45EC-AA49-4A478EEDBCAB}";
+
+        /// <summary>
+        /// Name for value modifiers
+        /// </summary>
+        public static readonly string ValueModifiersName = "UCLID AF-API Value Modifiers";
+    }
+}
