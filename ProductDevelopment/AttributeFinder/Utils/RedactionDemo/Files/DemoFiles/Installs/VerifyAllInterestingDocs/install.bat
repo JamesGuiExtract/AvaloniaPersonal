79e1d078848ca9adfce48cc498a2ec7655da92a3
Arvind_Ganesan <Arvind_Ganesan@extract.local>
2012-01-30 11:04:51 -0600

    ﻿Fixed copy and paste error which caused FLEXIDSCore #5023 [git-vault-id] Extract$/Engineering@18696/20155

--- ProductDevelopment/AttributeFinder/Utils/RedactionDemo/Files/DemoFiles/Installs/VerifyAllInterestingDocs/install.bat
+++ ProductDevelopment/AttributeFinder/Utils/RedactionDemo/Files/DemoFiles/Installs/VerifyAllInterestingDocs/install.bat
@@ -1,6 +1,6 @@
 @echo off
 C:
-CD C:\Demo_IDShield\DemoFiles\Installs\AutoRedact
+CD C:\Demo_IDShield\DemoFiles\Installs\VerifyAllInterestingDocs
 
 del ..\..\..\Input\*.* /s /q
 del ..\..\..\FPS\*.* /s /q
