87e7004ab3b617c444a8cf8c948215edcf7fcbac
Steve_Kurth <Steve_Kurth@extract.local>
2012-09-10 08:54:09 -0500

    ﻿The build complained about IStream references in XML comments. Removed these from the comments to make the build happy.
    Review pending…
    
    [git-vault-id] Extract$/Engineering@21017/22626

--- ProductDevelopment/AFIntegrations/NetDMS/NetDMSCustomComponents/Core/Code/NetDMSAdvanceWorkItemTask.cs
+++ ProductDevelopment/AFIntegrations/NetDMS/NetDMSCustomComponents/Core/Code/NetDMSAdvanceWorkItemTask.cs
@@ -369,9 +369,9 @@ namespace NetDMSCustomComponents
         }
 
         /// <summary>
-        /// Initializes an object from the <see cref="IStream"/> where it was previously saved.
+        /// Initializes an object from the IStream where it was previously saved.
         /// </summary>
-        /// <param name="stream"><see cref="IStream"/> from which the object should be loaded.
+        /// <param name="stream">IStream from which the object should be loaded.
         /// </param>
         public void Load(System.Runtime.InteropServices.ComTypes.IStream stream)
         {
@@ -393,10 +393,10 @@ namespace NetDMSCustomComponents
         }
 
         /// <summary>
-        /// Saves an object into the specified <see cref="IStream"/> and indicates whether the 
+        /// Saves an object into the specified IStream and indicates whether the 
         /// object should reset its dirty flag.
         /// </summary>
-        /// <param name="stream"><see cref="IStream"/> into which the object should be saved.
+        /// <param name="stream">IStream into which the object should be saved.
         /// </param>
         /// <param name="clearDirty">Value that indicates whether to clear the dirty flag after the
         /// save is complete. If <see langword="true"/>, the flag should be cleared. If 
