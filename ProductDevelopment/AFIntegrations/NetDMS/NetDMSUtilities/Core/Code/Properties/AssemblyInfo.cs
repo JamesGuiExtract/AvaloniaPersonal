67954843b4e2b5c897be73eca21d206bb386b31d
Steve_Kurth <Steve_Kurth@extract.local>
2012-10-17 16:56:45 -0500

    ﻿- Renamed assemblies and namespaces of NetDMS components to be prefixed with Extract. (e.g., Extract.NetDMSExporter). - Put in place, but currently commented out use of new license ID for NetDMS components.
    Reviewed by:
    William (0/0)
    
    [git-vault-id] Extract$/Engineering@21515/23128

--- ProductDevelopment/AFIntegrations/NetDMS/NetDMSUtilities/Core/Code/Properties/AssemblyInfo.cs
+++ ProductDevelopment/AFIntegrations/NetDMS/NetDMSUtilities/Core/Code/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NetDMSUtils")]
+[assembly: AssemblyTitle("Extract.NetDMSUtils")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Extract Systems")]
