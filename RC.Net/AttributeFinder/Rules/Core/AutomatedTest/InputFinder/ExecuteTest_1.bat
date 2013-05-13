86a79b3ea79b793d5ec4b8a41344e6290f1467a8
Steve_Kurth <Steve_Kurth@extract.local>
2013-05-13 17:27:48 -0500

    ﻿[FlexIDSCore:5294] Enhanced InputFinder memory leak test 1 to be a better test of memory usage.
    [git-vault-id] Extract$/Engineering@24387/46199

--- RC.Net/AttributeFinder/Rules/Core/AutomatedTest/InputFinder/ExecuteTest_1.bat
+++ RC.Net/AttributeFinder/Rules/Core/AutomatedTest/InputFinder/ExecuteTest_1.bat
@@ -4,11 +4,11 @@ call Clean.bat Test_1
 :: Wait a few seconds
 "%ccdir%\sleep" 10s
 
-REM Supply Numbered Files for 4 hours
-START "CopyNumberedFiles" "%ccdir%\CopyNumberedFiles.exe" ".\Image1.tif.uss" ".\Source" 400ms -h4
-
 REM Execute command-line for desired test
 START "ProcessFiles" "%ccdir%\ProcessFiles.exe" MemoryLeak_1.fps /s
 
 REM Start Logging Statistics to numbered subfolder
-"%ccdir%\LogProcessStats.exe" ProcessFiles 5s .\Stats\Test_1 /el
+START "" "%ccdir%\LogProcessStats.exe" ProcessFiles 5s .\Stats\Test_1 /el
+
+REM Supply Numbered Files for 4 hours
+CALL "CopyNumberedSets.bat" "." "Source" 1 12
