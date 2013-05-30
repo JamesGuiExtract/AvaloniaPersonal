9de409a6f0481cbef2aad7063ded896cca6a3900
Wayne_Lenius <Wayne_Lenius@extract.local>
2013-05-30 14:18:52 -0500

    ﻿ [git-vault-id] Extract$/Engineering@24631/46505

---
+++ RC.Net/AttributeFinder/Rules/Core/AutomatedTest/EntityNameDataScorer/ExecuteTest_1.bat
@@ -0,0 +1,11 @@
+REM Clean Source folder
+call Clean.bat
+
+REM Supply numbered file sets every second for 4 hours
+START CopyNumberedSets SourceImages Source 1 4
+
+REM Execute command-line for desired test
+START ProcessFiles.exe MemoryLeak.fps /s
+
+REM Start Logging Statistics to numbered subfolder
+LogProcessStats.exe ProcessFiles.exe 5s .\Stats\Test_1 /el
