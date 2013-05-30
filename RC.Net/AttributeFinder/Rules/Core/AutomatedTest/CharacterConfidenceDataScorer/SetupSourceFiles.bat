64de635af2095ccdfc57b172f807d53175fbd2f8
Wayne_Lenius <Wayne_Lenius@extract.local>
2013-05-30 14:19:05 -0500

    ﻿ [git-vault-id] Extract$/Engineering@24632/46506

---
+++ RC.Net/AttributeFinder/Rules/Core/AutomatedTest/CharacterConfidenceDataScorer/SetupSourceFiles.bat
@@ -0,0 +1,6 @@
+echo Copying two source images from AFCore test folder
+echo off
+copy /y "..\..\..\..\..\..\ProductDevelopment\AttributeFinder\AFCore\AutomatedTest\Images\Image1.tif" .\SourceImages\.
+copy /y "..\..\..\..\..\..\ProductDevelopment\AttributeFinder\AFCore\AutomatedTest\Images\Image1.tif.uss" .\SourceImages\.
+copy /y "..\..\..\..\..\..\ProductDevelopment\AttributeFinder\AFCore\AutomatedTest\Images\Image2.tif" .\SourceImages\.
+copy /y "..\..\..\..\..\..\ProductDevelopment\AttributeFinder\AFCore\AutomatedTest\Images\Image2.tif.uss" .\SourceImages\.
