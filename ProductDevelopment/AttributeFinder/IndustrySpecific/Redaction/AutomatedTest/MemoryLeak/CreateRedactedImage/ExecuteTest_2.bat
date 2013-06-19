9132ff3a799cd65a63b0f979ca695f8112829ff9
Steve_Kurth <Steve_Kurth@extract.local>
2013-06-19 14:11:12 -0500

    ﻿[FlexIDSCore:5322] Fixed memory leak in create redacted image task when applying redactions as annotations.
    [LegacyRCAndUtils:6441]
    Added email settings to DBInfo table.
    
    Also:
    Increased the pace of file copies for the CreateRedactedImage memory leak tests.
    
    Reviewed by:
    William (0/0)
    
    [git-vault-id] Extract$/Engineering@25029/46909

--- ProductDevelopment/AttributeFinder/IndustrySpecific/Redaction/AutomatedTest/MemoryLeak/CreateRedactedImage/ExecuteTest_2.bat
+++ ProductDevelopment/AttributeFinder/IndustrySpecific/Redaction/AutomatedTest/MemoryLeak/CreateRedactedImage/ExecuteTest_2.bat
@@ -2,8 +2,8 @@ REM Clean Source folder
 call Clean.bat Test_2
 
 REM Supply Numbered Files for 4 hours - first VOA files, then image files
-START CopyNumberedFiles.exe "Image1.tif.voa" ".\Source" 100ms -h4
-START CopyNumberedFiles.exe "Image1.tif" ".\Source" 100ms -h4
+START CopyNumberedFiles.exe "Image1.tif.voa" ".\Source" 200ms -h4
+START CopyNumberedFiles.exe "Image1.tif" ".\Source" 200ms -h4
 
 REM Execute command-line for desired test
 START ProcessFiles.exe MemoryLeak_2.fps /s
--- ProductDevelopment/AttributeFinder/IndustrySpecific/Redaction/AutomatedTest/MemoryLeak/CreateRedactedImage/ExecuteTest_5.bat
+++ ProductDevelopment/AttributeFinder/IndustrySpecific/Redaction/AutomatedTest/MemoryLeak/CreateRedactedImage/ExecuteTest_5.bat
@@ -2,8 +2,8 @@ REM Clean Source folder
 call Clean.bat Test_5
 
 REM Supply Numbered Files for 4 hours - first VOA files, then image files
-START CopyNumberedFiles.exe "Image1.pdf.voa" ".\Source" 500ms -h4
-START CopyNumberedFiles.exe "Image1.pdf" ".\Source" 500ms -h4
+START CopyNumberedFiles.exe "Image1.pdf.voa" ".\Source" 1s -h4
+START CopyNumberedFiles.exe "Image1.pdf" ".\Source" 1s -h4
 
 REM Execute command-line for desired test
 START ProcessFiles.exe MemoryLeak_5.fps /s
