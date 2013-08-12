e656fc642dd73c951127d64b32ba90c6d2d05d0d
William_Parr <William_Parr@extract.local>
2013-08-12 15:48:45 -0500

    ﻿ [git-vault-id] Extract$/Engineering@26243/58129

--- ProductDevelopment/Common/BuildLocal.bat
+++ ProductDevelopment/Common/BuildLocal.bat
@@ -4,4 +4,4 @@ SET BUILD_FINISHED_INSTALL=\\BuildVM\D$\ProductReleases
 
 SET BUILD_INSTALL_FILES=\\BuildVM\D$\ProductDevelopment
 
-SET BUILD_INTERNAL_INSTALLS=\\BuildVM\D$\ProductReleases\Internal
+SET BUILD_INTERNAL_INSTALLS=\\BuildVM\D$\ProductReleases_InternalUseOnly
