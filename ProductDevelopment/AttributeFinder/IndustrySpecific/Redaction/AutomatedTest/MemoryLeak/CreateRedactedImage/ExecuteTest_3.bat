87940e20a6d52ffff1c9b356314991a4cedca31f
Steve_Kurth <Steve_Kurth@extract.local>
2013-10-10 08:38:37 -0500

    ﻿CreateRedactedImage memory leak test #3 didn’t work for me; assumes path and environment variables I didn’t have and seemed to copy VOA files to the wrong name. Fixed this. [git-vault-id] Extract$/Engineering@27064/69023

--- ProductDevelopment/AttributeFinder/IndustrySpecific/Redaction/AutomatedTest/MemoryLeak/CreateRedactedImage/ExecuteTest_3.bat
+++ ProductDevelopment/AttributeFinder/IndustrySpecific/Redaction/AutomatedTest/MemoryLeak/CreateRedactedImage/ExecuteTest_3.bat
@@ -1,12 +1,13 @@
+IF DEFINED programfiles(x86) SET programfiles=%programfiles(x86)%
+
 REM Clean Source folder
 call Clean.bat Test_3
 
-REM Supply Numbered Files for 4 hours - first VOA files, then image files
-START CopyNumberedFiles.exe "Image1.tif.voa" ".\Source" 200ms -h4
-START CopyNumberedFiles.exe "Image1.tif" ".\Source" 200ms -h4
+REM Supply 288,000 files
+START CopyNumberedSets.bat OriginalImages Source 1 28
 
 REM Execute command-line for desired test
-START ProcessFiles.exe MemoryLeak_3.fps /s
+START "%programfiles%\ProcessFiles.exe" MemoryLeak_3.fps /s
 
 REM Start Logging Statistics to numbered subfolder
-LogProcessStats ProcessFiles 5s .\Stats\Test_3 /el
+"%programfiles%\LogProcessStats.exe" ProcessFiles 5s .\Stats\Test_3 /el
