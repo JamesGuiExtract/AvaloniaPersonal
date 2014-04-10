6478b41c81f44431f627bb7b97a67670eb75eaf3
Wayne_Lenius <Wayne_Lenius@extract.local>
2014-04-10 10:39:32 -0500

    ﻿ [git-vault-id] Extract$/Engineering@28823/70844

---
+++ ReusableComponents/COMComponents/UCLIDFileProcessing/AutomatedTest/MemoryLeak/EnhanceOCR/ExecuteTest_1.bat
@@ -0,0 +1,14 @@
+REM Clean Source folder
+call Clean.bat Test_1
+
+REM create TestArea
+mkdir Source
+
+REM Supply numbered file sets every 120 seconds for 6 hours
+START CopyNumberedSets SourceImages Source 120 6
+
+REM Execute command-line for desired test
+START ProcessFiles.exe MemoryLeak_1.fps /s
+
+REM Start Logging Statistics to numbered subfolder
+LogProcessStats.exe ProcessFiles 1m .\Stats\Test_1 /el
