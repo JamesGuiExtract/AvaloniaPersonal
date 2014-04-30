12d6cbe1855e0d5b573de005efa9f97c580a9bbb
Steve_Kurth <Steve_Kurth@extract.local>
2014-04-29 21:55:42 -0500

    ﻿Created new DEPProjectTemplateInstaller project which produces a utility that can be used to install Visual Studio templates for creating DEPs. [git-vault-id] Extract$/Engineering@28995/71019

---
+++ RC.Net/UtilityApplications/DEPProjectTemplateInstaller/Core/Code/DEPProjectTemplateInstallerForm.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace DEPProjectTemplateInstaller
+{
+    partial class DEPProjectTemplateInstallerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label label1;
+            System.Windows.Forms.GroupBox groupBox1;
+            this._templateNameTextBox = new System.Windows.Forms.TextBox();
+            this._sourceProjectTextBox = new System.Windows.Forms.TextBox();
+            this._blankRadioButton = new System.Windows.Forms.RadioButton();
+            this._sourceRadioButton = new System.Windows.Forms.RadioButton();
+            this._sourceProjectBrowseButton = new System.Windows.Forms.Button();
+            this._createButton = new System.Windows.Forms.Button();
+            this._labdeRadioButton = new System.Windows.Forms.RadioButton();
+            this._flexIndexRadioButton = new System.Windows.Forms.RadioButton();
+            label1 = new System.Windows.Forms.Label();
+            groupBox1 = new System.Windows.Forms.GroupBox();
+            groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(13, 13);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(85, 13);
+            label1.TabIndex = 0;
+            label1.Text = "Template Name:";
+            // 
+            // _templateNameTextBox
+            // 
+            this._templateNameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._templateNameTextBox.Location = new System.Drawing.Point(16, 29);
+            this._templateNameTextBox.Name = "_templateNameTextBox";
+            this._templateNameTextBox.Size = new System.Drawing.Size(446, 20);
+            this._templateNameTextBox.TabIndex = 1;
+            this._templateNameTextBox.Text = "DEP_Template";
+            // 
+            // _sourceProjectTextBox
+            // 
+            this._sourceProjectTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._sourceProjectTextBox.Enabled = false;
+            this._sourceProjectTextBox.Location = new System.Drawing.Point(16, 147);
+            this._sourceProjectTextBox.Name = "_sourceProjectTextBox";
+            this._sourceProjectTextBox.Size = new System.Drawing.Size(413, 20);
+            this._sourceProjectTextBox.TabIndex = 5;
+            // 
+            // _blankRadioButton
+            // 
+            this._blankRadioButton.AutoSize = true;
+            this._blankRadioButton.Checked = true;
+            this._blankRadioButton.Location = new System.Drawing.Point(16, 101);
+            this._blankRadioButton.Name = "_blankRadioButton";
+            this._blankRadioButton.Size = new System.Drawing.Size(229, 17);
+            this._blankRadioButton.TabIndex = 3;
+            this._blankRadioButton.TabStop = true;
+            this._blankRadioButton.Text = "New projects should start with a blank DEP";
+            this._blankRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // _sourceRadioButton
+            // 
+            this._sourceRadioButton.AutoSize = true;
+            this._sourceRadioButton.Location = new System.Drawing.Point(16, 124);
+            this._sourceRadioButton.Name = "_sourceRadioButton";
+            this._sourceRadioButton.Size = new System.Drawing.Size(264, 17);
+            this._sourceRadioButton.TabIndex = 4;
+            this._sourceRadioButton.Text = "New projects should be based on the DEP project:";
+            this._sourceRadioButton.UseVisualStyleBackColor = true;
+            this._sourceRadioButton.CheckedChanged += new System.EventHandler(this.HandleSourceRadioButton_CheckedChanged);
+            // 
+            // _sourceProjectBrowseButton
+            // 
+            this._sourceProjectBrowseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this._sourceProjectBrowseButton.Enabled = false;
+            this._sourceProjectBrowseButton.Location = new System.Drawing.Point(435, 146);
+            this._sourceProjectBrowseButton.Name = "_sourceProjectBrowseButton";
+            this._sourceProjectBrowseButton.Size = new System.Drawing.Size(27, 20);
+            this._sourceProjectBrowseButton.TabIndex = 6;
+            this._sourceProjectBrowseButton.Text = "...";
+            this._sourceProjectBrowseButton.UseVisualStyleBackColor = true;
+            this._sourceProjectBrowseButton.Click += new System.EventHandler(this.HandleSourceProjectBrowseButton_Click);
+            // 
+            // _createButton
+            // 
+            this._createButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this._createButton.Location = new System.Drawing.Point(340, 175);
+            this._createButton.Name = "_createButton";
+            this._createButton.Size = new System.Drawing.Size(122, 23);
+            this._createButton.TabIndex = 7;
+            this._createButton.Text = "Create Template";
+            this._createButton.UseVisualStyleBackColor = true;
+            this._createButton.Click += new System.EventHandler(this.HandleOkButton_Click);
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(this._flexIndexRadioButton);
+            groupBox1.Controls.Add(this._labdeRadioButton);
+            groupBox1.Location = new System.Drawing.Point(16, 56);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new System.Drawing.Size(446, 39);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Target product";
+            // 
+            // _labdeRadioButton
+            // 
+            this._labdeRadioButton.AutoSize = true;
+            this._labdeRadioButton.Checked = true;
+            this._labdeRadioButton.Location = new System.Drawing.Point(99, 15);
+            this._labdeRadioButton.Name = "_labdeRadioButton";
+            this._labdeRadioButton.Size = new System.Drawing.Size(58, 17);
+            this._labdeRadioButton.TabIndex = 0;
+            this._labdeRadioButton.TabStop = true;
+            this._labdeRadioButton.Text = "LabDE";
+            this._labdeRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // _flexIndexRadioButton
+            // 
+            this._flexIndexRadioButton.AutoSize = true;
+            this._flexIndexRadioButton.Location = new System.Drawing.Point(263, 15);
+            this._flexIndexRadioButton.Name = "_flexIndexRadioButton";
+            this._flexIndexRadioButton.Size = new System.Drawing.Size(80, 17);
+            this._flexIndexRadioButton.TabIndex = 1;
+            this._flexIndexRadioButton.Text = "FLEX Index";
+            this._flexIndexRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // DEPProjectTemplateInstallerForm
+            // 
+            this.AcceptButton = this._createButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 210);
+            this.Controls.Add(groupBox1);
+            this.Controls.Add(this._createButton);
+            this.Controls.Add(this._sourceProjectBrowseButton);
+            this.Controls.Add(this._sourceRadioButton);
+            this.Controls.Add(this._blankRadioButton);
+            this.Controls.Add(this._sourceProjectTextBox);
+            this.Controls.Add(this._templateNameTextBox);
+            this.Controls.Add(label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "DEPProjectTemplateInstallerForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DEP project template installer";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox _templateNameTextBox;
+        private System.Windows.Forms.TextBox _sourceProjectTextBox;
+        private System.Windows.Forms.RadioButton _blankRadioButton;
+        private System.Windows.Forms.RadioButton _sourceRadioButton;
+        private System.Windows.Forms.Button _sourceProjectBrowseButton;
+        private System.Windows.Forms.Button _createButton;
+        private System.Windows.Forms.RadioButton _flexIndexRadioButton;
+        private System.Windows.Forms.RadioButton _labdeRadioButton;
+    }
+}
+
---
+++ RC.Net/UtilityApplications/DEPProjectTemplateInstaller/Core/Code/DEPProjectTemplateInstallerForm.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Threading;
+
+namespace DEPProjectTemplateInstaller
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class DEPProjectTemplateInstallerForm : Form
+    {
+        #region Consts
+
+        /// <summary>
+        /// The name used for the default DEP template.
+        /// </summary>
+        static readonly string _DEP_TEMPLATE = "DEP_Template";
+
+        #endregion Consts
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DEPProjectTemplateInstallerForm"/> class.
+        /// </summary>
+        public DEPProjectTemplateInstallerForm()
+        {
+            InitializeComponent();
+        }
+
+        #endregion Constructors
+
+        #region Event Handlers
+
+        /// <summary>
+        /// Handles the <see cref="Control.Click"/> event of the <see cref="_createButton"/>.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.
+        /// </param>
+        void HandleOkButton_Click(object sender, EventArgs e)
+        {
+            string sourceDir = Path.Combine(
+                Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                _DEP_TEMPLATE);
+
+            string templateName = _templateNameTextBox.Text;
+            string templateFileName = Regex.Replace(templateName, @"[^a-zA-Z0-9_\-\x20]+", "_");
+
+            string installDir = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                @"Visual Studio 2010\Templates\ProjectTemplates",
+                templateFileName);
+            string installZip = installDir + ".zip";
+
+            if (File.Exists(installZip))
+            {
+                if (MessageBox.Show("The specified template already exists. Replace?", "Replace template?",
+                    MessageBoxButtons.OKCancel, MessageBoxIcon.Question) != DialogResult.OK)
+                {
+                    return;
+                }
+            }
+
+            InstallTemplateFiles(sourceDir, installDir);
+
+            ApplyTemplateName(templateName, installDir);
+
+            if (_sourceRadioButton.Checked && File.Exists(_sourceProjectTextBox.Text))
+            {
+                CopyTargetProjectFiles(installDir);
+            }
+
+            if (File.Exists(installZip))
+            {
+                File.Delete(installZip);
+            }
+            CreateZip(installDir, installZip);
+
+            Directory.Delete(installDir, true);
+
+            MessageBox.Show("Template created.", "Success", MessageBoxButtons.OK);
+        }
+
+        /// <summary>
+        /// Handles the <see cref="Control.Click"/> event of the
+        /// <see cref="_sourceProjectBrowseButton"/>.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.
+        /// </param>
+        void HandleSourceProjectBrowseButton_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFile = new OpenFileDialog())
+            {
+                if (File.Exists(_sourceProjectTextBox.Text))
+                {
+                    openFile.InitialDirectory = Path.GetDirectoryName(_sourceProjectTextBox.Text);
+                }
+
+                openFile.Filter = "C# Project Files (*.csproj)|*.csproj";
+                openFile.FilterIndex = 0;
+                openFile.AddExtension = true;
+                openFile.Multiselect = false;
+                openFile.CheckFileExists = true;
+                openFile.CheckPathExists = true;
+
+                if (openFile.ShowDialog() == DialogResult.OK)
+                {
+                    _sourceProjectTextBox.Text = openFile.FileName;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handles the <see cref="RadioButton.CheckedChanged"/> event of the
+        /// <see cref="_sourceRadioButton"/>.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.
+        /// </param>
+        void HandleSourceRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            _sourceProjectTextBox.Enabled = _sourceRadioButton.Checked;
+            _sourceProjectBrowseButton.Enabled = _sourceRadioButton.Checked;
+        }
+
+        #endregion Event Handlers
+
+        #region Private Members
+
+        /// <summary>
+        /// Installs appropriate product-specific files.
+        /// </summary>
+        /// <param name="sourceDir">The un-configured source template files.</param>
+        /// <param name="installDir">The template install directory.</param>
+        void InstallTemplateFiles(string sourceDir, string installDir)
+        {
+            CopyDirectory(sourceDir, installDir, true);
+
+            string labDEProj = Path.Combine(installDir, "LabDE.csproj");
+            string labDEConfig = Path.Combine(installDir, "LabDE.config");
+            string flexIndexProj = Path.Combine(installDir, "FlexIndex.csproj");
+            string flexIndexConfig = Path.Combine(installDir, "FlexIndex.config");
+            string targetProj = Path.Combine(installDir, _DEP_TEMPLATE + ".csproj");
+            string targetConfig = Path.Combine(installDir, _DEP_TEMPLATE + ".config");
+            if (_labdeRadioButton.Checked)
+            {
+                File.Move(labDEProj, targetProj);
+                File.Move(labDEConfig, targetConfig);
+                File.Delete(flexIndexProj);
+                File.Delete(flexIndexConfig);
+            }
+            else
+            {
+                File.Move(flexIndexProj, targetProj);
+                File.Move(flexIndexConfig, targetConfig);
+                File.Delete(labDEProj);
+                File.Delete(labDEConfig);
+            }
+        }
+
+        /// <summary>
+        /// Applies the name of the template to the template files.
+        /// </summary>
+        /// <param name="templateName">Th name to apply to the template.</param>
+        /// <param name="installDir">The directory the template is installed into.</param>
+        static void ApplyTemplateName(string templateName, string installDir)
+        {
+            string templateFile = Path.Combine(installDir, _DEP_TEMPLATE) + ".vstemplate";
+            if (templateName != _DEP_TEMPLATE)
+            {
+                ReplaceText(templateFile,
+                    "<Name>" + _DEP_TEMPLATE + "</Name>",
+                    "<Name>" + XmlEscape(templateName) + "</Name>");
+
+                if (!templateName.Equals(_DEP_TEMPLATE, StringComparison.OrdinalIgnoreCase))
+                {
+                    string newTemplateFile =
+                        Path.Combine(installDir, new DirectoryInfo(installDir).Name) + ".vstemplate";
+                    File.Move(templateFile, newTemplateFile);
+
+                    string formFile =
+                        Path.Combine(installDir, _DEP_TEMPLATE) + "Panel.cs";
+                    ReplaceText(formFile, _DEP_TEMPLATE, "$safeprojectname$");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Copies the target project files into the new template and renames members to match the
+        /// template name.
+        /// </summary>
+        /// <param name="templateDir">The template directory.</param>
+        void CopyTargetProjectFiles(string templateDir)
+        {
+            string sourceProjectDir = Path.GetDirectoryName(_sourceProjectTextBox.Text);
+            string oldProjectName =
+                Regex.Match(_sourceProjectTextBox.Text, @"(?<=Extract\.DataEntry\.DEP\.)[\w]+(?=\.csproj)")
+                .Value;
+
+            string designerFile = Directory.GetFiles(sourceProjectDir, "*.Designer.cs").Single();
+            string newDesignerFile =
+                Path.Combine(templateDir, _DEP_TEMPLATE) + "Panel.Designer.cs";
+            File.Copy(designerFile, newDesignerFile, true);
+            ReplaceText(newDesignerFile, oldProjectName, "$safeprojectname$");
+
+            string resxFile = Directory.GetFiles(sourceProjectDir, "*.resx").Single();
+            string newResxFile =
+                Path.Combine(templateDir, _DEP_TEMPLATE) + "Panel.resx";
+            File.Copy(resxFile, newResxFile, true);
+        }
+
+        static void CopyDirectory(string source, string destination, bool recursive)
+        {
+            // Ensure the destination path ends in the directory separator character
+            if (destination[destination.Length - 1] != Path.DirectorySeparatorChar)
+            {
+                destination += Path.DirectorySeparatorChar;
+            }
+
+            // If the destination doesn't exist, create it
+            if (!Directory.Exists(destination))
+            {
+                Directory.CreateDirectory(destination);
+            }
+
+            // For each element in the source directory copy it
+            foreach (string element in Directory.GetFileSystemEntries(source))
+            {
+                // Check if this is a directory
+                if (Directory.Exists(element))
+                {
+                    // If it is a directory and recursion was specified,
+                    // copy the sub-directory files
+                    if (recursive)
+                    {
+                        CopyDirectory(element, destination + Path.GetFileName(element), true);
+                    }
+                }
+                else
+                {
+                    // Copy the file
+                    File.Copy(element, destination + Path.GetFileName(element), true);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Escapes the specified tring for use in XML.
+        /// </summary>
+        /// <param name="unescaped">The unescaped string.</param>
+        /// <returns>The escaped string.</returns>
+        static string XmlEscape(string unescaped)
+        {
+            XmlDocument doc = new XmlDocument();
+            XmlNode node = doc.CreateElement("root");
+            node.InnerText = unescaped;
+            return node.InnerXml;
+        }
+
+        /// <summary>
+        /// Replaces the specified originalText in the specified file with the newText.
+        /// </summary>
+        /// <param name="fileName">Name of the file.</param>
+        /// <param name="originalText">The original text.</param>
+        /// <param name="newText">The new text.</param>
+        static void ReplaceText(string fileName, string originalText, string newText)
+        {
+            string fileText = File.ReadAllText(fileName);
+            fileText = fileText.Replace(originalText, newText);
+            File.Delete(fileName);
+            File.WriteAllText(fileName, fileText);
+        }
+
+        /// <summary>
+        /// Zips the specified directory into the specified zip archive.
+        /// <para><b>Note</b></para>
+        /// This code is from Gerald Gibson Jr's article here:
+        /// http://www.codeproject.com/Articles/12064/Compress-Zip-files-with-Windows-Shell-API-and-C
+        /// </summary>
+        /// <param name="sourceDirectory">The directory to zip.</param>
+        /// <param name="destFileName">The target zip file archive. Must end in .zip</param>
+        static void CreateZip(string sourceDirectory, string destFileName)
+        {
+            byte[] emptyZip = new byte[]
+                {80, 75, 5, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+            FileStream fs = File.Create(destFileName);
+            fs.Write(emptyZip, 0, emptyZip.Length);
+            fs.Flush();
+            fs.Close();
+            fs = null;
+
+            //Copy a folder and its contents into the newly created zip file
+            Shell32.ShellClass sc = new Shell32.ShellClass();
+            Shell32.Folder sourceFolder = sc.NameSpace(sourceDirectory);
+            Shell32.Folder destFolder = sc.NameSpace(destFileName);
+            Shell32.FolderItems items = sourceFolder.Items();
+            destFolder.CopyHere(items, 20);
+
+            // Ziping a file using the Windows Shell API creates another thread where the zipping is
+            // executed. This means that it is possible that this thread or event handler would end
+            // before the zipping thread  starts to execute which would cause the zip to never occur
+            // and you will end up with just an empty zip file. So wait a second and give the
+            // zipping thread time to get started.
+            Thread.Sleep(1000);
+        }
+    }
+
+    #endregion Private Members
+}
