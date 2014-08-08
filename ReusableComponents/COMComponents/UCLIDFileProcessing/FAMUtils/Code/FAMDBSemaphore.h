b648d3279118911bd207e4f7a74bd5447694587f
Steve_Kurth <Steve_Kurth@extract.local>
2014-08-08 10:43:13 -0500

    ﻿RC\CC\UCLIDFileProcessing RC\CC\UCLIDFileProcessing\FAMUtils
    https://extract.atlassian.net/browse/ISSUE-12328
    Encapsulated code intended to synchronize access to FileProcessingDB’s lockDB and unlockDB calls within the same thread into a new FAMUtils helper class (FAMDBSemaphore). Used this to cause an exception to be thrown before getting into a deadlock situation and to enhance LockGuard to prevent nested instances from creating a potential deadlock situation.
    
    Reviewed by:
    William (2/0)
    
    [git-vault-id] Extract$/Engineering@30029/72144

---
+++ ReusableComponents/COMComponents/UCLIDFileProcessing/FAMUtils/Code/FAMDBSemaphore.h
@@ -0,0 +1,42 @@
+// DBLockGuard.h : Declaration of the DBLockGuard
+
+#pragma once
+
+#include "FAMUtils.h"
+
+#include <afxmt.h>
+
+#include <string>
+
+using namespace std;
+
+// https://extract.atlassian.net/browse/ISSUE-12328
+// A class used to synchronize access amongst all threads in a process to the FileProcessingDB's
+// lock and unl
+class FAMUTILS_API FAMDBSemaphore
+{
+public:
+
+	// Gets a reference to a boolean indicating whether the current thread already has strLockName.
+	static bool IsLocked(const string &strLockName);
+	static bool ThisThreadHasLock(const string &strLockName);
+	static bool Lock(const string &strLockName, DWORD dwLockTimeout);
+	static void Unlock(const string &strLockName);
+
+private:
+	static void getSyncObjects(const string &strLockName, CSemaphore *&pSemaphore,
+		DWORD *&pdwLockThreadId);
+
+	// Semaphores indicating that this instance has the specified lock on the DB. The semaphores
+	// prevent multiple threads from each instance from sharing the lock.
+	// NOTE: If other locks are added, be sure to add the map entry in the constructor for the new
+	// lock.
+	// [FlexIDSCore:5244]
+	// Semaphores must be used instead of mutexes since it cannot be guaranteed that Lock will be
+	// called on the same thread Unlock even though both calls originate from the same thread.
+	static CSemaphore ms_semaphoreMainLock;
+	static CSemaphore ms_semaphoreCounterLock;
+
+	static DWORD ms_dwMainLockThread;
+	static DWORD ms_dwCounterLockThread;
+};
--- ReusableComponents/COMComponents/UCLIDFileProcessing/FAMUtils/Code/LockGuard.h
+++ ReusableComponents/COMComponents/UCLIDFileProcessing/FAMUtils/Code/LockGuard.h
@@ -1,23 +1,37 @@
 // DBLockGuard.h : Declaration of the DBLockGuard
 
 #pragma once
+#include "FAMDBSemaphore.h"
+
 #include <UCLIDException.h>
+
 
 // Template for use with a database smartpointer that has a LockDB() and UnlockDB() method
 template<class T> class LockGuard 
 {
 public:
-	// PROMISE: To lock the database. This will call ipDB->LockDB() 
-	// NOTE: Do not nest usage of LockGuard. If LockDB is called twice on the same thread without
-	// first calling Unlock, it will deadlock.
+	// PROMISE: To lock the database. This will call ipDB->LockDB()
+	// NOTE: If LockGuard is used while the database is already locked, the instance will not lock
+	// the database or unlock it when destroyed (and will not throw any errors).
 	LockGuard(T ipDB, const string& strLockName)
 		: m_strLockName(strLockName)
+		, m_bLockedByThisInstance(false)
 	{
 		ASSERT_ARGUMENT("ELI19755", ipDB != __nullptr );
 		m_ipDB = ipDB;
 
-		// lock the database
-		m_ipDB->LockDB(m_strLockName.c_str());
+		// https://extract.atlassian.net/browse/ISSUE-12328
+		// Don't attempt to lock the DB if a higher scope has already locked the DB on this thread.
+		if (!FAMDBSemaphore::ThisThreadHasLock(strLockName))
+		{
+			// lock the database
+			m_ipDB->LockDB(m_strLockName.c_str());
+			m_bLockedByThisInstance = true;
+		}
+		else
+		{
+			m_bLockedByThisInstance = false;
+		}
 	}
 
 	// PROMISE: To unlock the database. This will call m_ipDB->unlockDB() and unlock the m_ipDB->m_mutex
@@ -28,8 +42,11 @@ public:
 		// and don't want to throw an exception from a catch
 		try
 		{
-			// Unlock the DB
-			m_ipDB->UnlockDB(m_strLockName.c_str());
+			// Unlock the DB only if this instance locked it.
+			if (m_bLockedByThisInstance)
+			{
+				m_ipDB->UnlockDB(m_strLockName.c_str());
+			}
 		}
 		CATCH_AND_LOG_ALL_EXCEPTIONS("ELI19754");
 	}
@@ -37,4 +54,5 @@ public:
 private:
 	T m_ipDB;	
 	string m_strLockName;
+	bool m_bLockedByThisInstance;
 };
