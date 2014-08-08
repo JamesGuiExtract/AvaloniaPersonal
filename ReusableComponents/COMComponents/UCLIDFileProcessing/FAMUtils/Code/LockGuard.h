c63cdf01ded8639edacad88e748022beec2496b2
Steve_Kurth <Steve_Kurth@extract.local>
2014-08-08 13:17:00 -0500

    ﻿ [git-vault-id] Extract$/Engineering@30030/72146

--- ReusableComponents/COMComponents/UCLIDFileProcessing/FAMUtils/Code/LockGuard.h
+++ ReusableComponents/COMComponents/UCLIDFileProcessing/FAMUtils/Code/LockGuard.h
@@ -1,37 +1,23 @@
 // DBLockGuard.h : Declaration of the DBLockGuard
 
 #pragma once
-#include "FAMDBSemaphore.h"
-
 #include <UCLIDException.h>
-
 
 // Template for use with a database smartpointer that has a LockDB() and UnlockDB() method
 template<class T> class LockGuard 
 {
 public:
-	// PROMISE: To lock the database. This will call ipDB->LockDB()
-	// NOTE: If LockGuard is used while the database is already locked, the instance will not lock
-	// the database or unlock it when destroyed (and will not throw any errors).
+	// PROMISE: To lock the database. This will call ipDB->LockDB() 
+	// NOTE: Do not nest usage of LockGuard. If LockDB is called twice on the same thread without
+	// first calling Unlock, it will deadlock.
 	LockGuard(T ipDB, const string& strLockName)
 		: m_strLockName(strLockName)
-		, m_bLockedByThisInstance(false)
 	{
 		ASSERT_ARGUMENT("ELI19755", ipDB != __nullptr );
 		m_ipDB = ipDB;
 
-		// https://extract.atlassian.net/browse/ISSUE-12328
-		// Don't attempt to lock the DB if a higher scope has already locked the DB on this thread.
-		if (!FAMDBSemaphore::ThisThreadHasLock(strLockName))
-		{
-			// lock the database
-			m_ipDB->LockDB(m_strLockName.c_str());
-			m_bLockedByThisInstance = true;
-		}
-		else
-		{
-			m_bLockedByThisInstance = false;
-		}
+		// lock the database
+		m_ipDB->LockDB(m_strLockName.c_str());
 	}
 
 	// PROMISE: To unlock the database. This will call m_ipDB->unlockDB() and unlock the m_ipDB->m_mutex
@@ -42,11 +28,8 @@ public:
 		// and don't want to throw an exception from a catch
 		try
 		{
-			// Unlock the DB only if this instance locked it.
-			if (m_bLockedByThisInstance)
-			{
-				m_ipDB->UnlockDB(m_strLockName.c_str());
-			}
+			// Unlock the DB
+			m_ipDB->UnlockDB(m_strLockName.c_str());
 		}
 		CATCH_AND_LOG_ALL_EXCEPTIONS("ELI19754");
 	}
@@ -54,5 +37,4 @@ public:
 private:
 	T m_ipDB;	
 	string m_strLockName;
-	bool m_bLockedByThisInstance;
 };
