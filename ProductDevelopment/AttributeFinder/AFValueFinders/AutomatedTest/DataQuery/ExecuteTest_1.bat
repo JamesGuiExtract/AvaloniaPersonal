03db0015d5cfcd98965f30b391ee7ebbd1ba55b0
Steve_Kurth <Steve_Kurth@extract.local>
2015-05-28 10:37:17 -0500

    ﻿Make a couple tweaks to the DataQuery rule object memory leak test. [git-vault-id] Extract$/Engineering@33274/95438

--- ProductDevelopment/AttributeFinder/AFValueFinders/AutomatedTest/DataQuery/ExecuteTest_1.bat
+++ ProductDevelopment/AttributeFinder/AFValueFinders/AutomatedTest/DataQuery/ExecuteTest_1.bat
@@ -1,11 +1,11 @@
 IF DEFINED programfiles(x86) SET programfiles=%programfiles(x86)%
-SET PATH=PATH;%programfiles%\Extract Systems\CommonComponents
+SET PATH=%PATH%;%programfiles%\Extract Systems\CommonComponents
 
 REM Clean Source folder
 call Clean.bat
 
-REM Supply Numbered Files for 4 hours
-START CopyNumberedFiles "..\..\..\AFCore\AutomatedTest\Images\Image1.tif.uss" ".\Source" 3s -h4
+REM Supply Numbered Files for 6 hours
+START CopyNumberedSets Images Source 2 6
 
 REM Execute command-line for desired test
 START ProcessFiles.exe MemoryLeak_1.fps /s
