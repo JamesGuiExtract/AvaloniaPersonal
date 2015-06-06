07992f54b28dfc06419ba01def089a327d58f369
Steve_Kurth <Steve_Kurth@extract.local>
2015-06-06 14:08:56 -0500

    ﻿https://extract.atlassian.net/browse/ISSUE-13072 Removed some exception handling that dealt with errors in the .Net memory reporting system that occur on shutdown that appeared to itself be generating exceptions that crashed the process. For now exceptions reporting unmanaged memory usage to .Net will simply be eaten: exceptions at this point shouldn't have any direct impact on callers (assuming they are not  thrown out)-- negative effects would simply be that the .Net garbage collector would not have as accurate data on the amount of unmanaged memory tied up.
    Review pending…
    
    [git-vault-id] Extract$/Engineering@33429/95593

--- ReusableComponents/BaseUtils/Code/COMUtils.h
+++ ReusableComponents/BaseUtils/Code/COMUtils.h
@@ -9,55 +9,37 @@
 #include <string>
 #include <vector>
 
-// Name of IMemoryManger implmenter to be used to allow IManageableMemory implementers to report
+// Name of IMemoryManger implementer to be used to allow IManageableMemory implementers to report
 // their memory usage.
 #define MEMORY_MANAGER_CLASS	"Extract.Interop.MemoryManager"
 
 // [LegacyRCAndUtils:6460]
 // This macro releases the specified IMemoryManagerPtr, but ignores exception 0x8007042b
 // (The process terminated unexpectedly). While this issue should be fixed properly at some point,
-// in the meantime I can't figure out why this is occuring and if the process has terminated, there
+// in the meantime I can't figure out why this is occurring and if the process has terminated, there
 // is no need to report memory usage to the garbage collector.
 #define RELEASE_MEMORY_MANAGER(ipMemoryManager, strELI) \
 	try \
 	{ \
-		try \
+		if (ipMemoryManager != __nullptr) \
 		{ \
-			if (ipMemoryManager != __nullptr) \
-			{ \
-				ipMemoryManager->ReportUnmanagedMemoryUsage(0); \
-				ipMemoryManager = __nullptr; \
-			} \
+			ipMemoryManager->ReportUnmanagedMemoryUsage(0); \
+			ipMemoryManager = __nullptr; \
 		} \
-		CATCH_ALL_AND_RETHROW_AS_UCLID_EXCEPTION(strELI) \
 	} \
-	catch (UCLIDException &ue) \
-	{ \
-		const vector<NamedValueTypePair>& vecDebugInfo = ue.getDebugVector(); \
-		bool bIsProcessTerminatedException = false; \
-		for (unsigned long n = 0; n < vecDebugInfo.size(); n++) \
-		{ \
-			const NamedValueTypePair& data = vecDebugInfo[n]; \
-			if (data.GetName() == "HRESULT") \
-			{ \
-				const ValueTypePair& debugValue = data.GetPair(); \
-				if (debugValue.getType() == ValueTypePair::kString && \
-					debugValue.getStringValue() == "0x8007042b") \
-				{ \
-					bIsProcessTerminatedException = true; \
-				} \
-			} \
-		} \
-		if (!bIsProcessTerminatedException) \
-		{ \
-			throw ue; \
-		} \
-	}
+	catch (...) { }
+	// https://extract.atlassian.net/browse/ISSUE-13072
+	// It seems that the code attempting to determine if exceptions here could be ignored due to
+	// a process in the midst of shutting down was itself encountering exceptions. I think the
+	// safest change to make for now is to simply ignore any exception trying to report memory
+	// usage. Exceptions here shouldn't have any direct impact on callers (assuming they are not
+	// thrown out)-- negative effects would simply be that the .Net garbage collector would not
+	// have as accurate data on the amount of unmanaged memory tied up.
 
 using namespace std;
 
 //-------------------------------------------------------------------------------------------------
-// REQUIRE: the COM envionment should already have been initialized before these functions
+// REQUIRE: the COM environment should already have been initialized before these functions
 // are called.  CoInitialize() should already have been called, and CoUninitialize() should
 // not yet have been called.
 void EXPORT_BaseUtils createCOMCategory(CATID categoryID, const string& strCategoryName);
@@ -144,7 +126,7 @@ IPersistStreamPtr EXPORT_BaseUtils readObjFromSAFEARRAY(SAFEARRAY *psaData);
 //-------------------------------------------------------------------------------------------------
 // PURPOSE: Writes the ipObj object to a SAFEARRAY
 // PARAMS:  ipObj - the object to stream to a SAFEARRAY
-// NOTE:	it is the responsibilty of the caller to make sure the SAFEARRAY is destroyed
+// NOTE:	it is the responsibility of the caller to make sure the SAFEARRAY is destroyed
 //			when it is done being used
 LPSAFEARRAY EXPORT_BaseUtils writeObjToSAFEARRAY(IPersistStreamPtr ipObj);
 
