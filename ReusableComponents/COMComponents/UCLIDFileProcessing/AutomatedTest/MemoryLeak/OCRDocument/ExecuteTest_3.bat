9c36d7374edbb22510217b0fcb27b30882a1c7ea
Wayne_Lenius <Wayne_Lenius@extract.local>
2015-07-21 14:04:23 -0500

    ﻿Corrected path to test file. https://extract.atlassian.net/browse/ISSUE-13130
    Not reviewed
    [git-vault-id] Extract$/Engineering@34155/96328

--- ReusableComponents/COMComponents/UCLIDFileProcessing/AutomatedTest/MemoryLeak/OCRDocument/ExecuteTest_3.bat
+++ ReusableComponents/COMComponents/UCLIDFileProcessing/AutomatedTest/MemoryLeak/OCRDocument/ExecuteTest_3.bat
@@ -2,7 +2,7 @@ REM Clean Source and Destination folders
 call Clean.bat
 
 REM Supply Numbered Files for 6 hours
-START CopyNumberedFiles "C:\Program Files\Extract Systems\TestingFiles\ProductDevelopment\AttributeFinder\AFCore\AutomatedTest\Images\Image2.tif" ".\Source" 400ms -h6
+START "" CopyNumberedFiles "..\..\..\..\..\..\ProductDevelopment\AttributeFinder\AFCore\AutomatedTest\Images\Image2.tif" ".\Source" 400ms -h6
 
 REM Execute command-line for desired test
 START ProcessFiles.exe MemoryLeak_3.fps /s
