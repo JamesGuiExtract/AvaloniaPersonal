b8c73db5fa036d0ce6edcd4d1fe392ee3b4b6df6
Wayne_Lenius <Wayne_Lenius@extract.local>
2015-07-24 10:45:19 -0500

    ﻿ [git-vault-id] Extract$/Engineering@34188/96361

---
+++ RC.Net/FileActionManager/FileProcessors/Core/Testing/Manual/MemoryLeak/ModifyPdfFile/ExecuteTest_3.bat
@@ -0,0 +1,14 @@
+REM Clean Source and Destination folders
+call Clean.bat Test_1
+
+REM Create Source direcrtory
+md Source
+
+REM Supply numbered file sets every 2 seconds for 6 hours
+START "" CopyNumberedSets SourceImages Source 2 6
+
+REM Execute command-line for desired test
+START "" "C:\Program Files (x86)\Extract Systems\CommonComponents\ProcessFiles.exe" 3_ModifyPdfFile_Highlights.fps /s
+
+REM Start Logging Statistics every minute to a numbered subfolder
+"C:\Program Files (x86)\Extract Systems\CommonComponents\LogProcessStats.exe" ProcessFiles 1m .\Stats\Test_3 /el
