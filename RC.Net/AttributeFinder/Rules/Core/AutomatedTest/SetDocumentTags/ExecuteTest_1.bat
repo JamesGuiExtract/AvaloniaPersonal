72bf218cf64bb523352bfe8dcd044183471f05d6
Wayne_Lenius <Wayne_Lenius@extract.local>
2015-11-02 09:55:56 -0600

    ﻿ [git-vault-id] Extract$/Engineering@35507/97687

---
+++ RC.Net/AttributeFinder/Rules/Core/AutomatedTest/SetDocumentTags/ExecuteTest_1.bat
@@ -0,0 +1,11 @@
+REM Clean Source folder
+call Clean.bat
+
+REM Supply numbered file sets every second for 4 hours
+START "" "C:\Program Files (x86)\Extract Systems\CommonComponents\CopyNumberedFiles.exe" "..\..\..\..\..\..\ProductDevelopment\AttributeFinder\AFCore\AutomatedTest\Images\Image1.tif" ".\Source" 1s -h4
+
+REM Execute command-line for desired test
+START "" "C:\Program Files (x86)\Extract Systems\CommonComponents\ProcessFiles.exe" MemoryLeak_1.fps /s
+
+REM Start Logging Statistics to numbered subfolder
+"C:\Program Files (x86)\Extract Systems\CommonComponents\LogProcessStats.exe" ProcessFiles.exe 1m .\Stats\Test_1 /el
