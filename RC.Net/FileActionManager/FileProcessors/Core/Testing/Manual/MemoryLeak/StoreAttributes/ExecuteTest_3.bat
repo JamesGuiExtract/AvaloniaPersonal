c93f3e22fa1ae5cc82520ca274235b1e0bdfc604
Wayne_Lenius <Wayne_Lenius@extract.local>
2015-11-04 09:36:52 -0600

    ﻿ [git-vault-id] Extract$/Engineering@35539/97720

---
+++ RC.Net/FileActionManager/FileProcessors/Core/Testing/Manual/MemoryLeak/StoreAttributes/ExecuteTest_3.bat
@@ -0,0 +1,17 @@
+:: Clean Source and Destination folders
+call Clean.bat Test_3
+
+:: Get the correct program files directory for 32/64-bit OS
+if defined programfiles(x86) set programfiles=%programfiles(x86)%
+
+:: Set component directory
+set CommonComponentsDir=%programfiles%\Extract Systems\CommonComponents
+
+:: Supply Numbered Files for 6 hours
+START "" "%CommonComponentsDir%\CopyNumberedFiles.exe" "..\..\..\..\..\..\..\..\ProductDevelopment\AttributeFinder\AFCore\AutomatedTest\Images\Image2.tif" ".\Source" 2s -h6
+
+:: Execute command-line for desired test
+START "" "%CommonComponentsDir%\ProcessFiles.exe" 3_StoreAttributes_NoSpatial_NoEmpty.fps /s
+
+:: Start Logging Statistics every minute to a numbered subfolder
+"%CommonComponentsDir%\LogProcessStats.exe" ProcessFiles 1m .\Stats\Test_3 /el
