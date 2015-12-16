caac1694fba3ddfd2c092215e7a6b4407d23cc33
David_Brownstein <David_Brownstein@extract.local>
2015-12-16 14:02:38 -0600

    ﻿ISSUE-13521 Demo_LabDE: Update Rules.bat fails with exceptions Rolled back source files and projects for SQLCompactImporter and SQLCompactExporter. [git-vault-id] Extract$/Engineering@36140/108339

--- RC.Net/UtilityApplications/SQLCompactUtils/SQLCompactImporter/Core/Code/Program.cs
+++ RC.Net/UtilityApplications/SQLCompactUtils/SQLCompactImporter/Core/Code/Program.cs
@@ -1,5 +1,4 @@
 using Extract;
-using Extract.Database;
 using Extract.Licensing;
 using Extract.Utilities;
 using System;
@@ -17,11 +16,86 @@ namespace Extract.SqlCompactImporter
     class Program
     {
         /// <summary>
+        /// The settings that dictate how to import data into a database.
+        /// </summary>
+        class Settings
+        {
+            /// <summary>
+            /// The SQL Compact database to import data into.
+            /// </summary>
+            public string DatabaseFile;
+
+            /// <summary>
+            /// The table name the data should be imported into.
+            /// </summary>
+            public string TableName;
+
+            /// <summary>
+            /// The SQL command used to add the last row.
+            /// </summary>
+            public string CommandText;
+
+            /// <summary>
+            /// The file containing the data to import.
+            /// </summary>
+            public string InputFile;
+
+            /// <summary>
+            /// The string that delimits each row of data in the text file.
+            /// </summary>
+            public string RowDelimiter = Environment.NewLine;
+
+            /// <summary>
+            /// The string that delimits each column of data in the text file.
+            /// </summary>
+            public string ColumnDelimiter = "\t";
+
+            /// <summary>
+            /// Initializes a new <see cref="Settings"/> instance.
+            /// </summary>
+            /// <param name="args">The command-line arguments the application was launched with.</param>
+            public Settings(string[] args)
+            {
+                ExtractException.Assert("ELI27131", "Missing required argument.", args.Length >= 3);
+
+                // Read the mandatory settings
+                DatabaseFile = args[0];
+                TableName = args[1];
+                InputFile = args[2];
+
+                // Scan for optional settings.
+                for (int i = 3; i < args.Length; i++)
+                {
+                    if (args[i].Equals("/rd", StringComparison.OrdinalIgnoreCase))
+                    {
+                        i++;
+                        ExtractException.Assert("ELI27132", "Missing row delimeter value.", i < args.Length);
+
+                        RowDelimiter = ParamUnescape(args[i]);
+                    }
+                    else if (args[i].Equals("/cd", StringComparison.OrdinalIgnoreCase))
+                    {
+                        i++;
+                        ExtractException.Assert("ELI27133", "Missing column delimeter value.", i < args.Length);
+
+                        ColumnDelimiter = ParamUnescape(args[i]);
+                    }
+                    else
+                    {
+                        ExtractException.Assert("ELI27134", "Unrecognized argument: " + args[i], false);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
         /// Populates a table in an SQL compact DB using the data in a text file.
         /// </summary>
         static void Main(string[] args)
         {
-            ImportSettings settings = null;
+            Settings settings = null;
+            int rowsProcessed = 0;
+            int rowsFailed = 0;
 
             try
             {
@@ -40,7 +114,7 @@ namespace Extract.SqlCompactImporter
 
                 // Attempt to load the settings from the command-line argument.  If unsuccessful, 
                 // log the problem, print usage, then return.
-                settings = new ImportSettings(args);
+                settings = new Settings(args);
             }
             catch (Exception ex)
             {
@@ -52,27 +126,112 @@ namespace Extract.SqlCompactImporter
 
             try
             {
-                var result = ImportTable.ImportFromFile(settings);
-                int rowsProcessed = result.Item2.Length;
-                int rowsFailed = result.Item1;
-
-                int rowsAdded = rowsProcessed - rowsFailed;
-                Console.WriteLine("Added " + rowsAdded.ToString(CultureInfo.CurrentCulture) + " rows.");
-                if (rowsFailed > 0)
+                // Attempt to connect to the database
+                string connectionString = "Data Source='" + settings.DatabaseFile + "';";
+                using (SqlCeConnection sqlConnection = new SqlCeConnection(connectionString))
                 {
-                    Console.WriteLine("Failed to import " +
-                        rowsFailed.ToString(CultureInfo.CurrentCulture) + " rows.");
+                    sqlConnection.Open();
 
-                    int counter = 1;
-                    foreach (var line in result.Item2)
+                    string data = File.ReadAllText(settings.InputFile);
+                    string[] rows = data.Split(new string[] { settings.RowDelimiter },
+                            StringSplitOptions.RemoveEmptyEntries);
+
+                    // Obtain information about the columns the data is to be imported into.
+                    List<int> columnSizes = new List<int>();
+                    List<string> columnNames = new List<string>();
+                    string schemaQuery = "SELECT COLUMN_NAME, CHARACTER_MAXIMUM_LENGTH FROM " +
+                        "INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '" + settings.TableName + "'";
+                    using (SqlCeCommand schemaCommand = new SqlCeCommand(schemaQuery, sqlConnection))
                     {
-                        if (line.StartsWith("*", StringComparison.OrdinalIgnoreCase))
+                        using (SqlCeDataReader reader = schemaCommand.ExecuteReader())
                         {
-                            Console.WriteLine("{0}: {1}", counter, line);
-                            ++counter;
+                            for (int i = 0; reader.Read(); i++)
+                            {
+                                columnNames.Add(reader.GetString(0));
+
+                                if (reader.IsDBNull(1))
+                                {
+                                    columnSizes.Add(0);
+                                }
+                                else
+                                {
+                                    columnSizes.Add(reader.GetInt32(1));
+                                }
+                            }
                         }
                     }
-                }
+
+                    ExtractException.Assert("ELI27253",
+                        "Could not find table: " + settings.TableName, columnSizes.Count > 0);
+
+                    // Loop through each row of the input file and it to the DB.
+                    for (rowsProcessed = 0; rowsProcessed < rows.Length; rowsProcessed++)
+                    {
+                        Dictionary<string, string> columnValues =
+                            new Dictionary<string, string>(columnSizes.Count);
+
+                        // Split the input row into columns using the column delimiter.
+                        string[] columns = rows[rowsProcessed].Split(
+                            new string[] { settings.ColumnDelimiter }, StringSplitOptions.None);
+
+                        int columnCount = Math.Min(columns.Length, columnNames.Count);
+                        string[] includedColumns = new string[columnCount];
+                        columnNames.CopyTo(0, includedColumns, 0, columnCount);
+
+                        // Initialize the SQL command used to add the data.
+                        StringBuilder commandText = new StringBuilder("INSERT INTO [");
+                        commandText.Append(settings.TableName);
+                        commandText.Append("] ([");
+                        commandText.Append(string.Join("], [", includedColumns)); 
+                        commandText.Append("]) VALUES (");
+
+                        // Parameterize the data for each column and build them into the command.
+                        for (int i = 0; i < columnCount; i++)
+                        {
+                            string key = "@" + i.ToString(CultureInfo.InvariantCulture);
+                            string value = (i < columns.Length) ? columns[i] : "";
+                            if (columnSizes[i] > 0 && columnSizes[i] < value.Length)
+                            {
+                                value = value.Substring(0, columnSizes[i]);
+                            }
+
+                            columnValues[key] = value;
+                            commandText.Append(key);
+
+                            if (i + 1 < columnCount)
+                            {
+                                commandText.Append(", ");
+                            }
+                        }
+
+                        // Complete the command
+                        commandText.Append(")");
+                        settings.CommandText = commandText.ToString();
+
+                        // Issue the command to add the data.
+                        using (SqlCeCommand command =
+                            new SqlCeCommand(settings.CommandText, sqlConnection))
+                        {
+                            foreach (string key in columnValues.Keys)
+                            {
+                                command.Parameters.AddWithValue(key, columnValues[key]);
+                            }
+
+                            try
+                            {
+                                command.ExecuteNonQuery();
+                            }
+                            catch (Exception ex)
+                            {
+                                rowsFailed++;
+                                Console.WriteLine("Failed to import row \"" + rows[rowsProcessed] +
+                                    "\" with the following error:");
+                                Console.WriteLine(ex.Message);
+                                Console.WriteLine();
+                            }
+                        }
+                    }
+                } 
             }
             catch (Exception ex)
             {
@@ -87,6 +246,14 @@ namespace Extract.SqlCompactImporter
                 Console.WriteLine("ColumnDelimter: " + ParamEscape(settings.ColumnDelimiter));
 
                 Console.WriteLine();
+            }
+
+            int rowsAdded = rowsProcessed - rowsFailed;
+            Console.WriteLine("Added " + rowsAdded.ToString(CultureInfo.CurrentCulture) + " rows.");
+            if (rowsFailed > 0)
+            {
+                Console.WriteLine("Failed to import " +
+                    rowsFailed.ToString(CultureInfo.CurrentCulture) + " rows.");
             }
         }
 
@@ -134,5 +301,27 @@ namespace Extract.SqlCompactImporter
             }
         }
 
+        /// <summary>
+        /// Replaces printable escape sequences for carriage returns, line feeds and tab characters
+        /// with the characters themselves.
+        /// </summary>
+        /// <param name="parameter">The parameter to unescape.</param>
+        /// <returns>The unescaped parameter.</returns>
+        static string ParamUnescape(string parameter)
+        {
+            if (parameter == null)
+            {
+                return null;
+            }
+            else
+            {
+                string unescapedParameter = parameter;
+                unescapedParameter = unescapedParameter.Replace("\\r", "\r");
+                unescapedParameter = unescapedParameter.Replace("\\n", "\n");
+                unescapedParameter = unescapedParameter.Replace("\\t", "\t");
+
+                return unescapedParameter;
+            }
+        }
     }
 }
