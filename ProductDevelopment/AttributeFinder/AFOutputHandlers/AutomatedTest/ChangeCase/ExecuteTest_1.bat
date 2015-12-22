ff6f639d636c2c47dfc8e0e532417ff815ec5a7e
Wayne_Lenius <Wayne_Lenius@extract.local>
2015-12-22 09:57:52 -0600

    ﻿Update ML test files - Seamlessly handle 32-bit or 64-bit O/S - Process TIF instead of USS to avoid requirement for D:\Engineering folder on test machine [git-vault-id] Extract$/Engineering@36265/118453

--- ProductDevelopment/AttributeFinder/AFOutputHandlers/AutomatedTest/ChangeCase/ExecuteTest_1.bat
+++ ProductDevelopment/AttributeFinder/AFOutputHandlers/AutomatedTest/ChangeCase/ExecuteTest_1.bat
@@ -1,11 +1,17 @@
 REM Clean Source folder
 call Clean.bat Test_1
 
+REM Get the correct program files directory for 32/64-bit OS
+if defined programfiles(x86) set programfiles=%programfiles(x86)%
+
+REM Set component directory
+set CommonComponentsDir=%programfiles%\Extract Systems\CommonComponents
+
 REM Supply Numbered Files for 4 hours
-START CopyNumberedFiles "..\..\..\AFCore\AutomatedTest\Images\Image1.tif.uss" ".\Source" 400ms -h4
+START "" "%CommonComponentsDir%\CopyNumberedFiles.exe" "..\..\..\AFCore\AutomatedTest\Images\Image1.tif" ".\Source" 1s -h4
 
 REM Execute command-line for desired test
-START ProcessFiles.exe MemoryLeak_1.fps /s
+START "" "%CommonComponentsDir%\ProcessFiles.exe" MemoryLeak_1.fps /s
 
 REM Start Logging Statistics to numbered subfolder
-LogProcessStats ProcessFiles 5s .\Stats\Test_1 /el
+"%CommonComponentsDir%\LogProcessStats.exe" ProcessFiles 1m .\Stats\Test_1 /el
