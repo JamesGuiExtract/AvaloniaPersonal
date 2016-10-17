5151da07acb2a077e3af530be322fbae8a0dc7d7
david_brownstein <david_brownstein@extractsystems.com>
2016-10-17 17:32:25 -0500

    Create StatisticsReporter project, with working configuration.

---
+++ RC.Net/UtilityApplications/StatisticsReporter/Program.cs
@@ -0,0 +1,21 @@
+﻿using Extract.Licensing;
+using Extract.Utilities;
+using StatisticsReporter.Properties;
+using System;
+using System.Globalization;
+using System.Linq;
+
+namespace StatisticsReporter
+{
+    class Program
+    {
+        static void Main(/*string[] args*/)
+        {
+            // Validate the license
+            LicenseUtilities.LoadLicenseFilesFromFolder(0, new MapLabel());
+            LicenseUtilities.ValidateLicense(LicenseIdName.FlexIndexIDShieldCoreObjects,
+                                             "ELI41500", 
+                                             "Statistics Reporter");
+        }
+    }
+}
