c50380383b2d062026181aeb8e5a76bc7d73253d
david_brownstein <david_brownstein@extractsystems.com>
2016-10-28 17:58:44 -0500

    ISSUE-14202 Create nunit tests similar to TestAdtMergeUnmerge that cover all aspects of LabDE schema in FAMDB

---
+++ RC.Net/DataEntry/LabDE/Core/Testing/Automated/CommonTestMethods.cs
@@ -0,0 +1,93 @@
+﻿using NUnit.Framework;
+using System;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+
+namespace Extract.DataEntry.LabDE.Test
+{
+    class CommonTestMethods
+    {
+        /// <summary>
+        /// Extracts the value from name value pair, delimited by '='.
+        /// </summary>
+        /// <param name="nameValuePair">The name value pair.</param>
+        /// <returns>The requested value.</returns>
+        /// NOTE: This function asserts if the input is null or empty, 
+        /// if there isn't an '=' character in the input,
+        /// or if the value is empty.
+        public static string ValueFromNameValuePair(string nameValuePair)
+        {
+            Assert.That(!String.IsNullOrWhiteSpace(nameValuePair));
+
+            const int notFound = -1;
+            int startOfValue = nameValuePair.IndexOf('=');
+            Assert.That(startOfValue != notFound);
+
+            startOfValue += 1;
+
+            var value = nameValuePair.Substring(startOfValue);
+            Assert.That(!String.IsNullOrWhiteSpace(value));
+
+            return value;
+        }
+
+        /// <summary>
+        /// Gets the SQL results using the specified query.
+        /// </summary>
+        /// <param name="query">The specified query. 
+        /// Note that the query is assumed to only return a single row.</param>
+        /// <returns>A string with all of the column values pasted together with commas. 
+        /// There is no comma on the final column.</returns>
+        /// NOTE: This function waits for an amount of time to ensure that prior operations have had time 
+        /// to be written into the DB tables.
+        public static string GetSqlResults(string query, SqlConnection dbConnection)
+        {
+            const int twoSeconds = 2 * 1000;
+            Thread.Sleep(twoSeconds);
+
+            SqlCommand cmd = new SqlCommand(query, dbConnection);
+            using (var reader = cmd.ExecuteReader())
+            {
+                if (!reader.HasRows)
+                    return "";
+
+                StringBuilder sb = new StringBuilder();
+
+                while (reader.Read())
+                {
+                    int numberOfColumns = reader.FieldCount;
+                    for (int column = 0; column < numberOfColumns; ++column)
+                    {
+                        string value = reader[column].ToString();
+
+                        if (column < numberOfColumns - 1)
+                            sb.AppendFormat("{0}, ", value);
+                        else
+                            sb.AppendFormat("{0}", value);
+                    }
+                }
+
+                return sb.ToString();
+            }
+        }
+
+        /// <summary>
+        /// This function is primarily a logical description of the operation, and a good 
+        /// location for displaying the result and expected strings for trouble-shooting.
+        /// </summary>
+        /// <param name="result">The result (read from the DB tables).</param>
+        /// <param name="expected">The expected value.</param>
+        /// <returns></returns>
+        public static bool ExpectedMatch(string result, string expected)
+        {
+            Debug.WriteLine($"\n  Result: {result}\nExpected: {expected}");
+
+            return result == expected;
+        }
+
+
+    }
+
+}
---
+++ RC.Net/DataEntry/LabDE/Core/Testing/Automated/TestOrderAndEncounterTables.cs
@@ -0,0 +1,261 @@
+﻿using Extract.Testing.Utilities;
+using Extract.Utilities.Forms;
+using NUnit.Framework;
+using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading;
+using UCLID_COMUTILSLib;
+using UCLID_FILEPROCESSINGLib;
+using static Extract.DataEntry.LabDE.Test.CommonTestMethods;
+
+namespace Extract.DataEntry.LabDE.Test
+{
+    [TestFixture]
+    [NUnit.Framework.Category("DataEntryQueries")]
+    public class TestOrderAndEncounterTables
+    {
+        #region Constants        
+
+        /// <summary>
+        /// Patient constants for patients A, B, and C.
+        /// </summary>
+        private const string PatientA = "00000001";
+        private const string PatientB = "00000002";
+
+        static readonly string EncounterMsg_1 = "Resources.EncounterMsg_1.txt";
+        static readonly string EncounterMsg_2 = "Resources.EncounterMsg_2.txt";
+
+        #endregion Constants
+
+        #region Fields        
+        /// <summary>
+        /// Manages the test text files used by these tests (HL7 messages)
+        /// </summary>
+        static TestFileManager<TestOrderAndEncounterTables> _testTextFiles;
+
+        static private SqlConnection _dbConnection;
+
+        static private string _AdtDirectory;
+
+        #endregion Fields
+
+        #region Setup and Teardown
+
+        /// <summary>
+        /// Setup method to initialize the testing environment.
+        /// </summary>
+        [TestFixtureSetUp]
+        public static void Setup()
+        {
+            GeneralMethods.TestSetup();
+
+            _testTextFiles = new TestFileManager<TestOrderAndEncounterTables>();
+
+            //
+            // The nunit config file is here: \Engineering\RC.Net\Core\Testing\Automated\Extract-all.config
+            // The config file used when debugging is here: 
+            //      C:\engineering\RC.Net\DataEntry\LabDE\Core\Testing\Automated\app.config
+            //
+
+            string dbConnectString =
+                ConfigurationManager.ConnectionStrings["LabDECoreTestingAutomatedConnectionString"].ConnectionString;
+            _dbConnection = new SqlConnection(dbConnectString);
+            _dbConnection.Open();
+
+            _AdtDirectory = ConfigurationManager.AppSettings["AdtDropDirectory"];
+
+            CustomizableMessageBox cmb = new CustomizableMessageBox();
+            cmb.Caption = "Check configuration settings";
+            cmb.Text = $"Warning: Verify test configuration!\nADT directory: {_AdtDirectory}\n"+
+                $"DB Connect string: {dbConnectString}\n\n";
+            const int thirtySeconds = 30 * 1000;
+            cmb.Timeout = thirtySeconds;
+            cmb.UseDefaultOkButton = true;
+
+            cmb.Show();
+
+            ProgressStatus progressStatus = new ProgressStatus();
+            if (null != progressStatus)
+            {
+                FileProcessingDB fileProcessingDB = new FileProcessingDB();
+                string connect =
+                    ConfigurationManager.ConnectionStrings["LabDECoreTestingAutomatedConnectionString"].ConnectionString;
+
+                // The connectionString looks like this:
+                // connectionString="server=VM-MAIN;database=Demo_LabDE;Integrated Security=True"
+                string[] parts = connect.Split(';');
+                Assert.That(parts.Length >= 2);
+
+                const int serverIndex = 0;
+                const int databaseIndex = 1;
+
+                string server = ValueFromNameValuePair(parts[serverIndex]);
+                fileProcessingDB.DatabaseServer = server;
+
+                string dbName = ValueFromNameValuePair(parts[databaseIndex]);
+                fileProcessingDB.DatabaseName = dbName;
+
+                // This shouldn't cause the entire test to fail...
+                try
+                {
+                    fileProcessingDB.UpgradeToCurrentSchema(progressStatus);
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+
+        [TestFixtureTearDown]
+        public static void FinalCleanup()
+        {
+            _dbConnection.Close();
+            _dbConnection.Dispose();
+        }
+        #endregion Setup and Teardown
+
+        #region Public Test Functions
+
+        [Test, Category("Interactive")]
+        public static void Test1()
+        {
+            CleanupDb();
+
+            // drop ADT message that should populate the LabDEEncounter table.
+            CopyToDropDirectory(_AdtDirectory, EncounterMsg_1);
+
+            // read the LabDEEncounter table
+            var query = MakeEncounterQuery(PatientA);
+            var result = GetSqlResults(query, _dbConnection);
+
+            // compare results to expected
+            var expected = ExpectedEncounterText(CSN: "12345",
+                                                 PatientMRN: "00000001",
+                                                 EncounterDateTime: "9/8/2016 8:51:35 AM",
+                                                 Department: "64",
+                                                 EncounterType: "R",
+                                                 EncounterProvider: "02046");
+
+            Assert.That(ExpectedMatch(result, expected));
+        }
+
+        // This test builds on the first by updating the Encounter info established in test 1 - 
+        // hence it calls Test1, rather than celaring the DB.
+        // This test updates the the department id (rom 64 to 63), and the encounter type (from R to A)
+        [Test, Category("Interactive")]
+        public static void Test2()
+        {
+            // Setup initial condition for this test, which updates LabDEEncounter.CSN value
+            Test1();
+
+            // drop ADT message that should update the LabDEEncounter table.
+            CopyToDropDirectory(_AdtDirectory, EncounterMsg_2);
+
+            // read the LabDEEncounter table
+            const string CsnValue = "12345";
+            var query = MakeEncounterQuery(PatientA, CsnValue);
+            var result = GetSqlResults(query, _dbConnection);
+
+            // compare results to expected
+            var expected = ExpectedEncounterText(CSN: CsnValue,
+                                                 PatientMRN: "00000001",
+                                                 EncounterDateTime: "9/8/2016 8:51:35 AM",
+                                                 Department: "63",
+                                                 EncounterType: "A",
+                                                 EncounterProvider: "02046");
+
+            Assert.That(ExpectedMatch(result, expected));
+        }
+
+        #endregion Public Test Functions
+
+        #region Private Test functions
+
+        /// <summary>
+        /// Cleans up the database - removes all the test entries from the encounter table.
+        /// </summary>
+        private static void CleanupDb()
+        {
+            string stmt = $"delete from LabDEEncounter where PatientMRN='{PatientA}' OR " +
+                            "PatientMRN='{PatientB}' OR PatientMRN='{PatientC}';";
+
+            SqlCommand cmd = new SqlCommand(stmt, _dbConnection);
+            cmd.ExecuteNonQuery();
+        }
+
+        /// <summary>
+        /// Copies the specified file to the specified "drop" directory. The "drop" directory
+        /// is one which Corepoint has been configured to receive files into.
+        /// </summary>
+        /// <param name="targetDirectory">The target directory.</param>
+        /// <param name="targetFileName">Name of the target resource file.</param>
+        private static void CopyToDropDirectory(string targetDirectory, string targetFileName)
+        {
+            string testFile = _testTextFiles.GetFile(targetFileName);
+            Assert.That(File.Exists(testFile));
+
+            Assert.That(Directory.Exists(targetDirectory));
+            string copyFileTo = Path.Combine(targetDirectory, Path.GetFileName(targetFileName));
+            File.Copy(testFile, copyFileTo);
+
+            // Wait for Corepoint to move the file
+            Debug.WriteLine("");
+
+            const int oneSecond = 1 * 1000;
+            while (File.Exists(copyFileTo))
+            {
+                Thread.Sleep(oneSecond);
+                Debug.Write(".");
+            }
+        }
+
+        /// <summary>
+        /// Makes the patient query for the specified MRN.
+        /// </summary>
+        /// <param name="MRN">The MRN of the patient to query for</param>
+        /// <param name="CSN">an optional CSN to query for</param>
+        /// <returns>returns patient query</returns>
+        private static string MakeEncounterQuery(string MRN, string CSN = null)
+        {
+            string encounterQuery = $"select [CSN], [PatientMRN], [EncounterDateTime], [Department], "+
+                                    "[EncounterType], [EncounterProvider] from "+
+                                    $"[dbo].[LabDEEncounter] where [PatientMRN]='{MRN}'";
+            string csnClause = ";";
+            if (!String.IsNullOrWhiteSpace(CSN))
+            {
+                csnClause = $" AND [CSN]='{CSN}';";
+            }
+
+            return encounterQuery + csnClause;
+        }
+
+        /// <summary>
+        /// Creates the expected text for an encounter.
+        /// </summary>
+        /// <param name="CSN"></param>
+        /// <param name="PatientMRN"></param>
+        /// <param name="EncounterDateTime"></param>
+        /// <param name="Department"></param>
+        /// <param name="EncounterType"></param>
+        /// <param name="EncounterProvider"></param>
+        /// <returns></returns>
+        private static string ExpectedEncounterText(string CSN,
+                                                    string PatientMRN,
+                                                    string EncounterDateTime,
+                                                    string Department,
+                                                    string EncounterType,
+                                                    string EncounterProvider)
+        {
+            return $"{CSN}, {PatientMRN}, {EncounterDateTime}, {Department}, {EncounterType}, {EncounterProvider}";
+        }
+
+
+        #endregion Private Test functions
+
+    }
+
+}
