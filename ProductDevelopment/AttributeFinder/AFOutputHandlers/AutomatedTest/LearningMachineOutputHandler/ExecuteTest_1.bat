372cb1f04925af87ab90952383c5fcf3099a6223
Nat <nathaniel_heyer@extractsystems.com>
2016-11-18 09:50:34 -0600

    Modify batch file to properly use the START command

--- ProductDevelopment/AttributeFinder/AFOutputHandlers/AutomatedTest/LearningMachineOutputHandler/ExecuteTest_1.bat
+++ ProductDevelopment/AttributeFinder/AFOutputHandlers/AutomatedTest/LearningMachineOutputHandler/ExecuteTest_1.bat
@@ -5,12 +5,12 @@ REM Clean Source folder
 call Clean.bat Test_1
 
 REM Supply numbered file sets every 6 seconds for 10 hours
-START CopyNumberedSets Images\AttributeCategorization Source\AttributeCategorization 6 10
-START CopyNumberedSets Images\DocumentCategorization Source\DocumentCategorization 6 10
-START CopyNumberedSets Images\Pagination Source\Pagination 6 10
+START "Copy attribute categorization files" CopyNumberedSets Images\AttributeCategorization Source\AttributeCategorization 6 10
+START "Copy document categorization files" CopyNumberedSets Images\DocumentCategorization Source\DocumentCategorization 6 10
+START "Copy pagination files" CopyNumberedSets Images\Pagination Source\Pagination 6 10
 
 REM Execute command-line for desired test
-START "%ccdir%\ProcessFiles.exe" MemoryLeak_1.fps /s
+START "Run FAM" "%ccdir%\ProcessFiles.exe" MemoryLeak_1.fps /s
 
 REM Start Logging Statistics to numbered subfolder
 "%ccdir%\LogProcessStats.exe" ProcessFiles.exe,SSOCR2.exe,XOCR32b.exe,AdjustImageResolution.exe,CleanupImage.exe,ESConvertToPDF.exe,ESConvertUSSToTxt.exe,ImageFormatConverter.exe,RedactFromXml.exe,RunRules.exe,SQLServerInfo.exe 5s .\Stats\Test_1 /el
