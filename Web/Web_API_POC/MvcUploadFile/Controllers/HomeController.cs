7c0cf29c0ff578fb92bfd3de07b1c95a7e7d1139
david_brownstein <david_brownstein@extractsystems.com>
2017-06-19 10:26:51 -0500

    Fixed MvcFileUpload so that it correctly sends to Document.SubmitFile(). Also added logging of the elapsed time required to send each file, along with file name and size. There is one minor fix in Document.SubmitFile(). Removed dead code, and added logging of the http status code, as per review. Reviewed by Steve.

--- Web/Web_API_POC/MvcUploadFile/Controllers/HomeController.cs
+++ Web/Web_API_POC/MvcUploadFile/Controllers/HomeController.cs
@@ -1,16 +1,13 @@
 ﻿using Microsoft.AspNetCore.Hosting;     // IHostingEnvironment
 using Microsoft.AspNetCore.Http;        // IFormFile
-using Microsoft.AspNetCore.Http.Internal;        // FormFile
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
-using System.IO;                        // FileStream
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Diagnostics;
-
-using Microsoft.Extensions.Options;
+using System.Threading.Tasks;
 
 
 namespace MvcUploadFile.Controllers
@@ -71,7 +68,8 @@ namespace MvcUploadFile.Controllers
                             var response = await client.PostAsync(_options.WebApiPortionOfUrl, content);
 
                             timer.Stop();
-                            log.WriteLine($"Filename: {fileName}, \t\tLength: {file.Length}, \t\tElapsed time (mS): {timer.ElapsedMilliseconds}");
+                            var code = response.StatusCode;
+                            log.WriteLine($"Filename: {fileName}, \t\tLength: {file.Length}, \t\tElapsed time (mS): {timer.ElapsedMilliseconds}, \t\tStatus code: {code}");
                         }
                     }
                 }
@@ -79,6 +77,7 @@ namespace MvcUploadFile.Controllers
             catch (Exception ex)
             {
                 var msg = ex.Message;
+                log.WriteLine($"Error reported: {msg}");
             }
             finally
             {
@@ -87,99 +86,6 @@ namespace MvcUploadFile.Controllers
 
             return View();
         }
-
-/*
-                [HttpPost]
-                public async Task<IActionResult> Index(ICollection<IFormFile> files)
-                {
-                    try
-                    {
-                        using (var client = new HttpClient())
-                        {
-                            client.BaseAddress = new Uri(_options.SiteSpecificUrl);
-                            var token = _options.JWT;
-
-                            foreach (var file in files)
-                            {
-                                if (file.Length <= 0)
-                                    continue;
-
-                                var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
-                                var fileContent = new StreamContent(file.OpenReadStream());
-                                fileContent.Headers.Add("X-FileName", fileName);
-                                fileContent.Headers.Add("X-ContentType", file.ContentType);
-
-                                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-
-                                var response = await client.PostAsync(_options.WebApiPortionOfUrl, fileContent);
-                            }
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        var msg = ex.Message;
-                    }
-
-                    return View();
-                }
-*/
-        /*
-        // This doesn't work for public async Task<IActionResult> SubmitFile(IFormFile file), but
-        // would work for public async Task<IActionResult> SubmitFile()
-        // Note that it _should_ be possible to use Microsoft.aspnetcore.http.Internal.FormFile to do this,
-        // just need to figure out how to use that class - string name arg in CTOR is one issue, 
-        // value for ContentDispositionHeader is another.
-        [HttpPost]
-        public async Task<IActionResult> Index(ICollection<IFormFile> files)
-        {
-            try
-            {
-                using (var client = new HttpClient())
-                {
-                    client.BaseAddress = new Uri(_options.SiteSpecificUrl);
-                    var token = _options.JWT;
-
-                    foreach (var file in files)
-                    {
-                        if (file != null && file.Length > 0)
-                        {
-                            try
-                            {
-                                byte[] data;
-                                using (var br = new BinaryReader(file.OpenReadStream()))
-                                {
-                                    data = br.ReadBytes((int)file.OpenReadStream().Length);
-                                }
-
-                                ByteArrayContent bytes = new ByteArrayContent(data);
-
-                                MultipartFormDataContent multiContent = new MultipartFormDataContent();
-                                multiContent.Add(bytes, "file", file.FileName);
-
-                                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-
-                                var result = await client.PostAsync(_options.WebApiPortionOfUrl, multiContent);
-                                return View();
-                            }
-                            catch (Exception ex)
-                            {
-                                return BadRequest(ex.Message);
-                            }
-                        }
-
-                        return BadRequest("null file");
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                var msg = ex.Message;
-            }
-
-            return View();
-        }
-        */
-
 
         public IActionResult About()
         {
