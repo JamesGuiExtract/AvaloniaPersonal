b01e059fb7343cf067cb4cf58f84ab09338a5ea2
william_parr <william_parr@extractsystems.com>
2017-10-20 15:27:07 -0500

    Updated isInternalToolsLicensed function to check for drive mapped to \\extract.local\all with a valid serial number https://extract.atlassian.net/browse/ISSUE-14998

--- ReusableComponents/BaseUtils/Code/LicenseUtils.cpp
+++ ReusableComponents/BaseUtils/Code/LicenseUtils.cpp
@@ -36,10 +36,14 @@ const unsigned long	gulUCLIDKey16 = 0x6EDC5C7D;
 const unsigned long gulDOMAIN_PATH_SERIAL_NUMBER_FNP = 1558792743;		// for FNP
 const unsigned long gulDOMAIN_PATH_SERIAL_NUMBER_ESITDC01 = 15240731;		// for es-it-dc-01  - Jake's replacement
 
+// Since the DFS shares can be provided by different domain server, currently FNP2 and Lofton, the both volume serial number are needed
+// If another domain server is added -- hex value of Volume Serial number is displayed when dir \\<domain server>\All is ran from cmd prompt
+const unsigned long gulDOMAIN_PATH_ALL_LOFTON = 0x9E5F882D; // For extract.local\all 
+const unsigned long gulDOMAIN_PATH_ALL_FNP2 = 0x1CFA8C0A; // For extract.local\all 
+
 // Associated Network path
 const string gstrDOMAIN_PATH_FNP = "\\fnp2\\internal";		// for FNP
-const string gstrDOMAIN_PATH_ESITDC01 = "\\es-it-dc-01\\internal";		// for es-it-dc-01  - Jake's replacement
-const string gstrDOMAIN_PATH_ESITDC01_JAKE = "\\es-it-dc-01\\Jake-Internal"; // 2nd share that is the same as Internal
+const string gstrDOMAIN_PATH_ALL = "\\extract.local\\all"; // DFS share that contains the license utility
 
 //-------------------------------------------------------------------------------------------------
 // Local functions
@@ -134,24 +138,19 @@ bool isInternalToolsLicensed()
 		}
 	}
 
-	// Check ES-IT-DC-01 if FNP check failed
-	strDrive = getMappedDrive(gstrDOMAIN_PATH_ESITDC01);
-	if ( strDrive == "" )
-	{
-		// There are 2 possible drive mappings that go to the same drive
-		strDrive = getMappedDrive(gstrDOMAIN_PATH_ESITDC01_JAKE);
-	}
+	// Check Extract.local\All if FNP check failed
+	strDrive = getMappedDrive(gstrDOMAIN_PATH_ALL);
 	if (strDrive.length() > 0)
 	{
 		// Append a backslash character
 		strDrive += "\\";
 
-		// Get ES-IT-DC-01's disk serial number
+		// Get  disk serial number
 		unsigned long ulTemp = 0;
 		GetVolumeInformation(strDrive.c_str(), NULL, NULL, &ulTemp, NULL, NULL, NULL, NULL);
 
 		// Compare serial numbers
-		if (ulTemp == gulDOMAIN_PATH_SERIAL_NUMBER_ESITDC01)
+		if (ulTemp == gulDOMAIN_PATH_ALL_FNP2 || ulTemp == gulDOMAIN_PATH_ALL_LOFTON)
 		{
 			return true;
 		}
