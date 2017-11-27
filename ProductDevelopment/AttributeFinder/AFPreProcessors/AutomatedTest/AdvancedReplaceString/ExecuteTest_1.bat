f12d1884db1fdf8d115efb3added7265c442ee21
Nat <nathaniel_heyer@extractsystems.com>
2017-11-27 11:48:38 -0600

    Put FindIfXOf patterns in regex rule memory leak tests https://extract.atlassian.net/browse/ISSUE-14404
    Add call to find-if-x-of test in AdvancedReplaceString/MemoryLeak_Master1.rsd
    Increase rate of copying for AdvancedReplaceString/ExecuteTest_1.bat
    Add all three tests to automated test dat files

--- ProductDevelopment/AttributeFinder/AFPreProcessors/AutomatedTest/AdvancedReplaceString/ExecuteTest_1.bat
+++ ProductDevelopment/AttributeFinder/AFPreProcessors/AutomatedTest/AdvancedReplaceString/ExecuteTest_1.bat
@@ -2,8 +2,8 @@ REM Clean Source folder
 call Clean.bat
 
 REM Supply Numbered Files for 4 hours
-START CopyNumberedFiles "..\..\..\AFCore\AutomatedTest\Images\Image2.tif" ".\Source" 2s -h4
-START CopyNumberedFiles "..\..\..\AFCore\AutomatedTest\Images\Image2.tif.uss" ".\Source" 2s -h4
+START CopyNumberedFiles "..\..\..\AFCore\AutomatedTest\Images\Image2.tif" ".\Source" 500ms -h4
+START CopyNumberedFiles "..\..\..\AFCore\AutomatedTest\Images\Image2.tif.uss" ".\Source" 500ms -h4
 
 REM Execute command-line for desired test
 START ProcessFiles.exe MemoryLeak_1.fps /s
