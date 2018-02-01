JamesGuiExtract/AvaloniaPersonal

commit 704b7a9ae234c54eb022521a97b5676608586644

Add DB access to the LearningMachine classes https://extract.atlassian.net/browse/ISSUE-15217
1/2 of Expand the NERDataCollector and NERTrainer applications to include LearningMachines
https://extract.atlassian.net/browse/ISSUE-15218

Comment some not-fully-implemented online learning methods to get rid of warnings

Change usage of FileDerivedResourceCache to avoid disposing of, possibly, in-use objects

Change post-build cmd for aka plugin to allow it to build (idk why I keep running into problems with this...)

ReusableComponents/COMComponents/UCLIDFileProcessing/Code/FileProcessingDB_Internal.cpp
@@ -1851,6 +1851,7 @@ vector<string> CFileProcessingDB::getTableCreationQueries(bool bIncludeUserTable
 		vecQueries.push_back(gstrCREATE_WORKFLOW);
 		vecQueries.push_back(gstrCREATE_WEB_APP_CONFIG);
 		vecQueries.push_back(gstrCREATE_DATABASE_SERVICE_TABLE);
+		vecQueries.push_back(gstrCREATE_MLMODEL);
 	}
 
 	// Add queries to create tables to the vector
@@ -1889,7 +1890,6 @@ vector<string> CFileProcessingDB::getTableCreationQueries(bool bIncludeUserTable
 	vecQueries.push_back(gstrCREATE_WORKFLOWFILE);
 	vecQueries.push_back(gstrCREATE_WORKFLOWCHANGE);
 	vecQueries.push_back(gstrCREATE_WORKFLOWCHANGEFILE);
-	vecQueries.push_back(gstrCREATE_MLMODEL);
 	vecQueries.push_back(gstrCREATE_MLDATA);
 	vecQueries.push_back(gstrCREATE_REPORTING_VERIFICATION_RATES);
 

commit 7f2bd6be16d68e63663871a33fe00f9e5472d89d

Fix issue with clearing DBs (introduced in 704b7a9ae234c54eb022521a97b5676608586644)

ReusableComponents/COMComponents/UCLIDFileProcessing/Code/FileProcessingDB_Internal.cpp
@@ -1712,7 +1712,6 @@ void CFileProcessingDB::addTables(bool bAddUserTables)
 		vecQueries.push_back(gstrADD_WORKFLOWCHANGEFILE_FAMFILE_FK);
 		vecQueries.push_back(gstrADD_WORKFLOWCHANGEFILE_WORKFLOWCHANGE_FK);
 		vecQueries.push_back(gstrCREATE_WORKFLOWCHANGEFILE_INDEX);
-		vecQueries.push_back(gstrADD_MLDATA_MLMODEL_FK);
 		vecQueries.push_back(gstrADD_MLDATA_FAMFILE_FK);
 		vecQueries.push_back(gstrADD_REPORTING_VERIFICATION_RATES_FAMFILE_FK);
 		vecQueries.push_back(gstrADD_REPORTING_VERIFICATION_RATES_DATABASE_SERVICE_FK);
@@ -1731,6 +1730,7 @@ void CFileProcessingDB::addTables(bool bAddUserTables)
 			vecQueries.push_back(gstrADD_WORKFLOW_OUTPUTFILEMETADATAFIELD_FK);
 			vecQueries.push_back(gstrADD_FILE_HANDLER_WORKFLOW_FK);
 			vecQueries.push_back(gstrADD_WEB_APP_CONFIG_WORKFLOW_FK);
+			vecQueries.push_back(gstrADD_MLDATA_MLMODEL_FK);
 			
 			// Add triggers
 			vecQueries.push_back(gstrCREATE_ACTION_ON_DELETE_TRIGGER);