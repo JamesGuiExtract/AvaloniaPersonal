1046cd026456eed944cbbebceae24c987311af57
Nat <nathaniel_heyer@extractsystems.com>
2018-12-17 13:46:31 -0600

    Add more cmdline args to DocumentAPISubmit Switch to method that builds map of filenames to ids

--- Web/WebAPI/Testing/Manual/DocumentAPISubmit/Program.cs
+++ Web/WebAPI/Testing/Manual/DocumentAPISubmit/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -16,6 +17,9 @@ namespace SubmitDocs
             string root = ".";
             string urlFile = "url.txt";
             string tokenFile = "token.txt";
+            string url = null;
+            string token = null;
+            int batchSize = 50;
 
             for (int i = 0; i < args.Length; i++)
             {
@@ -30,9 +34,24 @@ namespace SubmitDocs
                         urlFile = args[++i];
                         continue;
                     }
+                    else if (flag == "URL")
+                    {
+                        url = args[++i];
+                        continue;
+                    }
                     else if (flag == "TOKENFILE")
                     {
                         tokenFile = args[++i];
+                        continue;
+                    }
+                    else if (flag == "TOKEN")
+                    {
+                        token = args[++i];
+                        continue;
+                    }
+                    else if (flag == "BATCHSIZE")
+                    {
+                        batchSize = int.Parse(args[++i]);
                         continue;
                     }
                 }
@@ -40,35 +59,53 @@ namespace SubmitDocs
             }
 
             HttpClient client = new HttpClient();
-            string url = File.ReadAllText(urlFile).Trim();
+            url = url ?? File.ReadAllText(urlFile).Trim();
             client.BaseAddress = new Uri(url);
 
             client.DefaultRequestHeaders.Accept.Add(
                 new MediaTypeWithQualityHeaderValue("application/json"));
-            string token = File.ReadAllText(tokenFile).Trim();
+            token = token ?? File.ReadAllText(tokenFile).Trim();
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
 
             var t = new Stopwatch();
             t.Start();
 
-            var taskBatches = Directory.EnumerateFiles(root, "*.*", SearchOption.AllDirectories)
+            var files = Directory.EnumerateFiles(root, "*.*", SearchOption.AllDirectories)
                 .Where(f =>
                 {
                     var ext = Path.GetExtension(f).ToUpperInvariant();
                     return ext == ".PDF" || ext == ".TIF";
-                })
-                .Select(file => PostAsync(client, file))
-                .Batch(50);
+                });
 
-            foreach (var tasks in taskBatches)
-            {
-                Task.WaitAll(tasks);
-            }
+            var mapNameToID = PostAll(client, files, batchSize).GetAwaiter().GetResult();
 
             Console.WriteLine(t.Elapsed);
         }
 
-        static async Task PostAsync(HttpClient client, string filename)
+        static async Task<Dictionary<string, int>> PostAll(HttpClient client, IEnumerable<string> files, int batchSize)
+        {
+            var map = new Dictionary<string, int>();
+            var taskBatches = files
+                .Select(file => PostAsync(client, Path.GetFullPath(file)))
+                .Batch(batchSize);
+
+            foreach (var tasks in taskBatches)
+            {
+                while (tasks.Count > 0)
+                {
+                    var finished = await Task.WhenAny(tasks);
+                    tasks.Remove(finished);
+                    if (finished.IsCompleted)
+                    {
+                        map.Add(finished.Result.fileName, finished.Result.id);
+                    }
+                }
+            }
+
+            return map;
+        }
+
+        static async Task<(string fileName, int id)> PostAsync(HttpClient client, string filename)
         {
             var content = new MultipartFormDataContent();
             using (var stream = File.OpenRead(filename))
@@ -81,17 +118,20 @@ namespace SubmitDocs
 
                 if (result.StatusCode == System.Net.HttpStatusCode.Created)
                 {
+                    var obj = JObject.Parse(await result.Content.ReadAsStringAsync());
+                    var id = (int)obj["id"];
                     var output = DateTime.Now.ToLongTimeString() + " " +
                         result.StatusCode.ToString() + ": " +
-                        result.Headers.Location.AbsolutePath.Substring(
-                            result.Headers.Location.AbsolutePath.LastIndexOf('/') + 1);
+                        id;
                     Console.WriteLine(output);
                     Trace.WriteLine(output);
+                    return (filename, id);
                 }
                 else
                 {
                     Console.WriteLine(result);
                     Trace.WriteLine(result);
+                    throw new InvalidOperationException("Exception posting file: " + result.ToString());
                 }
             }
         }
@@ -106,24 +146,22 @@ namespace SubmitDocs
         /// <typeparam name="T">The Type contained in the IEnumerable</typeparam>
         /// <param name="collection">The collection to return batches from</param>
         /// <param name="batchSize">The Size of each batch - last batch will just be the remaining</param>
-        /// <returns>The current batch as an Array</returns>
-        public static IEnumerable<T[]> Batch<T>(this IEnumerable<T> collection, int batchSize)
+        /// <returns>The current batch as a List</returns>
+        public static IEnumerable<List<T>> Batch<T>(this IEnumerable<T> collection, int batchSize)
         {
-            var nextBatch = new T[batchSize];
-            var i = 0;
+            List<T> nextBatch = new List<T>(batchSize);
             foreach(T item in collection)
             {
-                nextBatch[i++] = item;
-                if (i == batchSize)
+                nextBatch.Add(item);
+                if (nextBatch.Count == batchSize)
                 {
                     yield return nextBatch;
-                    nextBatch = new T[batchSize];
-                    i = 0;
+                    nextBatch = new List<T>(batchSize);
                 }
             }
-            if (i > 0)
+            if (nextBatch.Count > 0)
             {
-                yield return nextBatch.Where(item => item != null).ToArray();
+                yield return nextBatch;
             }
         }
     }
