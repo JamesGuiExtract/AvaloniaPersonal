e390a424140cb881edeaa27c3792a7d2401202ee
Steve_Kurth <steve_kurth@extractsystems.com>
2019-07-22 17:02:35 -0500

    Added requirement for MICR license for MICR rule to run. Despite the fact that we do not owe additional royalties for the use of Nuance's MICR engine, continue to use our MICR licensing code to allow for us to charge for it. https://extract.atlassian.net/browse/ISSUE-16485

--- RC.Net/AttributeFinder/Rules/Core/Code/MicrFinder.cs
+++ RC.Net/AttributeFinder/Rules/Core/Code/MicrFinder.cs
@@ -114,7 +114,7 @@ namespace Extract.AttributeFinder.Rules
         /// <summary>
         /// The license id to validate in licensing calls
         /// </summary>
-        const LicenseIdName _LICENSE_ID = LicenseIdName.FlexIndexIDShieldCoreObjects;
+        const LicenseIdName _LICENSE_ID = LicenseIdName.MicrFindingEngineFeature;
 
         /// <summary>
         /// Special MICR chars
