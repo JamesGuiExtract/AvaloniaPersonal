e044dcffec7ecb40811cff1b96f08b8ef3143f87
Nat <nathaniel_heyer@extractsystems.com>
2019-11-28 11:57:15 -0600

    DataEntryApplication build requires .NET Framework 4.6 SDK https://extract.atlassian.net/browse/ISSUE-16815

--- RC.Net/DataEntry/Utilities/DataEntryApplication/Core/Code/BrandingResources/GenerateResourceFiles.bat
+++ RC.Net/DataEntry/Utilities/DataEntryApplication/Core/Code/BrandingResources/GenerateResourceFiles.bat
@@ -4,7 +4,13 @@ REM %3 = FrameworkSDKDir
 
 SETLOCAL
 PUSHD "%1"
-SET resgenPath=%~3bin\NETFX 4.6 Tools
+SET "resgenPath=%~3bin\NETFX 4.6 Tools"
+IF NOT EXIST "%resgenPath%" (
+SET "resgenPath=%~3bin\NETFX 4.6.1 Tools"
+)
+IF NOT EXIST "%resgenPath%" (
+SET "resgenPath=%~3bin\NETFX 4.8 Tools"
+)
 
 FOR /f %%f IN ('dir /b *.resx') DO CALL :CREATE_RESOURCE_FILE %%f %2
 
