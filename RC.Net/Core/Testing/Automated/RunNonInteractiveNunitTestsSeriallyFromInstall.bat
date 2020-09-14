a85818242c28d8dc74cac3dc9c7c2fe0be95ac83
Nat <nathaniel_heyer@extractsystems.com>
2020-09-14 18:45:09 -0500

    Add missing batch file

---
+++ RC.Net/Core/Testing/Automated/RunNonInteractiveNunitTestsSeriallyFromInstall.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+SET ConsoleRunner='%~dp0nunit3-console.exe'
+SET OutputFile=%~dp0TestResults-NonInteractive
+SET Transform=%~dp0html-report.xslt
+SET Script='%~dp0NUnitConsoleRunner.ps1'
+SET DllDir='C:\Program Files (x86)\Extract Systems\CommonComponents'
+SET Options=--where:'cat!=Interactive and cat!=Broken' --dispose-runners --result='%OutputFile%.xml' --result='%OutputFile%.htm;transform=%Transform%' --agents=1 --workers=1
+
+powershell -Command "& %Script% %ConsoleRunner% %DllDir% %Options%"
+
+PAUSE
