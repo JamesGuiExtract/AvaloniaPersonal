4afacea87dd774b210c3946b845337d00dd6f3ef
William Parr <william_parr@extractsystems.com>
2021-06-30 16:33:51 -0500

    NUnitDebug powershell script Example Runs .\NUnitDebug.ps1 -If you set no parameters it will run all .test.dll in the debug folder for all categories except Interactive, Broken, Automated_ADMIN
    .\NUnitDebug.ps1 -FileList C:\Engineering\Binaries\Debug\Extract.FileActionManager.FileProcessors.Test.dll
    - runs all the defaults with the Extract.FileActionManager.FileProcessors.Test.dll
    
    .\NunitDebug.ps1 -FileList (Get-ChildItem C:\Engineering\Binaries\Debug\Extract.FileA*.Test.dll | ForEach-Object{ '"' + $_.FullName + '"' })
    - runs all the defaults with all the files in the Debug folder that match the pattern C:\Engineering\Binaries\Debug\Extract.FileA*.Test.dll
    
    .\NUnitDebug.ps1 -FileList C:\Engineering\Binaries\Debug\Extract.FileActionManager.FileProcessors.Test.dll -AdditionalOptions "-Debug"
    -runs all the defaults with the file C:\Engineering\Binaries\Debug\Extract.FileActionManager.FileProcessors.Test.dll and allowing you to connect with debugger
    
    Parameters as defined in the script
    param (
        [Parameter(HelpMessage="Path for ConsoleRunner exe")]
            [string]$ConsoleRunner="$PSScriptRoot\..\..\..\..\Binaries\Debug\nunit3-console.exe",
        [Parameter(, HelpMessage="Directory containing the test.dlls")]
            [string]$DllDir="$PSScriptRoot\..\..\..\..\Binaries\Debug",
        [Parameter(HelpMessage="Name of output file without extension 2 files will be created .htm and .xml")]
            [string]$OutputFile="$PSScriptRoot\TestResults-NonInteractive",
        [Parameter(HelpMessage="Name of xslt transform file")]
            [string]$Transform="$PSScriptRoot\html-report.xslt",
        [Parameter(HelpMessage="Options for nunit3")]
            [string[]]$Options=("--where:`"cat!=Interactive and cat!=Broken and cat!=Automated_ADMIN`"", "--dispose-runners", "--result=`"$OutputFile.xml`"", "--result=`"$OutputFile.htm;transform=$Transform`"", "--agents=1", "--workers=1"),
        [Parameter(HelpMessage="Options for nunit3")]
            [string[]]$AdditionalOptions,
        [Parameter(HelpMessage="List of files to run with nunint")]
            [string[]]$FileList

---
+++ RC.Net/Core/Testing/Automated/NUnitDebug.ps1
@@ -0,0 +1,27 @@
+param (
+    [Parameter(HelpMessage="Path for ConsoleRunner exe")]
+        [string]$ConsoleRunner="$PSScriptRoot\..\..\..\..\Binaries\Debug\nunit3-console.exe",
+    [Parameter(, HelpMessage="Directory containing the test.dlls")]
+        [string]$DllDir="$PSScriptRoot\..\..\..\..\Binaries\Debug",
+    [Parameter(HelpMessage="Name of output file without extension 2 files will be created .htm and .xml")]
+        [string]$OutputFile="$PSScriptRoot\TestResults-NonInteractive",
+    [Parameter(HelpMessage="Name of xslt transform file")]
+        [string]$Transform="$PSScriptRoot\html-report.xslt",
+    [Parameter(HelpMessage="Options for nunit3")]
+        [string[]]$Options=("--where:`"cat!=Interactive and cat!=Broken and cat!=Automated_ADMIN`"", "--dispose-runners", "--result=`"$OutputFile.xml`"", "--result=`"$OutputFile.htm;transform=$Transform`"", "--agents=1", "--workers=1"),
+    [Parameter(HelpMessage="Options for nunit3")]
+        [string[]]$AdditionalOptions,
+    [Parameter(HelpMessage="List of files to run with nunint")]
+        [string[]]$FileList
+)
+
+
+if (!$FileList){
+    $FileList = @(Get-ChildItem "$dllDir\*.Test.dll" | ForEach-Object{ '"' + $_.FullName + '"' })
+} 
+
+$params = $FileList + $Options + $AdditionalOptions
+
+$ConsoleRunner
+$params
+& $ConsoleRunner $params
