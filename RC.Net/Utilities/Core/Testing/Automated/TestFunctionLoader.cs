f1e82b1d2d3fb4d3f8661d924ae3c96ecc313dbe
Nat <nathaniel_heyer@extractsystems.com>
2021-09-09 12:44:25 -0500

    ISSUE-17681: FSharpPreprocessor: Compiler error seen when processing with a multi-threaded FAM https://extract.atlassian.net/browse/ISSUE-17681

---
+++ RC.Net/Utilities/Core/Testing/Automated/TestFunctionLoader.cs
@@ -0,0 +1,122 @@
+﻿using Extract.Testing.Utilities;
+using Extract.Utilities.FSharp;
+using Microsoft.FSharp.Core;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Extract.Utilities.Test
+{
+    [TestFixture]
+    [Category("FunctionLoader")]
+    [Parallelizable(ParallelScope.All)]
+    public class TestFunctionLoader
+    {
+        [OneTimeSetUp]
+        public static void Initialize()
+        {
+            GeneralMethods.TestSetup();
+        }
+
+        /// Confirm that functions can be loaded from multiple threads at once
+        [Test, Category("Automated")]
+        public static void LoadFunctionMultithreaded()
+        {
+            // Make a script file where the name is a valid module name in F#
+            string scriptFolder = Path.GetTempPath();
+            using TemporaryFile scriptFile = new(scriptFolder, "TestFunctionLoader.LoadFunctionMultithreaded.fsx", null, false);
+            string scriptPath = scriptFile.FileName;
+
+            // Write a function
+            File.WriteAllText(scriptPath, "let addOne x = x + 1");
+
+            FSharpFunc<int, int> creator()
+            {
+                return FunctionLoader.LoadFunction<int>(scriptPath, "addOne", true);
+            }
+
+            // Evaluate the script many times at once to confirm no errors or hangs
+            List<FSharpFunc<int, int>> funs = Enumerable.Range(0, 10)
+                .AsParallel()
+                .Select(_ => creator())
+                .ToList();
+
+            // Confirm that the functions work as expected
+            List<int> results = funs
+                .AsParallel()
+                .Select((fun, i) => fun.Invoke(i))
+                .ToList();
+
+            List<int> exp = Enumerable.Range(1, results.Count).ToList();
+            CollectionAssert.AreEqual(exp, results);
+        }
+
+        /// Confirm that multiple functions can be loaded from multiple threads at once
+        [Test, Category("Automated")]
+        public static void LoadTwoFunctionsMultithreaded()
+        {
+            // Make a script file where the name is a valid module name in F#
+            string scriptFolder = Path.GetTempPath();
+            using TemporaryFile scriptFile = new(scriptFolder, "TestFunctionLoader.LoadTwoFunctionsMultithreaded.fsx", null, false);
+            string scriptPath = scriptFile.FileName;
+
+            // Write functions
+            File.WriteAllText(scriptPath, "let addOne x = x + 1\nlet addTwo x = x + 2");
+
+            FSharpFunc<int, int>[] creator()
+            {
+                return FunctionLoader.LoadFunctions<int>(scriptPath, new[] { "addOne", "addTwo" }, true);
+            }
+
+            // Evaluate the script many times at once to confirm no errors or hangs
+            List<FSharpFunc<int, int>[]> funs = Enumerable.Range(0, 10)
+                .AsParallel()
+                .Select(_ => creator())
+                .ToList();
+
+            // Confirm that the functions work as expected
+            List<int[]> results = funs
+                .AsParallel()
+                .Select((funs, i) => funs.Select(fun => fun.Invoke(i)).ToArray())
+                .ToList();
+
+            List<int[]> exp = Enumerable.Range(0, results.Count)
+                .Select(i => new[] { i + 1, i + 2 })
+                .ToList();
+            CollectionAssert.AreEqual(exp, results);
+        }
+
+        /// Confirm that no exception is thrown for null includeDirectories param
+        [Test, Category("Automated")]
+        public static void LoadFunctionNullIncludeDirectories()
+        {
+            // Make a script file where the name is a valid module name in F#
+            string scriptFolder = Path.GetTempPath();
+            using TemporaryFile scriptFile = new(scriptFolder, "TestFunctionLoader.LoadFunctionNullIncludeDirectories.fsx", null, false);
+            string scriptPath = scriptFile.FileName;
+
+            // Write a function
+            File.WriteAllText(scriptPath, "let addOne x = x + 1");
+
+            // Confirm evaluation works without error
+            FSharpFunc<int, int> fun = FunctionLoader.LoadFunction<int>(scriptPath, "addOne", true, null);
+        }
+
+        /// Confirm that no exception is thrown for null includeDirectories param
+        [Test, Category("Automated")]
+        public static void LoadTwoFunctionsNullIncludeDirectories()
+        {
+            // Make a script file where the name is a valid module name in F#
+            string scriptFolder = Path.GetTempPath();
+            using TemporaryFile scriptFile = new(scriptFolder, "TestFunctionLoader.LoadTwoFunctionsNullIncludeDirectories.fsx", null, false);
+            string scriptPath = scriptFile.FileName;
+
+            // Write functions
+            File.WriteAllText(scriptPath, "let addOne x = x + 1\nlet addTwo x = x + 2");
+
+            // Confirm evaluation works without error
+            FSharpFunc<int, int>[] funs = FunctionLoader.LoadFunctions<int>(scriptPath, new[] { "addOne", "addTwo" }, true, null);
+        }
+    }
+}
--- RC.Net/Utilities/FSharp/CSharpInterop/FunctionLoader.fs
+++ RC.Net/Utilities/FSharp/CSharpInterop/FunctionLoader.fs
@@ -1,59 +1,74 @@
 ﻿module Extract.Utilities.FSharp.FunctionLoader
 
 open System
+open System.IO
+open System.Text
+open System.Threading
+open FSharp.Compiler.Interactive.Shell
+
+open Extract
+open Extract.Utilities
 
 type Transformation<'TData> = 'TData -> 'TData
 
-module Evaluator =
-    open System.IO
-    open System.Text
-    open FSharp.Compiler.Interactive.Shell
+let private fsiLock = obj()
 
-    // Load a script file with fsi and get the specified functions
-    // Assumes the functions are in a module with the same name as the file name, minus extension
-    let evaluate path functionNames includeDirectories collectible =
-        let fullPath = Path.GetFullPath path
-        let scriptDir = Path.GetDirectoryName fullPath
-        Extract.ExtractException.Assert("ELI46985", "Path is not a full path", (path = fullPath), "Path", path);
+// Setup function to create an FSI instance
+let private getEvaluationSessionCreator includeDirectories collectible =
+    let sbOut = StringBuilder()
+    let sbErr = StringBuilder()
+    let inStream = new StringReader("")
+    let outStream = new StringWriter(sbOut)
+    let errStream = new StringWriter(sbErr)
+    let fsiConfig = FsiEvaluationSession.GetDefaultConfiguration()
+    let argv =
+        seq {
+            yield "fsi" // Dummy argument required for historical reasons
+            yield "--simpleresolution" // Do not use MSBuild for reference resolution
+            yield "--quiet" // Don't print to std out
+            yield "--noninteractive" // Don't start a thread to watch for user input
+            yield "--nowarn:211" // Don't warn about nonexistent #Included directories
+            yield "-d:FUNCTION_LOADER" // Allow conditional compilation via #if FUNCTION_LOADER
+            yield! (includeDirectories |> Seq.map (sprintf @"-I:""%s"""))
+        } |> Seq.toArray
+    fun () ->
+        try
+            FsiEvaluationSession.Create(fsiConfig, argv, inStream, outStream, errStream, collectible = collectible)
+        with
+        | ex -> raise (ExtractException("ELI47006", "Error creating FSI session", ex))
 
-        let includeDirectories =
-          seq {
+// Load a script file with fsi and get the specified functions
+// Assumes the functions are in a module with the same name as the file name, minus extension
+let private evaluate path functionNames includeDirectories collectible =
+    let fullPath = Path.GetFullPath path
+    let scriptDir = Path.GetDirectoryName fullPath
+    ExtractException.Assert("ELI46985", "Path is not a full path", (path = fullPath), "Path", path);
+
+    let includeDirectories =
+        seq {
             yield scriptDir
-            yield Extract.Utilities.FileSystemMethods.CommonComponentsPath
-            yield! includeDirectories
-          }
-          |> Seq.distinct
+            yield FileSystemMethods.CommonComponentsPath
+            yield! if includeDirectories |> isNull then [||] else includeDirectories
+        } |> Seq.distinct
 
-        let fsi =
-            let sbOut = StringBuilder()
-            let sbErr = StringBuilder()
-            let inStream = new StringReader("")
-            let outStream = new StringWriter(sbOut)
-            let errStream = new StringWriter(sbErr)
-            try
-                let fsiConfig = FsiEvaluationSession.GetDefaultConfiguration()
-                let argv =
-                  seq {
-                    yield "fsi" // Dummy argument required for historical reasons
-                    yield "--simpleresolution" // Do not use MSBuild for reference resolution
-                    yield "--quiet" // Don't print to std out
-                    yield "--noninteractive" // Don't start a thread to watch for user input
-                    yield "--nowarn:211" // Don't warn about nonexistent #Included directories
-                    yield "-d:FUNCTION_LOADER" // Allow conditional compilation via #if FUNCTION_LOADER
-                    yield! (includeDirectories |> Seq.map (sprintf @"-I:""%s"""))
-                  } |> Seq.toArray
-                FsiEvaluationSession.Create(fsiConfig, argv, inStream, outStream, errStream, collectible = collectible)
-            with
-            | ex ->
-                let uex = Extract.ExtractException("ELI47006", "Error creating FSI session", ex)
-                raise uex
+    let sessionCreator = getEvaluationSessionCreator includeDirectories collectible
 
+    // Prevent deadlock if this thread already has the lock
+    ExtractException.Assert("ELI51863","Cannot create a new FSI session inside an FSI session",
+      not (Monitor.IsEntered fsiLock))
+
+    // Creating a session and evaluating code isn't threadsafe so use a lock around all FSI evaluation code
+    // https://extract.atlassian.net/browse/ISSUE-17681
+    lock fsiLock (fun () ->
+        let fsi = sessionCreator()
+
+        // Load the script file
         let moduleName = Path.GetFileNameWithoutExtension path
         let initScript = sprintf """#load @"%s"; open %s;;""" fullPath moduleName
         let res, errs = fsi.EvalInteractionNonThrowing(initScript)
         match res with
         | Choice2Of2 ex ->
-            let uex = Extract.ExtractException("ELI46925", "Error loading script", ex)
+            let uex = ExtractException("ELI46925", "Error loading script", ex)
             uex.AddDebugData("Script path", fullPath, false)
             errs |> Array.iter (fun errorInfo ->
                uex.AddDebugData("Error", errorInfo.ToString (), false))
@@ -62,6 +77,7 @@ module Evaluator =
 
         let typeName = sprintf "%s.%s" typeof<'TData>.Namespace typeof<'TData>.Name
 
+        // Get the functions
         functionNames
         |> Array.map (fun functionName ->
             // Annotate the function name with its full type to avoid value restriction exception
@@ -70,22 +86,22 @@ module Evaluator =
             match res with
             | Choice1Of2 (Some f) -> f.ReflectionValue :?> Transformation<'TData>
             | _ ->
-                let uex = Extract.ExtractException("ELI46954", "Error getting function")
+                let uex = ExtractException("ELI46954", "Error getting function")
                 uex.AddDebugData("Script path", fullPath, false)
                 uex.AddDebugData("Function name", functionName, false)
                 errs |> Array.iter (fun errorInfo ->
                    uex.AddDebugData("Error", errorInfo.ToString (), false))
                 raise uex
         )
+    )
 
-// Load a single function from a file
-// Throws if the script can't be loaded or the function doesn't exist
-let LoadFunction<'TData>(scriptPath, functionName, collectible, ([<ParamArray>] includeDirectories : string[])): Transformation<'TData> =
-    Evaluator.evaluate scriptPath [| functionName |] includeDirectories collectible
+/// Load a single function from a file
+/// Throws if the script can't be loaded or the function doesn't exist
+let LoadFunction<'TData>(scriptPath, functionName, collectible, ([<ParamArray>] includeDirectories: string[])): Transformation<'TData> =
+    evaluate scriptPath [| functionName |] includeDirectories collectible
     |> Array.head
 
-// Loads functions from a file
-// Throws if the script can't be loaded or any of the functions don't exist
-let LoadFunctions<'TData>(scriptPath, functionNames, collectible, [<ParamArray>] includeDirectories): Transformation<'TData>[] =
-    let includeDirectories = if includeDirectories = null then [||] else includeDirectories
-    Evaluator.evaluate scriptPath functionNames includeDirectories collectible
+/// Loads functions from a file
+/// Throws if the script can't be loaded or any of the functions don't exist
+let LoadFunctions<'TData>(scriptPath, functionNames, collectible, [<ParamArray>] includeDirectories: string[]): Transformation<'TData>[] =
+    evaluate scriptPath functionNames includeDirectories collectible
