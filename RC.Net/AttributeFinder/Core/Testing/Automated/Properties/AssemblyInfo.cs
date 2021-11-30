cc802d6f616dfa5419cbd0903d6ba22c76ea5d19
Nat <nathaniel_heyer@extractsystems.com>
2021-11-30 15:02:37 -0600

    ISSUE-17587: Nunit: Use of StringBuilder in named pipe code can result in bad data https://extract.atlassian.net/browse/ISSUE-17587
    Convert project to SDK style
    Add some unit tests for client/server code that uses the modified named pipes functions

--- RC.Net/AttributeFinder/Core/Testing/Automated/Properties/AssemblyInfo.cs
+++ RC.Net/AttributeFinder/Core/Testing/Automated/Properties/AssemblyInfo.cs
@@ -1,18 +1,5 @@
 ﻿using System;
-using System.Reflection;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Extract.AttributeFinder.Test")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Extract Systems")]
-[assembly: AssemblyProduct("Extract.AttributeFinder.Test")]
-[assembly: AssemblyCopyright("Copyright © Extract Systems 2021")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -23,16 +10,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("aa903315-92ca-4dd2-b247-2f2e6ce43216")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RC.Net/AttributeFinder/Core/Testing/Automated/TestClassifyCandidates.cs
@@ -0,0 +1,170 @@
+﻿using Extract.AttributeFinder.MLNet.ClassifyCandidates;
+using Extract.Testing.Utilities;
+using Extract.Utilities;
+using Extract.Utilities.FSharp;
+using Microsoft.FSharp.Core;
+using Microsoft.ML;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Extract.AttributeFinder.Test
+{
+    [Category("ClassifyCandidates"), Category("Automated")]
+    public class TestClassifyCandidates
+    {
+        private static TestFileManager<TestClassifyCandidates> _testFiles;
+
+        [OneTimeSetUp]
+        public static void Setup()
+        {
+            GeneralMethods.TestSetup();
+
+            _testFiles = new();
+        }
+
+        [OneTimeTearDown]
+        public static void FinalCleanup()
+        {
+            _testFiles.Dispose();
+        }
+
+        /// Confirm that the client/server code functions properly under heavy loads
+        /// Ideally, one server process will get created to handle all the clients
+        [Test]
+        [Parallelizable(ParallelScope.All)]
+        public static void StressServerCreationLogic(
+            [Values(1, 8, 16)] int simultaneousRequests,
+            [Range(0, 30, 10)] int secondsToCreateServer)
+        {
+            // Arrange
+
+            using TemporaryFile modelMock = new(false);
+            Mock<INamedPipeServer> serverMock = new();
+
+            // Configure the start action to run a server that creates and responds to requests on a named pipe
+            serverMock.Setup(x =>
+                x.Start(It.IsAny<string>()))
+                .Callback<string>(pipeName =>
+                {
+                    Task.Run(async () =>
+                    {
+                        await Task.Delay(TimeSpan.FromSeconds(secondsToCreateServer));
+                        NamedPipe.listenForRequests(pipeName,
+                            FuncConvert.FromAction<DTO.PredictionRequest>(x => { }));
+                    });
+                });
+
+            var startAction = FuncConvert.FromAction<string>(serverMock.Object.Start);
+
+            // The actual time to create the server will be longer than the nominal time
+            // E.g., it will depend on how many simultaneous requests are made
+            // and how many other tests are running
+            var parallelPadding = simultaneousRequests * 42;
+            var timeToWaitForServer = TimeSpan.FromSeconds(secondsToCreateServer + parallelPadding);
+
+            Predict.ServerRequest serverRequest = new(
+                modelPath: modelMock.FileName,
+                serverName: "Extract.TestServer",
+                serverCreator: startAction,
+                timeToWaitForServerCreation: timeToWaitForServer,
+                predictionRequest: new("input", "output"));
+
+            // Act
+
+            // Simultaneously try to send many messages
+            Enumerable.Range(1, simultaneousRequests)
+                .AsParallel()
+                .ForAll(_ => Predict.sendRequestToServer(serverRequest));
+
+            // Assert
+
+            // Verify that the server was started exactly one time 
+            serverMock.Verify(x => x.Start(It.IsAny<string>()), Times.Once);
+        }
+
+        /// Confirm that exceptions from the server are readable by the client
+        [Test]
+        [Parallelizable(ParallelScope.Self)]
+        public static void ExceptionHandling()
+        {
+            // Arrange
+
+            string modelFile = _testFiles.GetFile("Resources.MLNet.MLModel_Kofax_MICR_Classifier.zip");
+            DTO.PredictionRequest predictionRequest = new("input", "output");
+
+            // Act
+
+            Exception actualException = null;
+            try
+            {
+                Predict.predictWithMLNetQueue(modelFile, predictionRequest);
+            }
+            catch (Exception ex)
+            {
+                actualException = ex;
+            }
+
+            // Assert
+
+            Assert.IsInstanceOf<ArgumentOutOfRangeException>(actualException);
+            Assert.AreEqual("File does not exist at path: input\r\nParameter name: path", actualException.Message);
+            Assert.That(Regex.IsMatch(actualException.ToString(),
+                String.Join(@"[\S\s]*",
+                    new string[] {
+                        @"at Microsoft\.ML\.BinaryLoaderSaverCatalog\.LoadFromBinary",
+                        @"MLNetQueue\\Code\\Predict\.fs",
+                        @"Utilities\\FSharp\\Core\\Code\\NamedPipe\.fs",
+                        @"TestClassifyCandidates\.cs"
+                    })));
+        }
+
+        /// Confirm that predictions can be made
+        [Test]
+        [Parallelizable(ParallelScope.Self)]
+        public static void TestPredicting()
+        {
+            // Arrange
+
+            string modelFile = _testFiles.GetFile("Resources.MLNet.MLModel_Kofax_MICR_Classifier.zip");
+            string inputDataFile = _testFiles.GetFile("Resources.MLNet.inputData.bin");
+            using TemporaryFile outputDataFile = new(false);
+            DTO.PredictionRequest predictionRequest = new(inputDataFile, outputDataFile.FileName);
+
+            // Confirm the number of examples in the input data
+            const int expectedNumExamples = 18;
+            MLContext mlContext = new();
+            Assume.That(mlContext.Data.LoadFromBinary(inputDataFile).GetRowCount(), Is.EqualTo(expectedNumExamples));
+
+            // Act
+
+            Predict.predictWithMLNetQueue(modelFile, predictionRequest);
+
+            // Assert
+
+            IDataView dataView = mlContext.Data.LoadFromBinary(outputDataFile.FileName);
+            List<Prediction> predictions = mlContext.Data.CreateEnumerable<Prediction>(dataView, false).ToList();
+
+            // Output has the same number of examples as the input
+            Assert.AreEqual(expectedNumExamples, predictions.Count);
+
+            // One item is predicted to be true
+            Assert.AreEqual(1, predictions.Where(p => p.Probability > 0.5).Count());
+        }
+    }
+
+    public interface INamedPipeServer
+    {
+        public void Start(string pipeName);
+    }
+
+    public class Prediction
+    {
+        public float Probability { get; set; }
+        public float Score { get; set; }
+    }
+}
