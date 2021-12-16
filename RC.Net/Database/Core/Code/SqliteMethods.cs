JamesGuiExtract/AvaloniaPersonal

commit ef736d25866f42fba8dfc104f7a74523d17e658f

ISSUE-17879: Unable to Add AKAs to OrderMapping DB: unable to open database file https://extract.atlassian.net/browse/ISSUE-17879
Add a debug info value with the DB path to the exception.
Change debug logging of missing dispose calls so that they exclude false positives that can happen when a connection constructor throws an exception

RC.Net/Database/Core/Code/SqliteMethods.cs
@@ -71,23 +71,39 @@ namespace Extract.Database
         }
 
         /// <summary>
-        /// Escape leading backslashes in the DataSource if needed (For UNC paths. Changes \\ to \\\\)
+        /// Create and open a SQLite connection
         /// </summary>
-        /// <param name="connectionString">The connection string to fix</param>
-        public static string FixConnectionString(string connectionString)
+        /// <remarks>
+        /// Escapes leading backslashes in the DataSource if needed (For UNC paths. Changes \\ to \\\\)
+        /// </remarks>
+        /// <param name="connectionString">The connection string to use for the connection</param>
+        internal static SQLiteConnection OpenConnection(string connectionString)
         {
+            string databasePath = null;
+
             try
             {
                 _ = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
 
                 SQLiteConnectionStringBuilder builder = new(connectionString);
-                builder.DataSource = FixDatabasePath(builder.DataSource);
+                builder.DataSource = databasePath = FixDatabasePath(builder.DataSource);
 
-                return builder.ConnectionString;
+                SQLiteConnection connection = new()
+                {
+                    ConnectionString = connectionString
+                };
+                connection.Open();
+
+                return connection;
             }
             catch (Exception ex)
             {
-                throw ex.AsExtract("ELI51935");
+                var uex = ex.AsExtract("ELI51935");
+                if (databasePath != null)
+                {
+                    uex.AddDebugData("Database path", databasePath);
+                }
+                throw uex;
             }
         }
     }

RC.Net/Database/Core/Code/DatabaseConnectionInfo.cs
@@ -505,29 +505,26 @@ namespace Extract.Database
                     ExtractException.Assert("ELI34758", "Database provider has not been specified.",
                         TargetConnectionType != null);
 
-                    DbConnection dbConnection = null;
                     var expandedConnectionString = (PathTags == null)
                         ? ConnectionString
                         : PathTags.Expand(ConnectionString);
                     
                     if (TargetConnectionType == typeof(SQLiteConnection))
                     {
-                        dbConnection = (DbConnection)Activator.CreateInstance(TargetConnectionType);
-
-                        // Fix UNC path for SQLite
-                        // https://extract.atlassian.net/browse/ISSUE-17754
-                        expandedConnectionString = SqliteMethods.FixConnectionString(expandedConnectionString);
-                        dbConnection.ConnectionString = expandedConnectionString;
+                        return SqliteMethods.OpenConnection(expandedConnectionString);
                     }
-                    else if (!ExtractRoleConnection.TryGetConnection(expandedConnectionString, ref dbConnection))
+                    else if (ExtractRoleConnection.TryOpenConnection(expandedConnectionString, out ExtractRoleConnection connection))
                     {
-                        dbConnection = (DbConnection)Activator.CreateInstance(TargetConnectionType);
-                        dbConnection.ConnectionString = expandedConnectionString;
+                        return connection;
                     }
+                    else
+                    {
+                        DbConnection dbConnection = (DbConnection)Activator.CreateInstance(TargetConnectionType);
+                        dbConnection.ConnectionString = expandedConnectionString;
+                        dbConnection.Open();
 
-                    dbConnection.Open();
-
-                    return dbConnection; 
+                        return dbConnection; 
+                    }
                 }
             }
             catch (Exception ex)

RC.Net/SqlDatabase/Extract.SqlDatabase/ExtractRoleConnection.cs
@@ -41,29 +41,28 @@ namespace Extract.SqlDatabase
         }
 
         /// <summary>
-        /// Attempts to create the DbConnection specified by the provided <paramref name="connectionString"/>
+        /// Attempts to create a <see cref="SqlConnection"/> using the provided <paramref name="connectionString"/>
         /// as a connection to a FAM database with the Extract application role assigned.
         /// </summary>
         /// <param name="connectionString">The database connection string</param>
-        /// <param name="dbConnection">The resulting connection, if successful.
+        /// <param name="connection">The resulting connection, if successful.
         /// NOTE: If successful and the database connection string is returned, it will already be open.</param>
         /// <returns><c>true</c> if the database connection was established and Extract application role
         /// was successfully applied; <c>false</c> if not successful for any reason (connection string doesn't
-        /// specify an SQL database, couldn't connect to the database, or the database is not a FAM DB.</returns>
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "Changing exception to bool")]
-        public static bool TryGetConnection(string connectionString, ref DbConnection dbConnection)
+        /// specify a SQL database, couldn't connect to the database, or the database is not a FAM DB)</returns>
+        public static bool TryOpenConnection(string connectionString, out ExtractRoleConnection connection)
         {
-            dbConnection = null;
+            connection = null;
             try
             {
-                dbConnection = new ExtractRoleConnection(connectionString);
-                dbConnection.Open();
+                connection = new ExtractRoleConnection(connectionString);
+                connection.Open();
                 return true;
             }
             catch
             {
-                dbConnection?.Dispose();
-                dbConnection = null;
+                connection?.Dispose();
+                connection = null;
                 return false;
             }
         }

RC.Net/SqlDatabase/Extract.SqlDatabase/SqlAppRoleConnection.cs
@@ -24,7 +24,7 @@ namespace Extract.SqlDatabase
         /// <summary>
         /// Created when application role is enabled and needed for disabling the created app role
         /// </summary>
-        byte[] AppRoleCookie;
+        byte[] _appRoleCookie;
 
         protected SqlAppRoleConnection() : base()
         {
@@ -142,6 +142,7 @@ namespace Extract.SqlDatabase
             }
 
             BaseSqlConnection.Open();
+
             if (UseApplicationRoles)
             {
                 AssignRole();
@@ -174,12 +175,24 @@ namespace Extract.SqlDatabase
         }
 
         private bool disposedValue;
+
         protected override void Dispose(bool disposing)
         {
             if (disposedValue)
             {
                 return;
             }
+#if DEBUG
+            // Log an exception if the connection was opened and the app role assigned but dispose was not called.
+            // This also prevents an exception from being logged when Close is called instead of Dispose, which should be OK.
+            // (ExtractRoleConnection.TryOpenConnection can be called using invalid connection strings.
+            // This will cause an exception to be thrown before this instance is completely constructed and so it will not get disposed.
+            // An exception logged for this case would be misleading)
+            if (!disposing && _appRoleCookie != null)
+            {
+                new ExtractException("ELI53007", "Dispose was not called on a SqlAppRoleConnection").Log();
+            }
+#endif
             try
             {
                 // Call close to unset the app role when this object is finalized in case someone forgot to dispose
@@ -193,12 +206,6 @@ namespace Extract.SqlDatabase
                 BaseSqlConnection?.Dispose();
                 BaseSqlConnection = null;
             }
-#if DEBUG
-            if (!disposing)
-            {
-                new ExtractException("ELI53007", "Dispose was not called on a SqlAppRoleConnection").Log();
-            }
-#endif
 
             base.Dispose(disposing);
 
@@ -227,7 +234,7 @@ namespace Extract.SqlDatabase
 
 
                 cmd.ExecuteNonQuery();
-                AppRoleCookie = cmd.Parameters["@cookie"].Value as Byte[];
+                _appRoleCookie = cmd.Parameters["@cookie"].Value as Byte[];
             }
             catch (Exception ex)
             {
@@ -237,7 +244,7 @@ namespace Extract.SqlDatabase
 
         protected void UnsetApplicationRole()
         {
-            if (AppRoleCookie is null)
+            if (_appRoleCookie is null)
                 return;
 
             if (BaseSqlConnection?.State != ConnectionState.Open)
@@ -248,7 +255,7 @@ namespace Extract.SqlDatabase
                 using var cmd = BaseSqlConnection.CreateCommand();
                 cmd.CommandType = CommandType.StoredProcedure;
                 cmd.CommandText = "sys.sp_unsetapprole";
-                cmd.Parameters.AddWithValue("@cookie", AppRoleCookie);
+                cmd.Parameters.AddWithValue("@cookie", _appRoleCookie);
 
                 cmd.ExecuteNonQuery();
             }
@@ -258,7 +265,7 @@ namespace Extract.SqlDatabase
             }
             finally
             {
-                AppRoleCookie = null;
+                _appRoleCookie = null;
             }
         }
 

commit ccabe0ff3abd9ea25885ba326b372f802751a741

ISSUE-17754: DatabaseContentsCondition and DataQueryRuleObject fail when database path is UNC format https://extract.atlassian.net/browse/ISSUE-17754
Fix regression introduced by commit ef736d25866f42fba8dfc104f7a74523d17e658f

RC.Net/Database/Core/Code/SqliteMethods.cs
@@ -90,7 +90,7 @@ namespace Extract.Database
 
                 SQLiteConnection connection = new()
                 {
-                    ConnectionString = connectionString
+                    ConnectionString = builder.ConnectionString
                 };
                 connection.Open();
 