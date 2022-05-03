beaa98c1bc04bcfa78ee52f2fbf13770ef67e2b7
Nat <nathaniel_heyer@extractsystems.com>
2022-05-02 19:01:28 -0500

    Convert FileSuppliers project to SDK style

--- RC.Net/FileActionManager/FileSuppliers/Core/Code/Properties/AssemblyInfo.cs
+++ RC.Net/FileActionManager/FileSuppliers/Core/Code/Properties/AssemblyInfo.cs
@@ -1,20 +1,6 @@
 ﻿using System;
-using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Resources;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Extract.FileActionManager.FileSuppliers")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Extract Systems")]
-[assembly: AssemblyProduct("Extract.FileActionManager.FileSuppliers")]
-[assembly: AssemblyCopyright("Copyright © Extract Systems 2022")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Mark assembly as CLS compliant.
 [assembly: CLSCompliant(true)]
@@ -26,19 +12,5 @@ using System.Resources;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4ffcc04c-8ce2-443b-96d8-37cff91fa588")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: NeutralResourcesLanguageAttribute("en")]
 
 [assembly: InternalsVisibleTo("Extract.Email.GraphClient.Test, PublicKey=0024000004800000940000000602000000240000525341310004000001000100d52db94ff1a0cc337264e7a70fd9d97706667394327d7927573d59ae003bf63a47cbbfb497fcfe234f854042800a8cbca11a35e17fe12f0a021383ac0973541ff1648921c5ce72b7476138f311dd67bebe3b1b3360a4d17a4bec2a92514aca3b7962d89b8fffd7cae5c436b5e17720987bba72c85dfef30a59f64d2d459912cb")]
