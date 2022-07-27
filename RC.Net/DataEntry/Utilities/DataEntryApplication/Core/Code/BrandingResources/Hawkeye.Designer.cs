67c997b791d1a04a90179a8e80d3d3d3613227a9
Ethan Zimmermann <ethan_zimmermann@extractsystems.com>
2022-07-27 11:02:25 -0500

    Updating and adding new resource files to match contain new logos and store logos for reference outside of embedding in resource files https://extract.atlassian.net/browse/ISSUE-18379

--- RC.Net/DataEntry/Utilities/DataEntryApplication/Core/Code/BrandingResources/Hawkeye.Designer.cs
+++ RC.Net/DataEntry/Utilities/DataEntryApplication/Core/Code/BrandingResources/Hawkeye.Designer.cs
@@ -22,14 +22,14 @@ namespace Extract.DataEntry.Utilities.DataEntryApplication.BrandingResources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Default {
+    internal class Hawkeye {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Default() {
+        internal Hawkeye() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace Extract.DataEntry.Utilities.DataEntryApplication.BrandingResources {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Extract.DataEntry.Utilities.DataEntryApplication.BrandingResources.Default", typeof(Default).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Extract.DataEntry.Utilities.DataEntryApplication.BrandingResources.Hawkeye", typeof(Hawkeye).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -90,7 +90,7 @@ namespace Extract.DataEntry.Utilities.DataEntryApplication.BrandingResources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Extract Systems.
+        ///   Looks up a localized string similar to Hawkeye.
         /// </summary>
         internal static string ApplicationTitle {
             get {
