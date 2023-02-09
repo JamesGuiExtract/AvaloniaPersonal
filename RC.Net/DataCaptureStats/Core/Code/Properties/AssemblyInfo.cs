3c54a5db27e63d49bb6dce0b94b36944bf9024cf
Nat <nathaniel_heyer@extractsystems.com>
2023-02-09 14:02:56 -0600

    DataCaptureStats: Update project to SDK-style

--- RC.Net/DataCaptureStats/Core/Code/Properties/AssemblyInfo.cs
+++ RC.Net/DataCaptureStats/Core/Code/Properties/AssemblyInfo.cs
@@ -1,19 +1,5 @@
 ﻿using System;
-using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Extract.DataCaptureStats")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Extract.DataCaptureStats")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Mark assembly as not CLS compliant.
 [assembly: CLSCompliant(false)]
@@ -26,18 +12,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("32679a70-bc10-4dd9-9155-9b3e08586ee9")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes",
-    Scope = "namespace", Target = "Extract.DataCaptureStats")]
+    Scope = "namespace", Target = "~N:Extract.DataCaptureStats")]
