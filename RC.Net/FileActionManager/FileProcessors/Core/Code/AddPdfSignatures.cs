a3d77e630828724de7f569e0b94e393c8ccdde70
TreverExtract <115719776+TreverExtract@users.noreply.github.com>
2023-04-24 15:32:23 -0500

    Create task skeleton for PDF signatures (#155)
    * Create task skeleton for PDF signatures
    https://extract.atlassian.net/browse/ISSUE-19202

---
+++ RC.Net/FileActionManager/FileProcessors/Core/Code/AddPdfSignatures.cs
@@ -0,0 +1,492 @@
+﻿using Extract.Interop;
+using Extract.Licensing;
+using System;
+using System.Runtime.InteropServices;
+using UCLID_COMLMLib;
+using UCLID_COMUTILSLib;
+using UCLID_FILEPROCESSINGLib;
+
+namespace Extract.FileActionManager.FileProcessors
+{
+    /// <summary>
+    /// Interface definition for <see cref="AddPdfSignaturesTask"/>
+    /// </summary>
+    [ComVisible(true)]
+    [Guid("9F9D2133-2EAF-4B4A-A69D-A42DDCE109C3")]
+    [CLSCompliant(false)]
+    public interface IAddPdfSignaturesTask :
+        ICategorizedComponent,
+        IConfigurableObject,
+        IMustBeConfiguredObject,
+        ICopyableObject,
+        IFileProcessingTask,
+        ILicensedComponent,
+        IPersistStream
+    {
+    }
+
+    /// <summary>
+    /// An <see cref="IFileProcessingTask"/> that adds a pdf signatures.
+    /// </summary>
+    [ComVisible(true)]
+    [Guid("6BDC67A3-4D30-4361-8212-D4065C5D5416")]
+    [ProgId("Extract.FileActionManager.FileProcessors.AddPdfSignaturesTask")]
+    public class AddPdfSignaturesTask : IAddPdfSignaturesTask
+    {
+        #region fields
+        /// <summary>
+        /// The description of this task
+        /// </summary>
+        const string _COMPONENT_DESCRIPTION = "Core: Add a PDF Signatures";
+
+        /// <summary>
+        /// The license id to validate in licensing calls
+        /// </summary>
+        const LicenseIdName _LICENSE_ID = LicenseIdName.FileActionManagerObjects;
+
+        /// <summary>
+        /// Indicates that settings have been changed, but not saved.
+        /// </summary>
+        bool _dirty;
+
+        /// <summary>
+        /// Current task version.
+        /// </summary>
+        const int _CURRENT_VERSION = 1;
+        #endregion
+
+        #region constructors
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AddPdfSignaturesTask"/> class.
+        /// </summary>
+        public AddPdfSignaturesTask()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AddPdfSignaturesTask"/> class.
+        /// </summary>
+        /// <param name="task">The <see cref="AddPdfSignaturesTask"/> from which settings should
+        /// be copied.</param>
+        public AddPdfSignaturesTask(AddPdfSignaturesTask task)
+        {
+            try
+            {
+                CopyFrom(task);
+            }
+            catch (Exception ex)
+            {
+                throw ex.AsExtract("ELI54277");
+            }
+        }
+        #endregion
+
+        #region IAddPdfSignaturesTask
+        #endregion
+
+        #region ICategorizedComponent Members
+
+        /// <summary>
+        /// Gets the name of the COM object.
+        /// </summary>
+        public string GetComponentDescription()
+        {
+            return _COMPONENT_DESCRIPTION;
+        }
+
+        #endregion ICategorizedComponent Members
+
+        #region IConfigurableObject Members
+
+        /// <summary>
+        /// Performs configuration needed to create a valid <see cref="AddPdfSignaturesTask"/>.
+        /// </summary>
+        /// <returns><c>true</c> if the configuration was successfully updated or
+        /// <c>false</c> if configuration was unsuccessful.</returns>
+        public bool RunConfiguration()
+        {
+            try
+            {
+                // Validate the license
+                LicenseUtilities.ValidateLicense(_LICENSE_ID, "ELI54278", _COMPONENT_DESCRIPTION);
+
+                // Make a clone to update settings and only copy if ok
+                var cloneOfThis = (AddPdfSignaturesTask)Clone();
+
+                FileProcessingDBClass fileProcessingDB = new();
+                fileProcessingDB.ConnectLastUsedDBThisProcess();
+
+                //using FillPdfSignaturesTaskDialog dialog = new();
+                //if (dialog.ShowDialog() == DialogResult.OK)
+                //{
+                //    CopyFrom(dialog.Settings);
+                //    return true;
+                //}
+
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw ExtractException.CreateComVisible("ELI54279",
+                    "Error running configuration.", ex);
+            }
+        }
+
+        #endregion IConfigurableObject Members
+
+        #region IMustBeConfiguredObject Members
+
+        /// <summary>
+        /// Checks if the object has been configured properly.
+        /// </summary>
+        /// <returns>
+        /// <c>true</c> if the object has been configured and <c>false</c> otherwise.
+        /// </returns>
+        public bool IsConfigured()
+        {
+            try
+            {
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex.CreateComVisible("ELI54280", "Unable to check configuration.");
+            }
+        }
+
+        #endregion IMustBeConfiguredObject Members
+
+        #region ICopyableObject Members
+
+        /// <summary>
+        /// Creates a copy of the <see cref="AddPdfSignaturesTask"/> instance.
+        /// </summary>
+        /// <returns>A copy of the <see cref="AddPdfSignaturesTask"/> instance.</returns>
+        public object Clone()
+        {
+            try
+            {
+                return new AddPdfSignaturesTask(this);
+            }
+            catch (Exception ex)
+            {
+                throw ExtractException.CreateComVisible("ELI54281", "Unable to clone object.", ex);
+            }
+        }
+
+        /// <summary>
+        /// Copies the specified <see cref="AddPdfSignaturesTask"/> instance into this one.
+        /// </summary>
+        /// <param name="pObject">The object from which to copy.</param>
+        public void CopyFrom(object pObject)
+        {
+            try
+            {
+                if (pObject is not AddPdfSignaturesTask task)
+                {
+                    throw new InvalidCastException("Invalid copy-from object. Requires " + nameof(AddPdfSignaturesTask));
+                }
+                CopyFrom(task);
+            }
+            catch (Exception ex)
+            {
+                throw ExtractException.CreateComVisible("ELI54282", "Unable to copy object.", ex);
+            }
+        }
+
+        #endregion ICopyableObject Members
+
+        #region IFileProcessingTask Members
+
+        /// <summary>
+        /// Gets the minimum stack size needed for the thread in which this task is to be run.
+        /// </summary>
+        /// <value>
+        /// The minimum stack size needed for the thread in which this task is to be run.
+        /// </value>
+        [CLSCompliant(false)]
+        public uint MinStackSize
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        /// <summary>
+        /// Returns a value indicating that the task does not display a UI
+        /// </summary>
+        public bool DisplaysUI
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Stops processing the current file.
+        /// </summary>
+        public void Cancel()
+        {
+            // Do nothing, this task is not cancellable
+        }
+
+        /// <summary>
+        /// Called when all file processing has completed.
+        /// </summary>
+        public void Close()
+        {
+            // Nothing to do
+        }
+
+        /// <summary>
+        /// Called to notify the file processor that the pending document queue is empty, but
+        ///	the processing tasks have been configured to remain running until the next document
+        ///	has been supplied. If the processor will standby until the next file is supplied it
+        ///	should return <c>true</c>. If the processor wants to cancel processing,
+        ///	it should return <c>false</c>. If the processor does not immediately know
+        ///	whether processing should be cancelled right away, it may block until it does know,
+        ///	and return at that time.
+        /// <para><b>Note</b></para>
+        /// This call will be made on a different thread than the other calls, so the Standby call
+        /// must be thread-safe. This allows the file processor to block on the Standby call, but
+        /// it also means that call to <see cref="ProcessFile"/> or <see cref="Close"/> may come
+        /// while the Standby call is still occurring. If this happens, the return value of Standby
+        /// will be ignored; however, Standby should promptly return in this case to avoid
+        /// needlessly keeping a thread alive.
+        /// </summary>
+        /// <returns><c>true</c> to standby until the next file is supplied;
+        /// <c>false</c> to cancel processing.</returns>
+        public bool Standby()
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Called before any file processing starts.
+        /// </summary>
+        /// <param name="nActionID">The ID of the action being processed.</param>
+        /// <param name="pFAMTM">The <see cref="FAMTagManager"/> to use to expand path tags and
+        /// functions.</param>
+        /// <param name="pDB">The <see cref="FileProcessingDB"/> in use.</param>
+        /// <param name="pFileRequestHandler">The <see cref="IFileRequestHandler"/> that can be used
+        /// by the task to carry out requests for files to be checked out, released or re-ordered
+        /// in the queue.</param>
+        [CLSCompliant(false)]
+        public void Init(int nActionID, FAMTagManager pFAMTM, FileProcessingDB pDB,
+            IFileRequestHandler pFileRequestHandler)
+        {
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                throw ex.CreateComVisible("ELI54283",
+                    "Unable to initialize \"" + _COMPONENT_DESCRIPTION + "\" task.");
+            }
+        }
+
+        /// <summary>
+        /// Processes the specified file.
+        /// </summary>
+        /// <param name="pFileRecord">The file record that contains the info of the file being
+        /// processed.</param>
+        /// <param name="nActionID">The ID of the action being processed.</param>
+        /// <param name="pFAMTM">A File Action Manager Tag Manager for expanding tags.</param>
+        /// <param name="pDB">The File Action Manager database.</param>
+        /// <param name="pProgressStatus">Object to provide progress status updates to caller.
+        /// </param>
+        /// <param name="bCancelRequested"><c>true</c> if cancel was requested;
+        /// <c>false</c> otherwise.</param>
+        /// <returns>An <see cref="EFileProcessingResult"/> indicating the result of the
+        /// processing.</returns>
+        [CLSCompliant(false)]
+        public EFileProcessingResult ProcessFile(
+            FileRecord pFileRecord,
+            int nActionID,
+            FAMTagManager pFAMTM,
+            FileProcessingDB pDB,
+            ProgressStatus pProgressStatus,
+            bool bCancelRequested)
+        {
+            try
+            {
+                if (bCancelRequested)
+                {
+                    return EFileProcessingResult.kProcessingCancelled;
+                }
+
+                // Validate the license
+                LicenseUtilities.ValidateLicense(LicenseIdName.FileActionManagerObjects, "ELI54284", _COMPONENT_DESCRIPTION);
+
+                return EFileProcessingResult.kProcessingSuccessful;
+            }
+            catch (Exception ex)
+            {
+                throw ex.CreateComVisible("ELI54285", "Failed to add PDF signature");
+            }
+        }
+
+        #endregion IFileProcessingTask Members
+
+        #region IAccessRequired Members
+
+        /// <summary>
+        /// Returns bool value indicating if the task requires admin access.
+        /// </summary>
+        /// <returns><c>true</c> if the task requires admin access
+        /// <c>false</c> if task does not require admin access.</returns>
+        public bool RequiresAdminAccess()
+        {
+            return false;
+        }
+
+        #endregion IAccessRequired Members
+
+        #region ILicensedComponent Members
+
+        /// <summary>
+        /// Gets whether this component is licensed.
+        /// </summary>
+        /// <returns><c>true</c> if the component is licensed; <c>false</c>
+        /// if the component is not licensed.</returns>
+        public bool IsLicensed()
+        {
+            try
+            {
+                return LicenseUtilities.IsLicensed(_LICENSE_ID);
+            }
+            catch (Exception ex)
+            {
+                throw ExtractException.CreateComVisible("ELI54286",
+                    "Unable to determine license status.", ex);
+            }
+        }
+
+        #endregion ILicensedComponent Members
+
+        #region IPersistStream Members
+
+        /// <summary>
+        /// Returns the class identifier (CLSID) <see cref="Guid"/> for the component object.
+        /// </summary>
+        /// <param name="classID">Pointer to the location of the CLSID <see cref="Guid"/> on
+        /// return.</param>
+        public void GetClassID(out Guid classID)
+        {
+            classID = GetType().GUID;
+        }
+
+        /// <summary>
+        /// Checks the object for changes since it was last saved.
+        /// </summary>
+        /// <returns><see cref="HResult.Ok"/> if changes have been made;
+        /// <see cref="HResult.False"/> if changes have not been made.</returns>
+        public int IsDirty()
+        {
+            return HResult.FromBoolean(_dirty);
+        }
+
+        /// <summary>
+        /// Initializes an object from the <see cref="System.Runtime.InteropServices.ComTypes.IStream"/> where it was previously saved.
+        /// </summary>
+        /// <param name="stream"><see cref="System.Runtime.InteropServices.ComTypes.IStream"/> from which the object should be loaded.
+        /// </param>
+        public void Load(System.Runtime.InteropServices.ComTypes.IStream stream)
+        {
+            try
+            {
+                using IStreamReader reader = new(stream, _CURRENT_VERSION);
+
+                // Freshly loaded object is no longer dirty
+                _dirty = false;
+            }
+            catch (Exception ex)
+            {
+                throw ExtractException.CreateComVisible("ELI54287",
+                    "Unable to load object from stream.", ex);
+            }
+        }
+
+        /// <summary>
+        /// Saves an object into the specified <see cref="System.Runtime.InteropServices.ComTypes.IStream"/> and indicates whether the
+        /// object should reset its dirty flag.
+        /// </summary>
+        /// <param name="stream"><see cref="System.Runtime.InteropServices.ComTypes.IStream"/> into which the object should be saved.
+        /// </param>
+        /// <param name="clearDirty">Value that indicates whether to clear the dirty flag after the
+        /// save is complete. If <c>true</c>, the flag should be cleared. If
+        /// <c>false</c>, the flag should be left unchanged.</param>
+        public void Save(System.Runtime.InteropServices.ComTypes.IStream stream, bool clearDirty)
+        {
+            try
+            {
+                using IStreamWriter writer = new(_CURRENT_VERSION);
+
+                writer.WriteTo(stream);
+
+                if (clearDirty)
+                {
+                    _dirty = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ExtractException.CreateComVisible("ELI54288",
+                    "Unable to save object to stream", ex);
+            }
+        }
+
+        /// <summary>
+        /// Returns the size in bytes of the stream needed to save the object.
+        /// </summary>
+        /// <param name="size">Pointer to a 64-bit unsigned integer value indicating the size, in
+        /// bytes, of the stream needed to save this object.</param>
+        public void GetSizeMax(out long size)
+        {
+            size = HResult.NotImplemented;
+        }
+
+        #endregion IPersistStream Members
+
+        #region Private Members
+
+        /// <summary>
+        /// Code to be executed upon registration in order to add this class to the
+        /// <see cref="ExtractCategories.FileProcessorsGuid"/> COM category.
+        /// </summary>
+        /// <param name="type">The <paramref name="type"/> being registered.</param>
+        [ComRegisterFunction]
+        [ComVisible(false)]
+        static void RegisterFunction(Type type)
+        {
+            ComMethods.RegisterTypeInCategory(type, ExtractCategories.FileProcessorsGuid);
+        }
+
+        /// <summary>
+        /// Code to be executed upon unregistration in order to remove this class from the
+        /// <see cref="ExtractCategories.FileProcessorsGuid"/> COM category.
+        /// </summary>
+        /// <param name="type">The <paramref name="type"/> being unregistered.</param>
+        [ComUnregisterFunction]
+        [ComVisible(false)]
+        static void UnregisterFunction(Type type)
+        {
+            ComMethods.UnregisterTypeInCategory(type, ExtractCategories.FileProcessorsGuid);
+        }
+
+        /// <summary>
+        /// Copies the specified <see cref="IConvertEmailToPdfTask"/> instance into this one.
+        /// </summary>
+        /// <param name="task">The <see cref="IConvertEmailToPdfTask"/> from which to copy.</param>
+        void CopyFrom(IAddPdfSignaturesTask task)
+        {
+            _dirty = true;
+        }
+        #endregion Private Members
+
+        #region HelperMethods
+        #endregion
+    }
+}
